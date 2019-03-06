54b6a1724f9626259060f6df8c72fbb013a2a45d
Joel D. Simon <jdsimon@princeton.edu>
2019-03-06 16:29:31 -0500

    Clarify startup.m

--- omnealiud/startup.m
+++ omnealiud/startup.m
@@ -45,7 +45,8 @@ javaaddpath(fullfile(getenv('OMNIA'), 'notmycode', 'MatTaup', 'lib', ...
 addpath(genpath(getenv('IRISFETCH')))
 javaaddpath(fullfile(getenv('IRISFETCH'), 'IRIS-WS-2.0.18.jar'))
 
+% The rest is specific to JDS -- users who clone $OMNIA, and other
+% repositories it depends on, may delete.
 %____________________________________________________%
 
-% The rest is specific to JDS -- users who clone $OMNIA may delete.
 addpath(genpath(fullfile(getenv('MFILES'), 'papers', 'simon2019')));
