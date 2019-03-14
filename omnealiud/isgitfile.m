cbb5ef71bd3c2deaaa98cf116105feeb9526f2b1
Joel D. Simon <jdsimon@princeton.edu>
2019-03-14 11:31:02 -0400

    Modify isgitfile to have -C option for proper version

--- omnealiud/isgitfile.m
+++ omnealiud/isgitfile.m
@@ -1,27 +1,49 @@
 function  status = isgitfile(filename)
 % ISGITFILE(filename)
 %
-% ISGITFILE(filename) returns true if filename, given with a full
-% path, is under git version control.
+% ISGITFILE(filename) returns true if a FULL PATH filename is under
+% git version control.  Assumes user has git installed.
 %
 % Author: Joel D. Simon
 % Contact: jdsimon@princeton.edu
 % Last modified: 06-Dec-2018, Version 2017b
 
-if ~exist(filename, 'file') == 2
+% Sanity.
+if exist(filename, 'file') ~= 2
     error('%s does not exist or is not a filename\n', filename)
 
 end
 
-% Version 1.8.5 and above you can use git -C to perform git operations
-% in another folder.  Being that I have a prehistoric version of git
-% on my linux box I have to cd to the actual directory.
-startdir = pwd;
+%  Parse filename info.
 [filedir, name, ext] = fileparts(filename);
-cd(filedir)
 
-% Second output here to silence result of command in command window.
-[result, ~] = system(sprintf('git ls-files --error-unmatch -- %s', [name ext]));
+% Note the version of git; slightly different commands.  Wrap in try
+% command; if it fails likely don't have git installed.
+try
+    [~, git_version]  = system('git version');
+
+    
+catch
+    error('''git version'' in the command line failed -- is git installed?')
+
+end
+git_version = str2double(strrep(git_version(13:17), '.', ''));
+
+
+% Version 1.8.5 and above you can use git -C to perform git operations
+% in another folder. In older versions of git you must cd to the
+% actual file's directory.
+if git_version >= 185
+    %Second output here to silence result of command in command window.
+    [result, ~] = system(sprintf('git -C %s ls-files --error-unmatch -- %s', filedir, [name ext]));    
+
+else
+    startdir = pwd;
+    cd(filedir)
+    [result, ~] = system(sprintf('git ls-files --error-unmatch -- %s', [name ext]));
+    cd(startdir)
+
+end
 
 % Exit code of 0 means the file is tracked by git.
 if result == 0
@@ -32,7 +54,5 @@ else
 
 end
 
-% Back from whence you came.
-cd(startdir)
 
 
