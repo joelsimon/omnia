837caffad064a7d642ac32b971c6faee749ce77d
Joel D. Simon <jdsimon@princeton.edu>
2019-03-29 12:39:21 -0400

    Add tresphase.txt file write to treshist_merazur.m

--- mermaid/geoazur/treshist_merazur.m
+++ mermaid/geoazur/treshist_merazur.m
@@ -17,8 +17,9 @@ n_avail = [repmat(339, 1, 2) repmat(445, 1, 4)];
 locfac = 339 / 445;
 
 for j = 1:6
-    [hh(j), n_tot(j), n_plotted(j), mn(j), md(j), sd(j), dc(j)] = ...
-        hist_local(ha(j), tres_time(:, j), lim, n_avail(j));
+    [hh(j), n_tot(j), n_plotted(j), mn(j), md(j), sd(j), dc(j), ...
+     ph_plotted{j}, idx{j}] = hist_local(ha(j), tres_time(:, j), ...
+                                         lim, n_avail(j), tres_phase(:, j));
 
 end
 set(ha, 'XLim', [-lim lim])
@@ -41,7 +42,7 @@ longticks(ha, 0.75);
 axesfs(gcf, axfs, txfs)
 
 % Scale labels.
-proj = {'$x_1$ if $f_s=20$', ...
+scal = {'$x_1$ if $f_s=20$', ...
         '$x_2$ if $f_s=20$', ...
         '$x_1$ if $f_s=5$\n$x_3$ if $f_s=20$', ...
         '$x_2$ if $f_s=5$\n$x_4$ if $f_s=20$', ...
@@ -57,7 +58,7 @@ freq = {'$\\approx[10.0-5.0]$ Hz', ...
 
 for j = 1:6
     % Outside bottom: scale number and sampling frequency,
-    tx_proj(j) = text(ha(j), 0, 0, sprintf(proj{j}));
+    tx_scal(j) = text(ha(j), 0, 0, sprintf(scal{j}));
     tx_freq(j) = text(ha(j), 0, 0, sprintf(freq{j}));
     
     % Top right: mean and standard deviation 
@@ -68,8 +69,8 @@ for j = 1:6
     tn(j) = title(ha(j), sprintf('$\\mathrm{n}=%i~[%3.1f$%s]', n_plotted(j), dc(j), '\%'));
 
 end
-set(tx_proj(1:2), 'Position', [0 -47])
-set(tx_proj(3:end), 'Position', [0 -47/locfac])
+set(tx_scal(1:2), 'Position', [0 -47])
+set(tx_scal(3:end), 'Position', [0 -47/locfac])
 set(tx_freq(1:2), 'Position', [0 -70])
 set(tx_freq(3:end), 'Position', [0 -70/locfac])
 
@@ -77,10 +78,10 @@ set(tx_freq(3:end), 'Position', [0 -70/locfac])
 set(ms(1:2), 'Position', [-5.5 72])
 set(ms(3:end), 'Position', [-5.5 72/locfac])
 
-%tproj = text(ha(1), -11.5, -47, 'projection:');
+%tscal = text(ha(1), -11.5, -47, 'scalection:');
 %tfreq = text(ha(1), -11.5, -70, 'freq. band:');
 
-tproj = text(ha(1), -9, -47, 'proj.:');
+tscal = text(ha(1), -9, -47, 'scale:');
 tfreq = text(ha(1), -9, -70, 'freq.:');
 
 set(ha, 'XTick', [-6:2:6])
@@ -97,10 +98,10 @@ for j = 1:6
     %    ha(j).XTickLabel([2:2:end]) = {''};
 
 end
- set([tx_proj tproj tx_freq tfreq tn], 'Interpreter', 'Latex', ...
+ set([tx_scal tscal tx_freq tfreq tn], 'Interpreter', 'Latex', ...
                    'FontName', 'Times', 'FontSize', txfs, ...
                    'HorizontalAlignment', 'Center')
-set([tx_proj tx_freq tn], 'Interpreter', 'Latex', ...
+set([tx_scal tx_freq tn], 'Interpreter', 'Latex', ...
                   'FontName', 'Times', 'FontSize', txfs, ...
                   'HorizontalAlignment', 'Center')
 
@@ -118,19 +119,70 @@ moveh(lax, -0.014)
 
 savepdf('treshist')
 
+%______________________________________________________________%
+%% Collect phase information.
+
+% 1st: Find unique phases considering ALL scales.
+ph2consider = {};
+for j = 1:6
+    ph2consider = [ph2consider; unique(ph_plotted{j})];
+    
+end
+unique_ph = unique(ph2consider);
+
+
+for j = 1:6
+    for i = 1:length(unique_ph)
+        ph_count(i, j) = sum(strcmp(ph_plotted{j}, unique_ph(i)));
+
+    end
+    
+end
+
+
+% Sort based on scale with most arrivals.
+% [~, mostph] = max(n_plotted);
+% [~, sorder] = sort(ph_count(:, mostph), 'descend');  % 3rd column currently
+% unique_ph = unique_ph(sorder);
+% ph_count = ph_count(sorder, :);
+
+sum_scal = sum(ph_count, 1);
+sum_ph = sum(ph_count, 2);
+
+[sum_ph, sorder] = sort(sum_ph, 'descend');
+unique_ph = unique_ph(sorder);
+ph_count = ph_count(sorder, :);
+
+sz = size(ph_count);
+ph_count(sz(1)+1,:) = sum_scal;
+ph_count(:,sz(2)+1) = [sum_ph; NaN];
+
+% Concatenate into a matrix and write to text file.
+for i = 1:sz(1)
+    lyne{i} = sprintf('%5s:%s\n', unique_ph{i}, sprintf(repmat(' %3i', 1, sz(1)), ph_count(i,:)));
+
+end
+lyne{end+1} = sprintf('      %s\n', sprintf(repmat(' %3i', 1, sz(2)), ph_count(end,:)));
+
+fid = fopen('tresphase.txt', 'w');
+for i = 1:length(lyne)
+    fprintf(fid, '%s', lyne{i});
+
+end
+fclose(fid)
+
 %_________________________%
-function [hh, n_tot, n_plotted, mn, md, sd, dc] = hist_local(ha, tr, lim, n_avail)
-    tr_within_lims = tr(abs(tr) <= max(lim));
+function [hh, n_tot, n_plotted, mn, md, sd, dc, ph_plotted, idx] = ...
+        hist_local(ha, tr, lim, n_avail, ph)
+    
+    idx = find(abs(tr) <= max(lim));
+    tr_plotted = tr(idx);
+    ph_plotted = ph(idx);
     
     % Histogram travel time residual data.
-    % hh = histogram(ha, tr, 'BinLimits', [-lim lim], 'NumBins', 17, ...
-    %                'Normalization', 'Count', 'FaceColor', 'k', ...
-    %                'EdgeColor', 'k');
-
-
-    hh = histogram(ha, tr, 'BinLimits', [-lim lim], 'BinMethod', ...
-                   'Integer', 'Normalization', 'Count', 'FaceColor', ...
-                   'k', 'EdgeColor', 'k');
+    hh = histogram(ha, tr_plotted, 'BinMethod', 'Integer', ...
+                   'Normalization', 'Count', 'FaceColor', 'k', ...
+                   'EdgeColor', 'k');
 
     % This is the number actually plotted.
     n_plotted = sum(hh.BinCounts);
@@ -153,9 +205,9 @@ function [hh, n_tot, n_plotted, mn, md, sd, dc] = hist_local(ha, tr, lim, n_avai
 
     % Do statistics only on those values within the specified limits (say
     % outside limits obviously reject).
-    mn = nanmean(tr_within_lims);
-    md = nanmedian(tr_within_lims);
-    sd = nanstd(tr_within_lims, 1);
+    mn = nanmean(tr_plotted);
+    md = nanmedian(tr_plotted);
+    sd = nanstd(tr_plotted, 1);
 
     % NOTE I mimicked \textsc with a double superscript to string GA.
     %xlabel(ha, '$t_{\mathrm{res}}^{{}^\mathrm{GA}}$ (s)');
