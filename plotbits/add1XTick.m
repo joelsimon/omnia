6f947466fa76a0cfb09fe8e57c560058bee55b6b
Joel D. Simon <jdsimon@princeton.edu>
2019-06-05 15:26:31 -0400

    Introduce plotting nicety to add XTick at x = 1 or y = 1

--- plotbits/add1XTick.m
+++ plotbits/add1XTick.m
@@ -1,24 +1,42 @@
-function add1XTick(axs)
-% ADD1XTICK(axs)
+function add1XTick(ax, xory)
+% ADD1XTICK(ax, xory)
 %
-% Tries to add an XTick at x = 1 for every input axis handle.  Only
-% works if all current XTicks > 1. Default axs = gca.
+% Tries to add an XTick (or YTick) at x = 1 (or y = 1) for every input
+% axis handle.  Only works if all current XTicks > 1 (or YTicks > 1).
+%
+% Input:
+% ax       Axis handle (def: gca)
+% xory     'X' or 'Y' (def: 'X')
 %
 % Author: Joel D. Simon
 % Contact: jdsimon@princeton.edu
-% Last modified: 01-Dec-2017, Version 2017b
+% Last modified: 27-May-2019, Version 2017b
 
 % Default.
-defval('axs',gca)
+defval('ax', gca)
+defval('xory', 'X')
 
 % Sanity check.
-if ~all(isgraphics(axs,'axes'))
+if ~all(isgraphics(ax,'axes'))
     error('Pass axes handle as second argument')
 end
 
 % Do it.
-for i = 1:length(axs)
+for i = 1:length(ax)
+    switch lower(xory)
+      case 'x'
+        ticktype = 'XTick';
+
+      case 'y'
+        ticktype = 'YTick';
+
+      otherwise
+        error('Specify ''X'' or ''Y'' for input: xory')
+
+    end
+
     try
-        axs(i).XTick = [1 axs(i).XTick];
+        ax(i).XTick = [1 ax(i).(ticktype)];
+
     end
 end
