f8ce5ce59d4f584156141130cd7a8f3570bbddc2
Joel D. Simon <jdsimon@princeton.edu>
2019-08-05 18:01:09 -0400

    Complete V1.0 of firstarrival.m / plotfirstarrival.m
    Both codes need headers and cleanup, but they now work as expected.

--- mermaid/firstarrival.m
+++ mermaid/firstarrival.m
@@ -1,9 +1,6 @@
-function [tres, dat, syn, ph, xw, xaxw, maxc, diffc, twosd, EQ] = ...
-    firstarrival(s, ci, wlen, lohi, evtdir, sacdir)
-% [tres, dat, syn, ph, xw, xaxw, maxc, diffc, twosd, EQ] = ...
-%     FIRSTARRIVAL(s, ci, wlen, lohi, evtdir, sacdir)
+function [tres, dat, syn, ph, diffc, twosd, xw1, xaxw1, maxc_x, maxc_y, SNR, EQ, W1, xw2, W2] = firstarrival(s, ci, wlen, lohi, sacdir, evtdir)
 %
-%           tres = dat - syn
+%                  tres = dat - syn
 %
 % Input:   
 % s        SAC filename
@@ -22,14 +19,17 @@ function [tres, dat, syn, ph, xw, xaxw, maxc, diffc, twosd, EQ] = ...
 % dat      Actual arrival time computed with cpest.m
 % syn      Theoretical arrival time computed with taupTime.m
 % ph       Phase name associated with tres
-% xw       Windowed segment of x, after bandpass filtering,
+% xw1       Windowed segment of x, after bandpass filtering,
 %              centered on first theoretical arrival time
-% xaxw     x-axis centered on syn at 0 seconds
+% xaxw1     x-axis centered on syn at 0 seconds
+% maxc_x   
 % maxc     Maximum (or minimum) amplitude of bandpassed signal within
 %              window beginning at dat and ending wlen/2 seconds later
 % twosd    2-standard deviation error estimation per M1 method [s]*
 %              (def NaN)
 % EQ       Earthquake structure associated with SAC file
+%% W1
+%% W2
 %
 % * See cpci.m and paper??
 %
@@ -70,7 +70,7 @@ end
 % structure and the SAC file header.
 if ~isequal(EQ(1).TaupTimes(1).pt0, h.B)
     error('EQ(1).TaupTimes(1).pt0 ~= h.B')
-
+ 
 end
 
 % The synthetic (theoretical, 'syn') arrival time is stored in the EQ structure.
@@ -79,57 +79,73 @@ ph = EQ(1).TaupTimes(1).phaseName;
 
 % Bandpass filter the time series and select a windowed segment.
 xf = bandpass(x, 1/h.DELTA, lohi(1), lohi(2));
-[xw, W] = timewindow(xf, wlen, EQ(1).TaupTimes(1).truearsecs, 'middle', h.DELTA, h.B);
+[xw1, W1] = timewindow(xf, wlen, EQ(1).TaupTimes(1).truearsecs, 'middle', h.DELTA, h.B);
 
 % The offset x-axis, which sets syn at 0 s.
-xaxw = W.xax - syn;
+xaxw1 = W1.xax - syn;
 
-% Changepoint estimate sample w.r.t xw
-cp = cpest(xw, 'fast', false, true);  
+% Changepoint estimate sample considering only the windowed portion centered on the theoretical first arrival.
+cp = cpest(xw1, 'fast', false, true);  
+
+% Signal-to-noise ratio.
+SNR = wtsnr({xw1}, cp, 1);
 
 % The data ('dat') arrival sample is 1 sample after the changepoint
-% estimate, which is an estimate of the last sample of the noise.
-dat_sample = cp + 1;
+% estimate, which is an estimate of the last sample of the noise,
+% unless SNR <=1, at which point the arrival does not exist ("noise"
+% has more power than the "signal")
+if SNR > 1
+    dat_samp = cp + 1;
 
-% The data arrival time on an axis beginning at the time assigned to
-% the first sample of the seismogram (h.B in the SAC header and
-% EQ(1).TaupTimes(1).pt0 in the EQ structure, which are the same) is
-% simply the arrival sample index of the windowed x-axis.
-dat = W.xax(dat_sample);
+    % The data arrival time on an axis beginning at the time assigned to
+    % the first sample of the seismogram (h.B in the SAC header and
+    % EQ(1).TaupTimes(1).pt0 in the EQ structure, which are the same) is
+    % simply the arrival sample index of the windowed x-axis.
+    dat = W1.xax(dat_samp);
 
-% The travel time residual is defined as the arrival-time estimate (cp
-% + 1) of the windowed segment, mapped to the appropriate index in the
-% complete segment, minus the theoretical arrival time defined in the
-% complete time segment.
-tres = dat - syn;
+    % The travel time residual is defined as the arrival-time estimate (cp
+    % + 1) of the windowed segment, mapped to the appropriate index in the
+    % complete segment, minus the theoretical arrival time defined in the
+    % complete time segment.
+    tres = dat - syn;
 
-% Maximum amplitude (counts) considering a window of length wlen/2
-% starting at the actual phase arrival.
-xw2 = timewindow(xf, wlen/2, dat, 'first', h.DELTA, h.B);
-mmx = minmax(xw2');
-[~, mmx_idx] = max(abs(mmx));
-maxc = mmx(1);
+    % Maximum amplitude (counts) considering a window of length wlen/2
+    % starting at the actual phase arrival.
+    [xw2, W2] = timewindow(xf, wlen/2, dat, 'first', h.DELTA, h.B);
+    mmx = minmax(xw2');
+    [~, mmx_idx] = max(abs(mmx));
+    maxc_y = mmx(1);
 
-% Find the sample index of the (first occurrence) of the maximum
-% counts in the second windowed segment, which starts at dat and goes
-% for (roughly) wlen/2 s.
-diffc_samps = find(xw2 == maxc);
+    % Find the time on the original x-axis of the (first occurrence of)
+    % the maximum counts value.
+    maxc_x = W2.xax(find(xw2 == maxc_y));
 
-% As always, remove 1 sample from the sample length to convert to time
-% because N samples bracket N-1 sampling intervals.
-diffc = (diffc_samps - 1) * h.B; % *1
+    % diffc is then the dif between the arrival time and the time of the
+    % largest amplitude within the signal segment.
+    diffc = maxc_x - dat;
 
-% Uncertainty estimate.
-if ci 
-    M1 = cpci(xw, 'kw', 1000, [], 'fast', false, true);
+    % Uncertainty estimate.
+    if ci 
+        M1 = cpci(xw1, 'kw', 1000, [], 'fast', false, true);
 
-    % Do not remove 1 sample from M1 before multiplying it by the sampling
-    % interval because 0 error is 0 samples, not 1 (as is the case for the
-    % M2 method, where the interval under consideration is at minimum
-    % length 1 -- see changepoint.m).
-    twosd = M1.twostd * h.DELTA;
+        % Do not remove 1 sample from M1 before multiplying it by the sampling
+        % interval because 0 error is 0 samples, not 1 (as is the case for the
+        % M2 method, where the interval under consideration is at minimum
+        % length 1 -- see changepoint.m).
+        twosd = M1.twostd * h.DELTA;
 
+    else
+        twosd = NaN;
+        
+    end
 else
+    dat = NaN;
+    tres = NaN;
+    xw2 = NaN;
+    W2 = NaN;
+    maxc_y = NaN;
+    maxc_x = NaN;
+    diffc = NaN;
     twosd = NaN;
-    
+
 end
--- mermaid/plotfirstarrival.m
+++ mermaid/plotfirstarrival.m
@@ -1,15 +1,15 @@
 function f = plotfirstarrival(wlen)
 
 defval('s', [])
+defval('ci', false)
 defval('wlen', 30)
-defval('colo', 1)
-defval('cohi', 5);
+defval('lohi', [1 5])
 defval('sacdir', fullfile(getenv('MERMAID'), 'processed'))
-defval('revdir', fullfile(getenv('MERMAID'), 'events'))
+defval('evtdir', fullfile(getenv('MERMAID'), 'events'))
 defval('nplot', 2)
 
 if isempty(s)
-    s = revsac(1, sacdir, revdir);
+    s = revsac(1, sacdir, evtdir);
     
 end
 
@@ -29,71 +29,48 @@ switch nplot
     moveh(ha(1:4), -0.05)
     moveh(ha(9:12), 0.05)
     
-    movev(ha([1 5 9]), 0.08)
-    movev(ha([2 6 10]), (7/3)*10^-2)
-    movev(ha([3 7 11]), -(7/3)*10^-2)
-    movev(ha([4 8 12]), -0.06)    
+    axpos = linspace(-0.055, 0.085, 4);
+    movev(ha([1 5 9]), axpos(4))
+    movev(ha([2 6 10]), axpos(3))
+    movev(ha([3 7 11]), axpos(2))
+    movev(ha([4 8 12]), axpos(1))
 
   otherwise
     error('Specify either 1 or 2 for input: nplot');
 
 end
 
+latimes
+
 for i = 1:length(ha)
     ax = ha(i);
     axes(ax)
 
     ridx = randi([1, length(s)]);
-    [x, h] = readsac(s{ridx});
-    EQ = getevt(s{ridx}, revdir);
 
-    % Ensure time at first sample (pt0) is the same in both the EQ
-    % structure and the SAC file header.
-    if ~isequal(EQ(1).TaupTimes(1).pt0, h.B)
-        ridx
-        error('EQ(1).TaupTimes(1).pt0 ~= h.B')
-
-    end
-
-    % Bandpass filter the time series and select a windowed segment.
-    xf = bandpass(x, 1/h.DELTA, colo, cohi);
-    [xw, W] = timewindow(xf, wlen, EQ(1).TaupTimes(1).arsecs, 'middle', h.DELTA, h.B); % *1
-
-    % Changepoint estimate.
-    cp = cpest(xw, 'fast', false, true);  % w.r.t xw
-
-    % The travel time residual is defined as the arrival-time estimate 
-    % (cp + 1) of the windowed segment, mapped to the appropriate index in
-    % the complete segment, minus the theoretical arrival time defined
-    % in the complete time segment.
-    tres =  W.xax(cp + 1) - EQ(1).TaupTimes(1).truearsecs;
+    [tres, dat, syn, ph, diffc, twosd, xw1, xaxw1, maxc_x, maxc_y, SNR, ...
+     EQ, W1, xw2, W2] = firstarrival(s{ridx}, ci, wlen, lohi, sacdir, evtdir);
     
-    % Maximum amplitude (counts).
-    mmx = minmax(xw');
-    [~, mmx_idx] = max(abs(mmx));
-    max_counts = mmx(1);
 
-    % The offset values: xaxis
-    offset_xax = W.xax - EQ(1).TaupTimes(1).arsecs;
-    plot(offset_xax, xw, 'LineWidth', 1)
-
-   % Joel note: don't put dollar signs ($) around numbers because I
-   % use %e for the exponential notation and it makes the 'e' look
-   % weird.
-
-   % IDEA: have ridx be equal to the line number of firstarrival.txt;
-   % i.e., you may look up the SAC file with the line number.
-    title(sprintf('tres. = %.2f s', tres), 'FontSize', ax.FontSize, ...
-          'FontWeight', 'Normal')
+    plot(ax, xaxw1, xw1, 'LineWidth', 1)
+    
+    % Joel note: don't put dollar signs ($) around numbers because I
+    % use %e for the exponential notation and it makes the 'e' look
+    % weird.
+    title(sprintf('tres. = %.2f s, delay = %.2f s', tres, diffc), ...
+          'FontSize', ax.FontSize, 'FontWeight', 'Normal')
     sacname = strippath(strrep(s{ridx}, '_', '\_'));
-    ylabel(sprintf('counts [%.1e]', max_counts), 'FontSize', ax.FontSize)
-    xlabel(sprintf('time relative to %s \\textit{%s}-phase (s)\n[%s]', ...
-                   EQ(1).TaupTimes(1).model, EQ(1).TaupTimes(1).phaseName, sacname), ...
-           'FontSize', ax.FontSize)
+    ylabel(sprintf('counts [%.1e]', maxc_y), 'FontSize', ax.FontSize)
+    xlabel(sprintf('time relative to \\textit{%s}-phase (s)\n[%s]', ...
+                   ph, sacname), 'FontSize', ax.FontSize)
+
+    % xlabel(sprintf('time relative to %s \\textit{%s}-phase (s)\n%s', ...
+    %                EQ(1).TaupTimes(1).model, ph, sacname), ...
+    %        'FontSize', ax.FontSize)
 
     % Adjust the axes.
     xlim([-wlen/2 wlen/2])
-    rangey = range(xw) * 0.95;  % Expand axes by some multiple of the range of the windowed segment.
+    rangey = range(xw1) * 0.95;  % Expand axes by some multiple of the range of the windowed segment.
     ylim([-rangey rangey])
     if wlen == 30
         numticks(ax, 'x', 7);
@@ -102,19 +79,19 @@ for i = 1:length(ha)
         numticks(ax, 'x', 6);
 
     end
+
+    % Vertical lines marking theoretical and actual arrival times.
     hold(ax, 'on')
+    plot(ax, repmat(0, 1, 2), ylim, 'k', 'LineStyle', '--');  
+    plot(ax, repmat(tres, 1, 2), ylim, 'r', 'LineStyle', '-');
 
-    % Vertical lines marking theoretical and actual arrival times and max counts.
-    offset_TT_arsamp = EQ(1).TaupTimes(1).arsamp - (W.xlsamp - 1); % *2
-    plot(ax, repmat(offset_xax(offset_TT_arsamp), 1, 2), ylim, 'k', 'LineStyle', '--');
+    % Circle the maximum counts if it is within the time window plotted
+    % (W2 and thus the time of the maximum counts may extend beyond W1).
+    if maxc_x <=  W1.xrsecs
+        plot(ax, maxc_x - syn, maxc_y, 'ko', 'MarkerSize', 5)
 
-    offset_CP_arsamp = cp + 1;  % cp is already in reference to the offset, windowed segment
-    plot(ax, repmat(offset_xax(offset_CP_arsamp), 1, 2), ylim, 'r', 'LineStyle', '-');
-    
-    % Maximum amplitude (counts).
-    plot(ax, offset_xax(find(xw == max_counts)), max_counts, 'ko', 'MarkerSize', 5)
+    end
     hold(ax, 'off')
-    
     box on
     longticks(ax, 1.5);
 
@@ -133,22 +110,11 @@ for i = 1:length(ha)
     tack2corner(ax, txur, 'NorthEast')    
     
     % SNR.
-    SNR = wtsnr({xw}, cp, 1);
     txll = textpatch(ax, 'SouthWest', sprintf('SNR = %.1f', SNR), FontSize);
     tack2corner(ax, txll, 'SouthWest')    
 
     % Uncertainty estimate.
-    %    ci = true; 
-    ci = false;  
-    if ci 
-        M1 = cpci(xw, 'kw', 1000, [], 'fast', false, true);
-
-    else
-        M1.twostd = -999;
-    
-    end
-    M1_secs = M1.twostd * h.DELTA; % *3 
-    txlr = textpatch(ax, 'SouthEast', sprintf('2$\\cdot$SD = %.2f s', M1_secs), FontSize);
+    txlr = textpatch(ax, 'SouthEast', sprintf('2$\\cdot$SD = %.2f s', twosd), FontSize);
     tack2corner(ax, txlr, 'SouthEast')    
     latimes
 
