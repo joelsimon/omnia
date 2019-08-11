dc0fc46953a8d33df2045698fc38c22c9eebb0bf
Joel D. Simon <jdsimon@princeton.edu>
2019-08-11 16:32:15 -0400

    Cleanup and add header

--- mermaid/readfirstarrival.m
+++ mermaid/readfirstarrival.m
@@ -1,5 +1,14 @@
-function readfirstarrivals(filename)
+function varargout = readfirstarrival(filename)
+% [s, ph, tres, delay, twosd, SNR, maxc_y] = READFIRSTARRIVAL(filename)
+%
+% Reads and parses output text file of firstarrivals.m
+% See there for I/0/
+%
+% Author: Joel D. Simon
+% Contact: jdsimon@princeton.edu
+% Last modified: 09-Aug-2019, Version 2017b
 
+% Default.
 defval('filename', fullfile(getenv('MERMAID'), 'events', 'reviewed', ...
                             'identified', 'txt', 'firstarrivals.txt'))
 % Data format.
@@ -11,10 +20,12 @@ fmt = ['%44s    ' , ...
        '%9.1f    ' , ...
        '%d\n'];
 
+% Read.
 fid = fopen(filename, 'r');
 lynes = textscan(fid, fmt);
 fclose(fid);
 
+% Parse.
 s = lynes{1};
 ph = lynes{2};
 tres = lynes{3};
@@ -23,12 +34,6 @@ twosd = lynes{5};
 SNR = lynes{6};
 maxc_y = lynes{7};
 
-idx = find(~isnan(tres));
-
-
-keyboard
-scatter(dist(idx), mag(idx), tres(idx));
-keyboard
-
-
-tres(abs(tres(idx)) <= 6)
+% Collect.
+outargs = {s, ph, tres, delay, twosd, SNR, maxc_y};
+varargout = outargs(1:nargout);
