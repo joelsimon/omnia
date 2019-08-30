348420c3a3d209e3b4948c76040c5b6397ee163c
Joel D. Simon <jdsimon@princeton.edu>
2019-08-30 19:06:05 -0400

    Remove default wavelet scales (5) in favor of parsing from SAC filename

--- mermaid/writechangepointall.m
+++ mermaid/writechangepointall.m
@@ -7,7 +7,7 @@ function writechangepointall
 %
 % Author: Joel D. Simon
 % Contact: jdsimon@princeton.edu
-% Last modified: 23-Mar-2019, Version 2017b
+% Last modified: 30-Aug-2019, Version 2017b
 
 pool = gcp;
 diro = fullfile(getenv('MERMAID'), 'events', 'changepoints');
@@ -17,7 +17,18 @@ parfor i = 1:length(s)
     if isempty(CP)
         sans_sac = strrep(strippath(s{i}), '.sac', '');
         [x, h] = readsac(s{i});
-        writechangepoint(sans_sac, diro, 'time', x, 5, h.DELTA, h.B, 1, cpinputs, 1);
+
+        % Determine number of wavelet scales based on SAC filename.
+        if ~isempty(strfind(sans_sac, 'RAW'));
+            n = 6
+
+        else
+            wlt_idx = strfind(sans_sac, 'WLT');
+            n = sans_sac(wlt_idx+3:end);
+            n = str2double(n);
+
+        end
+        writechangepoint(sans_sac, diro, 'time', x, n, h.DELTA, h.B, 1, cpinputs, 1);
 
     end        
 end
