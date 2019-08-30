9f0a223368a5ee57b43d6ebb60c6507c152bb93c
Joel D. Simon <jdsimon@princeton.edu>
2019-08-30 18:55:26 -0400

    Add taper before filter and improve figure aesthetics

--- mermaid/recordsection.m
+++ mermaid/recordsection.m
@@ -1,34 +1,33 @@
 function [F, EQ, sac] = recordsection(id, lohi, alignon, ampfac, ...
                                       revdir, procdir, normlize)
-% [F, EQ, sac] = RECORDSECTION(id, lohi, alignon, ampfac, ...
-%                              revdir, procdir, normlize)
+% [F, EQ, sac] = RECORDSECTION(id, lohi, alignon, ampfac, evtdir, procdir, normlize)
 %
 % Plots a record section of all MERMAID seismograms that recorded the
-% same event, read from 'identified.txt', output from evt2txt.m
+% same event, according to 'identified.txt' (output of evt2txt.m)
 %
 % Input:
 % id        Event identification number (def: 10948555)
 % lohi      Bandpass corner frequencies in Hz, or
-%              NaN to plot raw seismograms  (def: [1 2])
+%               NaN to plot raw seismograms  (def: [1 2])
 % aligon    'etime': t=0 at event rupture time (def: etime)
 %           'atime': t=0 at theoretical first arrival
 %                    for every seismogram*
 % ampfac    Nondimensional amplitude multiplication factor (def: 3)
-% revdir    Path to 'reviewed' directory
+% evtdir    Path to 'events' directory
 %                (def: $MERMAID/events/)
 % procdir   Path to 'processed' directory
 %                (def: $MERMAID/processed/)
 % normlize  true: normalize each seismogram against itself (def: true)
 %                 (removes 1/sqrt(dist) amplitude decay)
 %           false: normalize each seismogram against ensemble
-%                  (preserves 1/sqrt(dist) amplitude decay)
+%                 (preserves 1/sqrt(dist) amplitude decay)
 %
 % Output:
 % F        Structure with figure handles and bits
-% EQ       EQ structure, as returned found with sac2evt.m
+% EQ       EQ structure returned by cpsac2evt.m
 % sac      SAC files whose traces are plotted
 %
-% *Travel time are not plotted if alignon='atime'. Also note that a
+% *Travel time are not plotted if alignon = 'atime'. Also note that a
 % vertical line at 0 seconds does not necessarily correspond to the
 % same phase / phase branch across different seismograms.  I.e., the 0
 % time for each seismogram is individually set to its first arrival,
@@ -52,6 +51,8 @@ function [F, EQ, sac] = recordsection(id, lohi, alignon, ampfac, ...
 
 % Wish list:
 %
+% Input phase name option, to compute only requested travel time curves.
+%
 % Overlaid travel time curves for 'atime' option.  Compute differences
 % between all phases and first-arriving phase at every distance.  Then
 % set first-arriving phase at every distance to 0 seconds and all
@@ -68,48 +69,12 @@ defval('id', 10948555)
 defval('lohi', [1 2]);
 defval('alignon', 'etime')
 defval('ampfac', 3)
-defval('revdir', fullfile(getenv('MERMAID'), 'events'))
+defval('evtdir', fullfile(getenv('MERMAID'), 'events'))
 defval('procdir', fullfile(getenv('MERMAID'), 'processed'))
 defval('normlize', true)
 
-% Assumes Princeton-owned, third-generation MERMAID float SAC file
-% naming convention (NOT older, GeoAzur SAC files).  Assuming
-% identified.txt is formatted such that:
-% (1) SAC filename is first column,
-% (2) event ID is last column,
-% (3) every line is formatted identically,
-% (4) the column separator is a space,
-% this method of arbitrary reading should be robust.
-% See evt2text.m for details of 'textfile' write.
-textfile = fullfile(revdir, 'reviewed', 'identified', 'txt', 'identified.txt');
-textlines = readtext(textfile);
-columnsep = strfind(textlines{1}, ' ');
-
-% Don't add +1 to columnsep(end) here because that +1 might include
-% an asterisk "*", indicating possible multiple events
-eventid_index = [columnsep(end):length(textlines{1})];
-
-% Find the lines in identified.txt which include that event
-% identification number.
-id = num2str(id);
-num_matches = 0;
-for i = 1:length(textlines)
-    eventid{i} = strtrim(textlines{i}(eventid_index));
-    if strcmp(eventid{i}, id)
-        num_matches = num_matches + 1;
-        sac{num_matches} = strtrim(textlines{i}(1:columnsep(1)));
-
-        if strcmp(eventid{i}(1), '*')
-            warning(['%s may be contain signals from multiple ' ...
-                     'earthquakes'], sac{i})
-
-        end
-    end
-end
-if ~num_matches
-    error('No matching event id: %s', id)
-
-end
+% Find all the SAC files that match this event ID.
+sac = getsac(id, evtdir);
 
 % Generate figure window.
 F.f = figure;
@@ -123,10 +88,9 @@ for i = 1:length(sac)
 
     % Retrieve the event data associated with that SAC file, as
     % found through sac2evt.m
-    EQ{i} = getevt(fullpath_sac{i}, revdir);
+    EQ{i} = getevt(fullpath_sac{i}, evtdir);
 
     % Parse the event info.
-    evtdate = EQ{i}(1).TaupTimes(1).arrivaldatetime;
     dist(i) = EQ{i}(1).TaupTimes(1).distance;
     phase_cell = [phase_cell {EQ{i}(1).TaupTimes.phaseName}];
 
@@ -154,28 +118,22 @@ for i = 1:length(sac)
     % Generate an x-axis.
     xax{i} = xaxis(length(x{i}), h{i}.DELTA, pt0);
 
-    % Filter, possibly.
+    % Taper and filter.
     if ~isnan(lohi)
-        x{i} = bandpass(x{i}, 1/h{i}.DELTA, lohi(1), lohi(2));
+        taper = hanning(length(x{i}));
+        x{i} = bandpass(taper .* x{i}, 1/h{i}.DELTA, lohi(1), lohi(2));
 
-        % Bandpassing leaves some edge artifacts. I could taper or otherwise
-        % preprocess the time series but given that this script is to
-        % display, and not otherwise analyze the data, I will simply
-        % remove some offending-samples from the start and end of the
-        % seismogram.  Do this before normalizing so the large
-        % spurious signals are removed.
-        x{i}(1:100) = NaN;
-        x{i}(end-100:end) = NaN;
-        
     end
 
 end
+
 % This is the max amplitude across all seismograms (i.e., likely the
 % one with the shortest epicentral distance, ignoring propagation
 % patterns etc.) and may be used below to normalize but maintain
 % distance decay.
 maxx = max(cellfun(@(xx) max(abs(xx)), x));
 
+% Normalize the traces and annotate with float numbers.
 for i = 1:length(x)
     if normlize
         % Normalize this seismogram with itself, thereby removing distance
@@ -196,7 +154,7 @@ for i = 1:length(x)
     floatnum = floatnum(1:2);
 
     F.pltr(i) = plot(xax{i}, ampfac * x{i} + dist(i));
-    F.pltx(i) = text(min(xax{i}), dist(i), num2str(floatnum));
+    F.pltx(i) = text(0, dist(i), num2str(floatnum));
     F.pltx(i).Color = F.pltr(i).Color;
 
 end
@@ -207,33 +165,31 @@ grid(F.ax, 'on')
 % Parse event info.  Event time is the same for all because it is the
 % same event.
 EQ1 = EQ{1}(1);
-evtdate = datetime(EQ1.PreferredTime, 'InputFormat', ['uuuu-MM-dd ' ...
-                    'HH:mm:ss.SSS'], 'TimeZone', 'UTC');
-magstr = sprintf('M%2.1f %s', EQ1.PreferredMagnitudeValue, ...
-                 EQ1.PreferredMagnitudeType);
+evttime = EQ1.PreferredTime;
+magtype = [upper(EQ1.PreferredMagnitudeType(1)) ...
+           lower(EQ1.PreferredMagnitudeType(2:end))];
+magstr = sprintf('M%2.1f %s', EQ1.PreferredMagnitudeValue, magtype);
 depthstr = sprintf('%2.1f km depth', EQ1.PreferredDepth);
 locstr = sprintf('%s', EQ1.FlinnEngdahlRegionName);
+F.tl = title([magstr ' ' locstr ' at ' depthstr]);
 
-% Add title and labels.
+% Add labels.
 hold(F.ax, 'on')
 current_xlim = get(F.ax, 'XLim');
 current_ylim = get(F.ax, 'YLim');
 if strcmp(alignon, 'atime')
-    % Title and labels specific to aligning on first-arrival.
-    F.tl = title(sprintf('%s UTC %s', datestr(evtdate), EQ1.FlinnEngdahlRegionName));
-    F.magtx = text(F.tl.Position(1), F.tl.Position(2), F.ax, ...
-                   sprintf('M%2.1f %s at %2.1f km depth', ...
-                           EQ1.PreferredMagnitudeValue, ...
-                           EQ1.PreferredMagnitudeType, EQ1.PreferredDepth));
-    F.xl = xlabel('time relative to theoretical first arrival (s)');
+    % XLabel specific to aligning on first-arrival.
+    ph = EQ1.TaupTimes(1).phaseName;
+    
+    F.xl = xlabel(sprintf(['time relative to \\textit{%s}-phase ' ...
+                        '(s)\n[origin: %s UTC]'], ph, evttime));
+
     warning(['Theoretical first arrival may not be the same phase ' ...
              'or phase branch across different seismograms'])
 
-    F.magtx.HorizontalAlignment = 'center';
-
     % Add vertical line at 0 seconds.
     F.vl = plot(F.ax, [0 0], get(F.ax, 'XLim'), 'k');
-    bottom(F.vl)
+    botz(F.vl);
 
 else
     % Compute travel time curves for the phases present.
@@ -248,11 +204,10 @@ else
         
     end
     
-    % Title and labels specific to aligning on event-rupture time.
-    F.tl = title([magstr ' ' locstr ' at ' depthstr]);
-    timstr = sprintf('%s UTC', EQ1.PreferredTime(1:19));
-    F.xl = xlabel(sprintf('time since %s (s)', timstr));
-    F.lg = legend(F.ph, phase_cell, 'AutoUpdate', 'off');
+    % XLabel specific to aligning on event-rupture time.
+    F.xl = xlabel(sprintf('time relative to %s UTC (s)', evttime));
+    F.lg = legend(F.ph, phase_cell, 'AutoUpdate', 'off', 'Location', ...
+                  'NorthWest', 'FontSize', F.xl.FontSize);
 
     % Send travel time curves to the bottom.
     botz(F.ph, F.ax);
@@ -262,17 +217,27 @@ hold(F.ax, 'off')
 set(F.ax, 'XLim', current_xlim);
 set(F.ax, 'YLim', current_ylim);
 
-% Annotate figure with bandpass corner frequencies and event id number.
+% Annotate figure with bandpass corner frequencies.
 if ~isnan(lohi)
-    [F.bhul, F.thul] = boxtexb('ul', F.ax, sprintf('%.2f~-~%.2f Hz', lohi), F.xl.FontSize);
-    F.bhul.Visible = 'off';
+    [F.lghz, F.txhz] = textpatch(F.ax, 'SouthEast', sprintf(['%.2f~-~%.2f ' ...
+                        'Hz'], lohi), F.xl.FontSize, 'Times', true);
 
 end
-[F.bhlr, F.thlr] = boxtexb('lr', F.ax, sprintf('%s', id), F.xl.FontSize);
-F.bhlr.Visible = 'off';
 
-% Final cosmetics.
+% Cosmetics.
 F.yl = ylabel(F.ax, 'distance ($^{\circ}$)');
 F.tl.FontWeight = 'normal';
 latimes
 
+% Shift and stagger the float numbers.
+rangex = range(F.ax.XLim);
+shiftx = rangex * 0.025;
+for i = 1:length(F.pltx)
+    if mod(i, 2) ~= 0
+        F.pltx(i).Position(1) = xax{i}(1) - (1.75 * shiftx); % 1.75 to account for width of number.
+
+    else
+        F.pltx(i).Position(1) = xax{i}(end) + shiftx;
+
+    end
+end
