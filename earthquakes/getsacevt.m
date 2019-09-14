48254d42143d4f98d39aeebaf821f86748bafbc0
Joel D. Simon <jdsimon@princeton.edu>
2019-09-14 17:35:25 -0400

    Add path to SAC file directory as input

--- earthquakes/getsacevt.m
+++ earthquakes/getsacevt.m
@@ -1,5 +1,5 @@
-function [sac, EQ] = getsacevt(id, evtdir)
-% [sac, EQ] = GETSACEVT(id, evtdir)
+function [sac, EQ] = getsacevt(id, evtdir, sacdir)
+% [sac, EQ] = GETSACEVT(id, evtdir, sacdir)
 %
 % GETSACEVT combines getsac.m and getevet.m to return the list of SAC
 % files and reviewed EQ structures corresponding to an event ID.
@@ -9,6 +9,8 @@ function [sac, EQ] = getsacevt(id, evtdir)
 %               column of identified.txt(def: 10948555)
 % evtdir    Path to directory containing 'raw/' and 'reviewed' 
 %               subdirectories (def: $MERMAID/events/)
+% sacdir    Path to directory to be (recursively) searched for
+%               SAC files (def: $MERMAID/processed/)
 %
 % Output:
 % sac       Cell array of SAC files
@@ -23,9 +25,10 @@ function [sac, EQ] = getsacevt(id, evtdir)
 % Defaults.
 defval('id', 10948555)
 defval('evtdir', fullfile(getenv('MERMAID'), 'events'))
+defval('sacdir', fullfile(getenv('MERMAID'), 'processed'))
 
 % This function is just simple wrapper.
-sac = getsac(id, evtdir);
+sac = getsac(id, evtdir, sacdir);
 for i = 1:length(sac)
     % Must be cell in case multiple EQ's corresponding to one SAC file.
     EQ{i}  = getevt(sac{i}, evtdir);
