96a3a897f0650c60db2f6cba3582a009926c10ba
Joel D. Simon <jdsimon@princeton.edu>
2019-09-25 11:53:14 -0400

    Add option to return only triggered or requested data

--- earthquakes/fullsac.m
+++ earthquakes/fullsac.m
@@ -1,5 +1,5 @@
-function s = fullsac(singl, diro)
-% s = FULLSAC(singl, diro)
+function s = fullsac(singl, diro, returntype)
+% s = FULLSAC(singl, diro, returntype)
 %
 % FULLSAC returns a cell array of full-path filenames of all SAC files
 % in "diro" if "singl" is left empty (default) (Ex1)
@@ -11,12 +11,16 @@ function s = fullsac(singl, diro)
 % to '20180811T094852.09_5B6F01F6.MER.DET.WLT5.sac' (Ex2)
 %
 % Input:
-% singl        Single SAC file, possibly with incomplete path, 
+% singl        Single SAC file, possibly with incomplete path,
 %                  to which you wish to have the complete path returned.
 %                  Leave empty to return all SAC files in diro (def: [])
 % diro         Directory which contains (or whose subdirectories contain)
 %                  the SAC files of interest (def: $MERMAID/processed/)
-%                 
+% returntype   For third-generation+ MERMAID only:
+%              'ALL': both triggered and user-requested SAC files (def)
+%              'DET': triggered SAC files as determined by onboard algorithm
+%              'REQ': user-requested SAC files%
+%
 % Output:
 % s            Full-path SAC file(s) names, either:
 %                 A cell array if "singl" is empty (Ex. 1)
@@ -26,17 +30,18 @@ function s = fullsac(singl, diro)
 %    s = FULLSAC([], fullfile(getenv('MERMAID'), 'processed'))
 %
 % Ex2: (return the complete path to a single SAC file)
-%    s = FULLSAC('20180811T094852.09_5B6F01F6.MER.DET.WLT5.sac') 
+%    s = FULLSAC('20180811T094852.09_5B6F01F6.MER.DET.WLT5.sac')
 %
 % See also: mermaid_sacf.m
 %
 % Author: Joel D. Simon
 % Contact: jdsimon@princeton.edu
-% Last modified: 01-Sep-2018, Version 2017b
+% Last modified: 25-Sep-2019, Version 2017b on GLNXA64
 
 % Default is to return all SAC filenames in $MERMAID/processed.
 defval('singl', [])
 defval('diro', fullfile(getenv('MERMAID'), 'processed'))
+defval('returntype', 'ALL')
 
 % Sanity.
 assert(isempty(singl) || issac(singl), ['Input ''singl'' must be ' ...
@@ -54,9 +59,11 @@ if isempty(singl)
     d = dir(fullfile(diro, '**/*sac'));
     if ~isempty(d)
         s = fullfile({d.folder}, {d.name});
+
     else
         s = [];
         warning('No .sac files found in %s', diro)
+
     end
     s = s(:);
 
@@ -64,11 +71,42 @@ else
     % Nab a single SAC file.
     d = dir(fullfile(diro, sprintf('**/%s', strippath(strtrim(singl)))));
     if ~isempty(d)
-        s = fullfile(d.folder, d.name);     
+        s = fullfile(d.folder, d.name);
+
     else
         s = [];
         warning('%s was not found in %s or any of its subfolders.', ...
                 singl, diro)
+        return
+
     end
-    
 end
+
+% Separate 'DET' (triggered) and 'REQ' (requested) data for MERMAID.
+switch upper(returntype)
+  case 'DET'
+    idx = cellstrfind(s, 'MER.DET.*.sac');
+    if isempty(idx)
+        warning('No tiggered (''DET'') SAC files found')
+        s = [];
+        return
+
+    end
+
+  case 'REQ'
+    idx = cellstrfind(s, 'MER.REQ.*.sac');
+    if isempty(idx)
+        warning('No requested (''REQ'') SAC files found')
+        s = [];
+        return
+
+    end
+
+  case 'ALL'
+    idx = [1:length(s)];
+
+  otherwise
+    error('Specify one of ''ALL'', ''DET'', or ''REQ'' for input: returntype')
+
+end
+s = s(idx);
--- earthquakes/revsac.m
+++ earthquakes/revsac.m
@@ -1,7 +1,7 @@
-function [sac, evt] = revsac(iup, sacdir, evtdir)
-% [sac, evt] = REVSAC(iup, sacdir, evtdir)
+function [sac, evt] = revsac(iup, sacdir, evtdir, returntype)
+% [sac, evt] = REVSAC(iup, sacdir, evtdir, returntype)
 %
-% REVSAC returns a list of fullpath .sac and .evt filenames 
+% REVSAC returns a list of fullpath .sac and .evt filenames
 % whose event information has been reviewed and sorted with
 % reviewevt.m into either:
 %
@@ -10,21 +10,24 @@ function [sac, evt] = revsac(iup, sacdir, evtdir)
 % [evtdir]/reviewed/purgatory/evt.
 %
 % Input:
-% iup       [evtdir]/reviewed/ directory (def: 1)
-%           1 identified
-%          -1 unidentified
-%           0 purgatory
-% sacdir    Directory where .sac files are kept
-%                def($MERMAID/processed)
-% evtdir    Path to directory containing 'raw/' and 'reviewed'
-%               subdirectories (def: $MERMAID/events/)
-%
+% iup          [evtdir]/reviewed/ directory (def: 1)
+%              1: identified
+%             -1: unidentified
+%              0: purgatory
+% sacdir       Directory where .sac files are kept
+%                  def($MERMAID/processed)
+% evtdir       Path to directory containing 'raw/' and 'reviewed'
+%                  subdirectories (def: $MERMAID/events/)
+% returntype   For third-generation+ MERMAID only:
+%              'ALL': both triggered and user-requested SAC files (def)
+%              'DET': triggered SAC files as determined by onboard algorithm
+%              'REQ': user-requested SAC files%
 % Output:
-% sac       Cell array of SAC filenames whose corresponding 
-%              .evt file is in the reviewed subdirectory of interest
-% evt        Cell array of .evt filenames whose corresponding 
-%              in the reviewed subdirectory of interest
-%           
+% sac          Cell array of SAC filenames whose corresponding
+%                  .evt file is in the reviewed subdirectory of interest
+% evt          Cell array of .evt filenames whose corresponding
+%                  in the reviewed subdirectory of interest
+%
 % Before running the example below run the example in reviewevt.m
 %
 % Ex: (load EQ data from an identified SAC file)
@@ -38,12 +41,13 @@ function [sac, evt] = revsac(iup, sacdir, evtdir)
 %
 % Author: Joel D. Simon
 % Contact: jdsimon@princeton.edu
-% Last modified: 24-Oct-2018, Version 2017b
+% Last modified: 25-Sep-2019, Version 2017b on GLNXA64
 
 % Defaults.
 defval('iup', 1)
 defval('sacdir', fullfile(getenv('MERMAID'), 'processed'))
 defval('evtdir', fullfile(getenv('MERMAID'), 'events'))
+defval('returntype', 'ALL')
 
 sac = [];
 evt = [];
@@ -52,10 +56,10 @@ evt = [];
 switch iup
   case 1
     status = 'identified';
-    
+
   case -1
     status = 'unidentified';
-    
+
   case 0
     status = 'purgatory';
 
@@ -64,25 +68,56 @@ switch iup
 
 end
 
-% Find the relevant paths. 
+% Find the relevant paths.
 evt_path = fullfile(evtdir, 'reviewed', status, 'evt');
 evtdir = dir([evt_path '/*.evt']);
 if isempty(evtdir)
     warning('No .evt files found in %s', evt_path)
+    return
 
 end
 
+% Concatenate the .evt paths.
 for i = 1:length(evtdir)
     evt{i} = fullfile(evtdir(i).folder, evtdir(i).name);
 
 end
+
+% Keep just those events in of the requested return type.
+switch upper(returntype)
+  case 'DET'
+    idx = cellstrfind(evt, 'MER.DET.*.evt');
+    if isempty(idx)
+        warning('No tiggered (''DET'') .evt files found')
+        evt = [];
+        return
+
+    end
+
+  case 'REQ'
+    idx = cellstrfind(evt, 'MER.REQ.*.evt');
+    if isempty(idx)
+        warning('No requested (''REQ'') .evt files found')
+        evt = [];
+        return
+
+    end
+
+  case 'ALL'
+    idx = [1:length(evt)];
+
+  otherwise
+    error('Specify one of ''ALL'', ''DET'', or ''REQ'' for input: returntype')
+
+end
+evt = evt(idx);
 
 % Replace the filename extension.
 sac = strrep({evtdir.name}, '.evt', '.sac');
 
 % To find the fullpath SAC filenames: start with all SAC fullpath
 % filenames.
-allsac = fullsac([], sacdir);
+allsac = fullsac([], sacdir, returntype);
 if isempty(allsac)
     sac = []; % No warning necessary here as it will be thrown in fullsac.m
     return
@@ -94,5 +129,5 @@ end
 allsac_nopath = cellfun(@(xx) strippath(xx), allsac, 'UniformOutput', false);
 [~, ~, idx] = intersect(sac, allsac_nopath);
 sac = allsac(idx);
-
-
+sac = sac(:);
+evt = evt(:);
