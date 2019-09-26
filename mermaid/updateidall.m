7a3b289e640e4f6d82fc2d51ea203626f64b448f
Joel D. Simon <jdsimon@princeton.edu>
2019-09-26 11:04:31 -0400

    Update header

--- mermaid/updateidall.m
+++ mermaid/updateidall.m
@@ -1,9 +1,17 @@
-function [updated, failed] = updateidall
+function [updated, failed] = updateidall(force)
+% [updated, failed] = UPDATEIDALL
+%
+% Updates every MERMAID and 'nearby' .evt file associated with every
+% identified event, using updateid.m
+%
+% Pulls event IDs from the last column of 'identified.txt', written
+% with evt2txt.m, assuming JDS system defaults.
 %
 % Author: Joel D. Simon
 % Contact: jdsimon@princeton.edu
-% Last modified: 03-Sep-2019, Version 2017b
+% Last modified: 26-Sep-2019, Version 2017b on GLNXA64
 
+defval('force', false)
 defval('filename', fullfile(getenv('MERMAID'), 'events', 'reviewed', ...
                             'identified', 'txt', 'identified.txt'))
 defval('txtfile', fullfile(getenv('MERMAID'), 'events', 'nearbystations', 'nearbystations.txt'))
@@ -24,9 +32,11 @@ for i = 1:length(star_idx)
 end
 id = unique(id);
 
+attempted = 0;
 updated = {};
 failed = {};
 for i = 1:length(id)
+    attempted = attempted + 1;
     try
         rev_evt = updateid(id{i}, false);
         if ~isempty(rev_evt)
@@ -34,8 +44,13 @@ for i = 1:length(id)
 
         end
     catch ME
-        % keyboard
+        keyboard
         failed = [failed ; id{i}];
 
     end
 end
+
+fprintf('Total events:      %4i\n', length(id))
+fprintf('Events attempted:  %4i\n', attempted)
+fprintf('Events updated:    %4i\n', length(updated))
+fprintf('Events failed:     %4i\n', length(failed))
