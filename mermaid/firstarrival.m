69eeaa8965062982390f75daf6100c5a1bbf0049
Joel D. Simon <jdsimon@princeton.edu>
2019-10-02 16:17:47 -0400

    Add error if bandpass exceeds Nyquist & return windows' completion sentinel value
    The latter alerts user if either/both of the two time windows are
    incomplete

--- mermaid/firstarrival.m
+++ mermaid/firstarrival.m
@@ -45,8 +45,10 @@ function [tres, dat, syn, ph, delay, twosd, xw1, xaxw1, maxc_x, maxc_y, ...
 %% W1
 %% xw2
 %% W2
-% incomplete true if requested time window extends beyond edge of
-%            input time series (def: false)
+% incomplete 0: Both time windows complete
+%            1: W1 incomplete, W2 complete
+%            2: W1 complete, W2 incomplete
+%            3: Both time windows incomplete
 %
 % *The x-axis here is w.r.t to original, NOT windowed, seismogram,
 % i.e. xaxis(h.NPTS, h.DELTA, h.B), where h is the SAC header
@@ -56,7 +58,7 @@ function [tres, dat, syn, ph, delay, twosd, xw1, xaxw1, maxc_x, maxc_y, ...
 %
 % Author: Joel D. Simon
 % Contact: jdsimon@princeton.edu
-% Last modified: 01-Oct-2019, Version 2017b on MACI64
+% Last modified: 02-Oct-2019, Version 2017b on GLNXA64
 
 defval('s', '20180629T170731.06_5B3F1904.MER.DET.WLT5.sac')
 defval('ci', true)
@@ -65,6 +67,10 @@ defval('lohi', [1 5])
 defval('sacdir', fullfile(getenv('MERMAID'), 'processed'))
 defval('evtdir', fullfile(getenv('MERMAID'), 'events'))
 defval('EQ', [])
+
+% Start with baseline assumption both time windows will be complete.
+incomplete1 = false;
+incomplete2 = false;
 
 % Read data and retrieve event structure.  EQ contains the theoretical
 % arrival times of all phases included in the time window, computed
@@ -114,15 +120,21 @@ ph = EQ(1).TaupTimes(1).phaseName;
 
 % Bandpass filter the time series and select a windowed segment.
 if ~isnan(lohi)
-    x = detrend(x, 'linear');
+    if round(1/h.DELTA) <= 2*lohi(2)
+         error('Upper cutoff frequency >= 1/2 the sampling frequency')
+
+     end
+
     taper = hanning(length(x));
-    xf = bandpass(taper .* x, 1/h.DELTA, lohi(1), lohi(2));
+    x = detrend(x, 'linear');
+    x = taper .* x;
+    xf = bandpass(x, 1/h.DELTA, lohi(1), lohi(2));
 
 else
     xf = x;
 
 end
-[xw1, W1, incomplete] = timewindow(xf, wlen, EQ(1).TaupTimes(1).truearsecs, ...
+[xw1, W1, incomplete1] = timewindow(xf, wlen, EQ(1).TaupTimes(1).truearsecs, ...
                                    'middle', h.DELTA, h.B);
 
 % The offset x-axis, which sets syn at 0 s.
@@ -156,7 +168,8 @@ if SNR > 1
 
     % Maximum absolute amplitude (counts) considering a window of length
     % wlen/2 starting at the actual phase arrival.
-    [xw2, W2, incomplete] = timewindow(xf, wlen/2, dat, 'first', h.DELTA, h.B);
+    [xw2, W2, incomplete2] = timewindow(xf, wlen/2, dat, 'first', h.DELTA, h.B);
+
     maxy = max(xw2);
     miny = min(xw2);
     if maxy > abs(miny)
@@ -198,5 +211,23 @@ else
     maxc_x = NaN;
     delay = NaN;
     twosd = NaN;
+
+end
+
+% Determine windows' completion classification.
+if ~incomplete1 && ~incomplete2
+    incomplete = 0;
+
+end
+if incomplete1
+    incomplete = 1;
+
+end
+if incomplete2
+    incomplete = 2;
+
+end
+if incomplete1 && incomplete2
+    incomplete = 3;
 
 end
