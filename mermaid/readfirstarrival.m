a8771afa0511bb89817f65bea9153de5d47b6b82
Joel D. Simon <jdsimon@princeton.edu>
2019-10-02 16:22:43 -0400

    Update to handle expanded firstarrival.m I/0

--- mermaid/readfirstarrival.m
+++ mermaid/readfirstarrival.m
@@ -1,24 +1,27 @@
-function varargout = readfirstarrival(filename)
-% [s, ph, tres, delay, twosd, SNR, maxc_y] = READFIRSTARRIVAL(filename)
+function varargout = readfirstarrival(filename, fmt)
+% [s, ph, tres, delay, twosd, maxc_y, SNR, ID, incomplete] = ...
+%     READFIRSTARRIVAL(filename, fmt)
 %
 % Reads and parses output text file of firstarrivals.m
 % See there for I/0/
 %
 % Author: Joel D. Simon
 % Contact: jdsimon@princeton.edu
-% Last modified: 09-Aug-2019, Version 2017b
+% Last modified: 02-Oct-2019, Version 2017b on GLNXA64
 
 % Default.
 defval('filename', fullfile(getenv('MERMAID'), 'events', 'reviewed', ...
                             'identified', 'txt', 'firstarrivals.txt'))
 % Data format.
-fmt = ['%44s    ' , ...
-       '%5s    '  , ...
-       '%6.2f    ', ...
-       '%6.2f    ', ...
-       '%5.2f    ', ...
-       '%9.1f    ' , ...
-       '%d\n'];
+defval('fmt', ['%44s    ' , ...
+               '%5s    ' ,  ...
+               '%6.2f   ' , ...
+               '%6.2f   ' , ...
+               '%5.2f   ' , ...
+               '%19.12f    ' , ...
+               '%18.12f    '  , ...
+               '%8s    ' , ...
+               '%u\n'])
 
 % Read.
 fid = fopen(filename, 'r');
@@ -26,14 +29,16 @@ lynes = textscan(fid, fmt);
 fclose(fid);
 
 % Parse.
-s = lynes{1};
+s = strtrim(lynes{1});
 ph = lynes{2};
 tres = lynes{3};
 delay = lynes{4};
 twosd = lynes{5};
-SNR = lynes{6};
-maxc_y = double(lynes{7});
+maxc_y = double(lynes{6});
+SNR = lynes{7};
+ID = lynes{8};
+incomplete = lynes{9};
 
 % Collect.
-outargs = {s, ph, tres, delay, twosd, SNR, maxc_y};
+outargs = {s, ph, tres, delay, twosd, maxc_y, SNR, ID, incomplete};
 varargout = outargs(1:nargout);
