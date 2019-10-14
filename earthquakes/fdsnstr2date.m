628e6c4c20bc1d655933462744786ecdc87c5ed4
Joel D. Simon <jdsimon@princeton.edu>
2019-10-14 12:28:04 -0400

    Add recursion to process cell arrays

--- earthquakes/fdsnstr2date.m
+++ earthquakes/fdsnstr2date.m
@@ -8,18 +8,18 @@ function tdate = fdsnstr2date(tstr)
 %               2019-03-25T14:32:08.191
 %
 % to a datetime object in the UTC timezone.
-% 
+%
 % See http://www.fdsn.org/webservices/FDSN-WS-Specifications-1.1.pdf
 % pg. 6, 'Time parameter values'.
 %
 % Ignores any fractional seconds beyond 3 digits (milliseconds).
-% 
+%
 % Input:
 % tstr        Time string formatted per FDSN specification.
-% 
+%
 % Output:
 % tdate       Datetime object
-%             
+%
 % Ex: (datetime and datestr of reported event time)
 %    [~, h] = readsac('m35.20140915T080858.sac');
 %    [~, ~, ~, ~, tdate1] = seistime(h)
@@ -31,11 +31,26 @@ function tdate = fdsnstr2date(tstr)
 %
 % Author: Joel D. Simon
 % Contact: jdsimon@princeton.edu
-% Last modified: 17-Mar-2019, Version 2017b
+% Last modified: 08-Oct-2019, Version 2017b on GLNXA64
+
+%% Recursive.
+
+if iscell(tstr)
+    for i = 1:length(tstr)
+
+        %% Recursive.
+
+        tdate(i) = fdsnstr2date(tstr{i});
+
+    end
+    tdate = reshape(tdate, size(tstr));
+    return
+
+end
 
 % Remove the "T" at the 11th index.
 tstr(11) = [];
 
 % https://www.mathworks.com/help/matlab/ref/datetime.html?searchHighlight=datetime&s_tid=doc_srchtitle#buhzxmk-1-Format
 fmt = 'uuuu-MM-ddHH:mm:ss.SSS';
-tdate = datetime(tstr, 'InputFormat', fmt, 'TimeZone', 'UTC')
+tdate = datetime(tstr, 'InputFormat', fmt, 'TimeZone', 'UTC');
