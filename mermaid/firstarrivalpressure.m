2aa2a89f4751c0cd40956050bcff597dd725d90c
Joel D. Simon <jdsimon@princeton.edu>
2019-10-29 14:01:27 -0400

    Add handling for NaN cases

--- mermaid/firstarrivalpressure.m
+++ mermaid/firstarrivalpressure.m
@@ -1,6 +1,6 @@
-function [RMS, P, maxc_y, delay, xw3, W3, xw2, W2, EQ, incomplete] = ...
-    firstarrivalpressure(s, wlen, lohi, sacdir, evtdir, EQ, bathy)
-% [RMS, P, maxc_y, delay, xw3, W3, xw2, W2, EQ, incomplete] = ...
+function [RMS, ph, P, maxc_y, delay, xw3, W3, xw2, W2, EQ, incomplete] = ...
+        firstarrivalpressure(s, wlen, lohi, sacdir, evtdir, EQ, bathy)
+% [RMS, ph, P, maxc_y, delay, xw3, W3, xw2, W2, EQ, incomplete] = ...
 %       FIRSTARRIVALPRESSURE(s, wlen, lohi, sacdir, evtdir, EQ, bathy)
 %
 % Extension of firstarrival.m (which deals with travel time residuals)
@@ -25,6 +25,7 @@ function [RMS, P, maxc_y, delay, xw3, W3, xw2, W2, EQ, incomplete] = ...
 %
 % Output:
 % RMS      Root mean squared value of xw3: arrival to max. amplitude of wavetrain
+% ph       Phase name associated with RMS
 % P        Theoretical pressure of first arrival computed with reid.m [Pa]
 % maxc_y   Amplitude (counts) of maximum (or minimum) amplitude of bandpassed
 % delay    Time delay between true arrival time and time at largest
@@ -37,7 +38,7 @@ function [RMS, P, maxc_y, delay, xw3, W3, xw2, W2, EQ, incomplete] = ...
 % W2       timewindow.m struct of length wlen/2 [s]
 %              beginning at dat
 % EQ       EQ structure, either input or retrieved with getevt.m
-% incomplete Flags from firstarrival.m (N.B., W3 cannot be incomplete)
+% incomplete Flags from firstarrival.m (N.B., W3 is always complete)
 %            0: Both time windows complete
 %            1: W1 incomplete, W2 complete
 %            2: W1 complete, W2 incomplete
@@ -45,7 +46,7 @@ function [RMS, P, maxc_y, delay, xw3, W3, xw2, W2, EQ, incomplete] = ...
 %
 % Author: Joel D. Simon
 % Contact: jdsimon@princeton.edu
-% Last modified: 26-Oct-2019, Version 2017b on GLNXA64
+% Last modified: 29-Oct-2019, Version 2017b on GLNXA64
 
 % Defaults.
 defval('s', '20180819T042909.08_5B7A4C26.MER.DET.WLT5.sac')
@@ -57,7 +58,7 @@ defval('EQ', [])
 defval('bathy', true)
 
 % Run firstarrival to retrieve relevant statistics about the first-arriving phase.
-[~, ~, ~, ~, delay, ~, ~, ~, ~, maxc_y, ~, EQ, ~, xw2, W2, incomplete] = ...
+[~, ~, ~, ph, delay, ~, ~, ~, ~, maxc_y, ~, EQ, ~, xw2, W2, incomplete] = ...
     firstarrival(s, false, wlen, lohi, sacdir, evtdir, EQ, bathy);
 
 % Find the new RMS time window which brackets just the time starting
@@ -66,13 +67,22 @@ defval('bathy', true)
 % wavetrain. Make time window in reference to W2.
 
 % Do not need to be worried about it being incomplete because that was
-% checked in firstarrival.m and we know the delay will be at max as
+% checked in firstarrival.m and we know the delay will be at most as
 % long as the input time series.
-[xw3, W3] = timewindow(xw2, delay, W2.xlsecs, 'first', W2.delta, W2.xlsecs);
+if ~isnan(delay)
+    [xw3, W3] = timewindow(xw2, delay, W2.xlsecs, 'first', W2.delta, W2.xlsecs);
 
-% Compute the RMS
-RMS = rms(xw3);
+    % Compute the RMS
+    RMS = rms(xw3);
 
-% Retrieve the theoretical pressure (Pa) of the first-arriving phase,
-% if it exists (requires an 'Mb' or 'Ml' magnitude type; see sac2evt.m).
-P = EQ(1).TaupTimes.pressure;
+    % Retrieve the theoretical pressure (Pa) of the first-arriving phase,
+    % if it exists (requires an 'Mb' or 'Ml' magnitude type; see sac2evt.m).
+    P = EQ(1).TaupTimes.pressure;
+
+else
+    xw3 = NaN;
+    W3 = NaN;
+    RMS = NaN;
+    P = NaN;
+
+end
