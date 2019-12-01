ed6212ecd2309858b7193e57caf76faed54cf012
Joel D. Simon <jdsimon@princeton.edu>
2019-12-01 10:44:48 -0500

    Add option to control whether labels are returned inside or between discrete colors

--- plotbits/x2color.m
+++ plotbits/x2color.m
@@ -1,9 +1,9 @@
-function [col, cbticks, cbticklabels, cmap, idx] = x2color(x, xmin, xmax, cmap)
-% [col, cbticks, cbticklabels, cmap, idx] = X2COLOR(x, xmin, xmax, cmap)
+function [col, cbticks, cbticklabels, cmap, idx] = x2color(x, xmin, xmax, cmap, within)
+% [col, cbticks, cbticklabels, cmap, idx] = X2COLOR(x, xmin, xmax, within)
 %
-% Linearly maps data vector to RGB colormap.
+% Linearly map data vector to an RGB colormap.
 %
-% A colormap is a 3 x n matrix where cmap(1, :) maps to the lowest
+% A colormap is an M x 3 matrix where cmap(1, :) maps to the lowest
 % value in x, and cmap(end, :) maps to the highest value in x.  This
 % function distributes (as best as possible) all values linearly
 % between those minimum- and maximum-intensity RGB triplets.
@@ -15,16 +15,18 @@ function [col, cbticks, cbticklabels, cmap, idx] = x2color(x, xmin, xmax, cmap)
 % xmax      Inclusive x value of upper limit of cmap
 %               (def: max(x))
 % cmap      Colormap (def: jet(64))
+% within    true: cbticks/labels fall within discrete color intervals,
+%                 length(cbticks/labels) = length(cmap) (see Ex3)
+%           false: cbticks/labels fall at the edges of discrete colors,
+%                 length(cbticks/labels) = length(cmap)+1 (def)
 %
 % Output:
-% col          RGB value of x
-% cbticks      Every possible discrete colorbar tick spanning
-%                  entire colormap (linspace(0,1,length(cmap)+1)
-% cbticklabels Cell of chars of every possible discrete colorbar
-%                  tick label which maps x to cbticks
+% col           RGB value of x
+% cbticks       length(col)+1 tick mark locations
+% cbticklabels  length(col)+1 tick mark labels
 %
 % cmap         Colormap used
-% idx          Index (row) of coloramp: col = cmap(idx, :)
+% idx          Index (row) of colormap: col = cmap(idx, :)
 %
 % Ex1: (recreate scatter.m automatic color-scaling)
 %    x = randn(25, 1);
@@ -45,14 +47,28 @@ function [col, cbticks, cbticklabels, cmap, idx] = x2color(x, xmin, xmax, cmap)
 %    cb.Ticks = cbticks(1:20:end);
 %    cb.TickLabels = cbticklabels(1:20:end);
 %
+% Ex3: (place tick labels inside or between discrete colors)
+%    x = 1:4;
+%    [col, cbticks1, cbticklabels1, cmap] = X2COLOR(x, [], [], hsv(length(x)), false);
+%    scatter(x, x, [], col, 'Filled')
+%    colormap(gca, cmap); axis tight; box on
+%    cb1 = colorbar;
+%    cb1.Ticks = cbticks1;
+%    cb1.TickLabels = cbticklabels1;
+%    [~, cbticks2, cbticklabels2] = X2COLOR(x, [], [], hsv(length(x)), true);
+%    cb2 = colorbar('SouthOutside');
+%    cb2.Ticks = cbticks2;
+%    cb2.TickLabels = cbticklabels2;
+%
 % Author: Joel D. Simon
 % Contact: jdsimon@princeton.edu
-% Last modified: 31-Oct-2019, Version 2017b on GLNXA64
+% Last modified: 30-Nov-2019, Version 2017b on GLNXA64
 
 % Defaults.
 defval('xmin', min(x))
 defval('xmax', max(x))
-defval('cmap', jet(64));
+defval('cmap', jet(64))
+defval('within', false)
 
 % Set all values below/above the intensity cutoffs to the min/max
 % requested saturation level.
@@ -73,4 +89,8 @@ col = cmap(idx, :);
 % Return the mapping for colorbar.m (whose ticks natively go from [0:1])
 % between ticks and ticklabels given the data.
 cbticks = linspace(0, 1, length(cmap)+1);
+if within
+    cbticks = cbticks(1:end-1) + diff(cbticks)/2;
+
+end
 cbticklabels = num2cell(norm2ab(cbticks, xmin, xmax));
