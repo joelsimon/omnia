fb117ad5e697d3674fde0cd4afc5e9f07f21c4f5
Joel D. Simon <jdsimon@princeton.edu>
2019-12-19 12:12:26 -0500

    Introduce function to write textfile of ray turning depths and locations

---
+++ mermaid/writeturndepth.m
@@ -0,0 +1,108 @@
+function writeturndepth(sac, evtdir, savefile)
+% WRITETURNDEPTH(sac, evtdir, savefile)
+%
+% Writes a textfile of turning depths and locations for the first
+% phase of the first event associated with each SAC file in the
+% input list.
+%
+% Input:
+% sac       Cell array of fullpath SAC filenames
+% evtdir    Path to directory containing 'raw/' and 'reviewed'
+%               subdirectories (def: $MERMAID/events/)
+% savefile  Filename of textfile to create
+%
+% Output:
+% Textfile with columns:
+% (1) SAC filename
+% (2) phase name
+% (3) turning depth (km)
+% (4) latitude at turning depth (decimal degrees)
+% (5) longitude at turning depth (decimal degrees)
+%
+% Author: Joel D. Simon
+% Contact: jdsimon@princeton.edu
+% Last modified: 19-Dec-2019, Version 2017b on MACI64
+
+% Default.
+defval('evtdir', fullfile(getenv('MERMAID'), 'events'))
+
+% Textfile format.
+fmt = ['%44s    ', ...
+       '%5s    ', ...
+       '%7.2f    ', ...
+       '%7.3f    ', ...
+       '%8.3f\n'];
+
+
+% If the file exists it is write-protected; lift that restriction.
+if exist(savefile, 'file') == 2
+    wstatus = fileattrib(savefile, '+w', 'a');
+    if wstatus == 0
+        error('Unable to allow write access to %s.', savefile)
+
+    end
+end
+
+% Open the save file without auto flushing; discard contents.
+fid = fopen(savefile, 'W');
+
+for i = 1:length(sac)
+    % Fetch event info associated with this SAC file.
+    EQ = getevt(sac{i}, evtdir);
+    if  ~isstruct(EQ) && ~isempty(EQ) && isnan(EQ)
+        error('No reviewed .evt file associated with %s', sac{i});
+
+    end
+
+    % Default output.
+    phasename = NaN;
+    turndepth = NaN;
+    turnlat = NaN;
+    turnlon = NaN;
+
+    % There is no turning depth for an upgoing phase (lowercase 'p').
+    if ~isempty(EQ) && ~isempty(EQ(1).TaupTimes) ...
+            && ~strcmp(EQ(1).TaupTimes(1).phaseName, 'p')
+
+        % Fetch SAC metadata.
+        [~, h] = readsac(sac{i});
+
+        % Compute turning depth for the first phase of the first event only.
+        tp = taupPierce(EQ(1).TaupTimes(1).model, ...
+                        EQ(1).TaupTimes(1).srcDepth, ...
+                        EQ(1).TaupTimes(1).phaseName, ...
+                        'sta', ....
+                        [h.STLA h.STLO], ...
+                        'evt', ...
+                        [EQ(1).PreferredLatitude EQ(1).PreferredLongitude], ...
+                        'turn');
+
+        % Find the index of the turning depth.
+        turn_idx = find(tp.pierce.depth);
+
+        % Parse outputs.
+        phasename = tp.phaseName;
+        turndepth = tp.pierce.depth(turn_idx);
+        turnlat = tp.pierce.latitude(turn_idx);
+        turnlon = tp.pierce.longitude(turn_idx);
+    end
+
+    % Collect the data.
+    data = {strippath(sac{i}), ...
+            phasename, ...
+            turndepth, ...
+            turnlat, ...
+            turnlon};
+
+    % Write the relevant line.
+    fprintf(fid, fmt, data{:});
+
+end
+
+% Close the file and restrict write-access.
+fclose(fid);
+wstatus = fileattrib(savefile, '-w', 'a');
+if wstatus == 0
+    error('Unable to restrict write access to %s.', savefile)
+
+end
