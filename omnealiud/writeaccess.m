ef10694f43d38fcbb42adef12a6cbed7870c2245
Joel D. Simon <jdsimon@princeton.edu>
2019-12-24 13:16:13 -0500

    Introduce function to manage write access to an input file

---
+++ omnealiud/writeaccess.m
@@ -0,0 +1,42 @@
+function writeaccess(lock, filename)
+% WRITEACCESS(lock, filename)
+%
+% Manage write access to an input file.
+%
+%
+% Input:
+% lock       'lock': restrict write-access (def)
+%            'unlock': allow write-access
+% filename    Filename
+%
+% Author: Joel D. Simon
+% Contact: jdsimon@princeton.edu
+% Last modified: 24-Dec-2019, Version 2017b on GLNXA64
+
+defval('lock', 'lock')
+
+if ~exist(filename, 'file') == 2
+    warning('filename %s does not exist', filename)
+    return
+
+end
+
+switch lower(lock)
+  case 'lock'
+    wstatus = fileattrib(filename, '-w', 'a');
+    if wstatus == 0
+        error('Unable to restrict write access to %s.', filename)
+
+    end
+
+  case 'unlock'
+    wstatus = fileattrib(filename, '+w', 'a');
+    if wstatus == 0
+        error('Unable to allow write access to %s.', filename)
+
+    end
+
+  otherwise
+    error('First input must be either ''lock'' or ''unlock''')
+
+end
