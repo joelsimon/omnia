6428263629e706a01c528e3c93342bf8997e56a1
Joel D. Simon <jdsimon@princeton.edu>
2020-01-30 13:21:14 -0500

    Clean up header

--- mermaid/nearbystations/nearbyworkflow.m
+++ mermaid/nearbystations/nearbyworkflow.m
@@ -1,7 +1,7 @@
 function nearbyworkflow
 % Fetches nearby traces, removes their instrument response, writes
 % their corresponding event files, and updates all nearby and MERMAID
-% event files assuming JDS system defaults
+% event files assuming JDS system defaults.
 %
 % Author: Joel D. Simon
 % Contact: jdsimon@princeton.edu
