c9d56830db535ae99a9baa5ac32d09c5c803fb16
Joel D. Simon <jdsimon@princeton.edu>
2020-01-31 14:53:23 -0500

    Introduce complement to getnearbysac.m, for CPPT data

---
+++ mermaid/cpptdata/getcpptsac.m
@@ -0,0 +1,33 @@
+function cppt_sac = getcpptsac(id, otype, cpptdir)
+% cppt_sac = GETCPPTSAC(id, otype, cpptdir)
+%
+% Return the SAC file(s) in [cpptdir]/sac/[id].
+%
+% Input:
+% id          IRIS public event identification number
+%                 (def: 10948555)
+% otype       CPPT SAC file output type
+%             []: (empty) return raw time series (def)
+%             'none': return displacement time series (nm)
+%             'vel': return velocity time series (nm/s)
+%             'acc': return acceleration time series (nm/s/s)
+% cpptdir     Path to directory containing CPPT stations
+%                 'sac/' and 'evt/' subdirectories
+%                 (def: $MERMAID/events/cpptstations/)
+%
+% Output:
+% cppt_sac   Cell array of SAC files from CPPT stations
+%
+% See also: getcpptsacevt.m
+%
+% Author: Joel D. Simon
+% Contact: jdsimon@princeton.edu
+% Last modified: 30-Jan-2020, Version 2017b on GLNXA64
+
+% Defaults.
+defval('id', '10932551')
+defval('otype', [])
+defval('cpptdir', fullfile(getenv('MERMAID'), 'events', 'cpptstations'))
+
+% This is just a wrapper for getnearbysac with the appropriate paths.
+cppt_sac = getnearbysac(id, otype, cpptdir);
