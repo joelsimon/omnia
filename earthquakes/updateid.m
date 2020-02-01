56f9e2431431d950c685e1e0a8a0c15eef20a2e6
Joel D. Simon <jdsimon@princeton.edu>
2020-02-01 12:35:29 -0500

    Add CPPT data to update workflow

--- earthquakes/updateid.m
+++ earthquakes/updateid.m
@@ -1,6 +1,7 @@
-function [mer_evt, mer_EQ, nearby_evt, nearby_EQ, nearby_evtu, nearby_EQu] = updateid(id, force, mer_evtdir, mer_sacdir, nearbydir, model, ph, baseurl)
-% [mer_evt, mer_EQ, nearby_evt, nearby_EQ, nearby_evtu, nearby_EQu] = ...
-%     UPDATEID(id, force, mer_evtdir, mer_sacdir, nearbydir, model, ph, baseurl)
+function [mer_evt, mer_EQ, nearby_evt, nearby_EQ, nearby_evtu, nearby_EQu, cppt_EQ] = ...
+    updateid(id, force, mer_evtdir, mer_sacdir, nearbydir, cpptdir, model, ph, baseurl)
+% [mer_evt, mer_EQ, nearby_evt, nearby_EQ, nearby_evtu, nearby_EQu, cppt_EQ] = ...
+%     UPDATEID(id, force, mer_evtdir, mer_sacdir, nearbydir, cpptdir, model, ph, baseurl)
 %
 % UPDATEID refetches event metadata from IRIS and updates (overwrites)
 % the associated .evt files for a given event ID.
@@ -24,6 +25,9 @@ function [mer_evt, mer_EQ, nearby_evt, nearby_EQ, nearby_evtu, nearby_EQu] = upd
 % nearbydir     Path to directory containing nearby stations
 %                   'sac/' and 'evt/' subdirectories
 %                   (def: $MERMAID/events/nearbystations/)
+% cpptdir     Path to directory containing CPPT stations
+%                   'sac/' and 'evt/' subdirectories
+%                   (def: $MERMAID/events/cpptstations/)
 % model         Taup model (def: 'ak135')
 % ph            Taup phases (def: defphases)
 %                   N.B.: use force=true to update with new phases
@@ -49,7 +53,7 @@ function [mer_evt, mer_EQ, nearby_evt, nearby_EQ, nearby_evtu, nearby_EQu] = upd
 %
 % Author: Joel D. Simon
 % Contact: jdsimon@princeton.edu
-% Last modified: 29-Nov-2019, Version 2017b on GLNXA64
+% Last modified: 01-Feb-2020, Version 2017b on GLNXA64
 
 % Defaults.
 defval('id', '10937574')
@@ -57,24 +61,62 @@ defval('force', false)
 defval('mer_evtdir', fullfile(getenv('MERMAID'), 'events'))
 defval('mer_sacdir', fullfile(getenv('MERMAID'), 'processed'))
 defval('nearbydir', fullfile(getenv('MERMAID'), 'events', 'nearbystations'))
+defval('cpptdir', fullfile(getenv('MERMAID'), 'events', 'cpptstations'))
 defval('model', 'ak135')
 defval('ph', defphases)
 defval('baseurl', 1);
+
+mer_sac = {};
 mer_evt = {};
 mer_EQ = {};
+
+nearby_sac = {};
 nearby_evt = {};
 nearby_EQ = {};
+nearby_sacu = {};
 nearby_evtu = {};
 nearby_EQu = {};
 
-% Retrieve all EQ files associated with this event.  Do not
+cppt_sac = {};
+cppt_evt = {};
+cppt_EQ = {};
+
+
+% Retrieve all MERMAID EQ files associated with this event -- will
+% always exist because you matched on that event. No need to
 % check4update because that happens here, next.
 id = num2str(id);
-[mer_sac, mer_EQ, nearby_sac, nearby_EQ, nearby_sacu, nearby_EQu] = ...
-    getnearbysacevt(id, mer_evtdir, mer_sacdir, nearbydir, false, 'ALL', '');
+[mer_sac, mer_EQ] = getsacevt(id, mer_evtdir, mer_sacdir, false, 'ALL');
+
+%% Do not retrieve mer_* info in getnearbysacevt.m or getcpptsacevt.m
+%% because those will fail if nearby/CPPT data do not exist.  We do not
+%% want to make the retrieval of MERMAID data hinge on the existence of
+%% other data; MERMAID data will always exist here and thus this
+%% function sill always AT LEAST check those EQ structures.
+
+% Retrieve all 'nearby' EQ files associated with this event. N.B.:
+% 'otype' is irrelevant here because it applies to the .SAC files, not
+% the .evt files, which apply to the same SAC files regardless of
+% 'otype'.
+nearby_idpath = fullfile(nearbydir, 'evt', id);
+nearby_iddir = dir(fullfile(nearby_idpath, '**/*.evt'));
+if ~isempty(nearby_iddir)
+    [~, ~, nearby_sac, nearby_EQ, nearby_sacu, nearby_EQu] = ...
+        getnearbysacevt(id, mer_evtdir, mer_sacdir, nearbydir, false, 'ALL');
+
+end
+
+% Repeat the same fetch for CPPT data.
+cppt_idpath = fullfile(cpptdir, 'evt', id);
+cppt_iddir = dir(fullfile(cppt_idpath, '**/*.evt'));
+if ~isempty(cppt_iddir)
+    [~, ~, cppt_sac, cppt_EQ] = ...
+        getcpptsacevt(id, mer_evtdir, mer_sacdir, cpptdir, false, 'ALL');
+
+end
 
 % Determine if the EQ structures differ and exit if they do not.
-if ~force && ~need2updateid([mer_EQ ; nearby_EQ ; nearby_EQu], id)
+if ~force && ~need2updateid([mer_EQ; nearby_EQ; nearby_EQu; cppt_EQ], id)
     fprintf('ID %s: update not required, all metadata match\n', id)
     return
 
@@ -96,6 +138,9 @@ updated_EQ = sac2evt(mer_sac{1}, model, ph, baseurl, 'eventid', id);
 
 % Nearby stations, unmerged -- may not exist.
 [nearby_evtu, nearby_EQu] = main(updated_EQ, nearby_EQu, nearby_sacu, fullfile(nearbydir, 'evt', id, 'unmerged'), model, ph);
+
+% CPPT stations -- may not exist.
+[cppt_evt, cppt_EQ] = main(updated_EQ, cppt_EQ, cppt_sac, fullfile(cpptdir, 'evt', id), model, ph);
 
 %___________________________________________________________________________________________%
 function [rev_evt, rev_EQ] = main(updated_EQ, old_EQ, sac, evt_path, model, ph)
