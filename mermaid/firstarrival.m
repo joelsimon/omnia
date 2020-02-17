3e2121b473047bea7fa37ebc41b51bbc1dc94750
Joel D. Simon <jdsimon@princeton.edu>
2020-02-17 15:58:35 -0500

    Adjust taper so that its length is a consistent proportion of the requested window length
    Now only a portion of the data are tapered, not the entire input, so that the
    effect is consistent regardless if the input time series is 200 s or 60 m in
    length.  Also, add flags warning if taper failed (too long for time series), or
    if there are suspected filler/missing values (implied by two contiguous datum
    equaling exactly 0).

--- mermaid/firstarrival.m
+++ mermaid/firstarrival.m
@@ -1,16 +1,28 @@
-function [tres, dat, syn, tadj, ph, delay, twosd, xw1, xaxw1, maxc_x, maxc_y, ...
-          SNR, EQ, W1, xw2, W2, incomplete] = firstarrival(s, ci, wlen, lohi, sacdir, evtdir, EQ, bathy)
+function [tres, dat, syn, tadj, ph, delay, twosd, xw1, xaxw1, maxc_x, ...
+          maxc_y, SNR, EQ, W1, xw2, W2, winflag, tapflag, zerflag] ...
+        = firstarrival(s, ci, wlen, lohi, sacdir, evtdir, EQ, bathy)
 % [tres, dat, syn, tadj, ph, delay, twosd, xw1, xaxw1, maxc_x, maxc_y, ...
-%          SNR, EQ, W1, xw2, W2, incomplete] = FIRSTARRIVAL(s, ci, wlen, lohi, sacdir, evtdir, EQ, bathy)
+%        SNR, EQ, W1, xw2, W2, winflag, tapflag, zerflag] = ...
+%        FIRSTARRIVAL(s, ci, wlen, lohi, sacdir, evtdir, EQ, bathy)
 %
-% Computes the travel time residual between the AIC-based arrival-time
-% estimate of paper?? and the theoretical arrival time of the
-% first-arriving phase in the associated EQ structure from cpsac2evt.m.
+% Computes the travel-time residual between the AIC-based arrival-time
+% estimate of Simon, J. D. et al., (2020), BSSA, doi:
+% 10.1785/0120190173, and the theoretical arrival time of the
+% first-arriving phase in the associated EQ structure (the latter may
+% be generated with cpsac2evt.m, and reviewed with reviewevt.m)
 %
-% Note that the data are detrended, tapered, and bandpassed BEFORE
-% windowing (W1, W2), meaning that the taper will have different
-% effects depending on the length of the input time series as related
-% to the window length and particularly its position w.r.t to the edges.
+% The AIC arrival-time estimate is made in a time window of the length
+% specified as input, which is centered on the first-arriving phase in
+% EQ(1).  This windowed segment of data may optionally be filtered
+% before the AIC pick is made if 'lohi' is specified.  In that case,
+% windowed segment of data twice the length of the requested window is
+% tapered with a Tukey window with a 0.5 cosine taper; i.e., if 'wlen'
+% is 30 s, the taper is flat in a 30 s window centered on the
+% theoretical first arrival, then cosine-tapered over 15 seconds on
+% either end for a total of 60 seconds of nonzero data.  After the
+% data are tapered the entire time series is filtered, but only the 30
+% s window within the flat part of the taper is considered for
+% finding the AIC pick.
 %
 % Input:
 % s        SAC filename (def: '20180819T042909.08_5B7A4C26.MER.DET.WLT5.sac')
@@ -33,48 +45,63 @@ function [tres, dat, syn, tadj, ph, delay, twosd, xw1, xaxw1, maxc_x, maxc_y, ..
 % Output:
 % tres     Travel time residual [s] w.r.t first phase arrival:
 %              estimated (cpest.m) - theoretical (taupTime.m)
-% dat      Actual arrival time computed with cpest.m [s]*
+% dat      AIC arrival-time estimate computed with cpest.m [s]*
 % syn      Theoretical arrival time computed with taupTime.m [s]*
-% tadj     Time adjustment for bathymtry, if bathytime is true [s]
+% tadj     Time adjustment for bathymetry, if 'bathy' is true [s]
 % ph       Phase name associated with tres
 % delay    Time delay between true arrival time and time at largest
 %             amplitude (max_y) [s]
-% xw1      Windowed segment of x (maybe filtered) contained in W1
+% xw1      Windowed segment of x (maybe filtered) contained in W1 --
+%              the entire segment of (maybe filtered) data considered
+%              for the AIC pick
 % xaxw1    x-axis centered on syn at 0 seconds, i.e., compliment to xw1
-% maxc_x   Time at of maximum (or minimum) amplitude of bandpassed signal [s]*
-% maxc_y   Amplitude (counts) of maximum (or minimum) amplitude of bandpassed
-%              signal within window beginning at dat and ending wlen/2 later
+% maxc_x   Time at of maximum (or minimum) amplitude of signal [s]*
+% maxc_y   Amplitude (e.g., counts, nm) of maximum (or minimum) amplitude of
+%              signal within W2 -- window beginning at dat and ending wlen/2 later
 % twosd    2-standard deviation error estimation per M1 method [s]**
 %              (def NaN)
 % SNR      SNR, defined as ratio of biased variance of signal and
 %              noise segments (see wtsnr.m)
 % EQ       Input EQ structure or reviewed EQ struct associated with SAC file
 %              via getevt.m
-% W1       timewindow.m struct of length wlen [s]
-%              centered on theoretical first arrival time
-% xw2      Windowed segment of x (maybe filtered) contained in W2
+% W1       timewindow.m struct of length wlen [s] centered on theoretical
+%              first arrival-time -- all the data considered for AIC pick
+% xw2      Windowed segment of x (maybe filtered) contained in W2 --
+%              window beginning at dat and ending wlen/2 later
 % W2       timewindow.m struct of length wlen/2 [s]
-%              beginning at dat
-% incomplete 0: Both time windows complete
-%            1: W1 incomplete, W2 complete
-%            2: W1 complete, W2 incomplete
-%            3: Both time windows incomplete
+%              beginning at dat -- all the data considered for maxc_y pick
+% winflag  Windlow flag (sentinel value)
+%          0: Both time windows complete
+%          1: W1 incomplete, W2 complete
+%          2: W1 complete, W2 incomplete
+%          3: Both time windows incomplete
+% tapflag  Taper flag (sentinel value; only relevant if bandpass filtered)
+%          NaN: Bandpass (and thus taper) not requested
+%          0: Success; time series tapered before bandpass
+%          1: Failure; time series not tapered before bandpass because
+%             taper extends beyond start/end time of time series
+% zerflag  Null-value flag (sentinel value), to catch if e.g.,
+%              if gaps in data were filled with zeros
+%          0: No two contiguous datum == 0 in W1, W2,
+%             or taper (if the latter exists)
+%          1: At least two contiguous datum == 0 in W1, W2,
+%             or taper (if the latter exists)
 %
 % *The x-axis here is w.r.t to original, NOT windowed, seismogram,
 % i.e. xaxis(h.NPTS, h.DELTA, h.B), where h is the SAC header
 % structure from readsac.m
 %
-% **See cpci.m and paper??
+% **See cpci.m Simon, J. D. et al., (2020), BSSA, doi: 10.1785/0120190173
 %
-% ***If 'lohi' is specific the data are linearly detrended then tapered
-% with a Hanning window before bandpass filtering
+% ***If 'lohi' is specific the data are tapered with a Hanning window
+% before bandpass filtering
 %
-% ****If mermaid depth is not contained in the header ('STDP' field),
-% then it is assumed to be 1500 m below the sea surface.
+% ****If MERMAID depth is not contained in the header ('STDP' field),
+% then it is assumed to be 1500 m below the sea surface
 %
 % Author: Joel D. Simon
 % Contact: jdsimon@princeton.edu
-% Last modified: 18-Dec-2019, Version 2017b on GLNXA64
+% Last modified: 17-Feb-2020, Version 2017b on MACI64
 
 % Defaults.
 defval('s', '20180819T042909.08_5B7A4C26.MER.DET.WLT5.sac')
@@ -84,11 +111,18 @@ defval('lohi', [1 5])
 defval('sacdir', fullfile(getenv('MERMAID'), 'processed'))
 defval('evtdir', fullfile(getenv('MERMAID'), 'events'))
 defval('EQ', [])
-defval('bathy', true)
+defval('bathy', false)
 
 % Start with baseline assumption both time windows will be complete.
 incomplete1 = false;
 incomplete2 = false;
+
+% Start with assumption that time series will not be tapered.
+tapflag = NaN;
+
+% Start with the assumption that the data have no 0 values (real data
+% is unlikely to have ever have a value exactly equal to zero).
+zerflag = 0;
 
 % Read data and retrieve event structure.  EQ contains the theoretical
 % arrival times of all phases included in the time window, computed
@@ -156,28 +190,96 @@ else
 
 end
 
-% Bandpass filter the time series.
+% Window the time series.
+[xw1, W1, incomplete1] = timewindow(x, wlen, syn, 'middle', h.DELTA, h.B);
+
+% Check if any two contiguous datum within first time window == 0
+% (likely signals missing, filler values).
+zero_vals = find(xw1  == 0);
+if any(diff(zero_vals)) == 1 % diff = 1 means zero-values are next to each other
+        zerflag = 1;
+
+end
+
+% Filter the windowed time series.
 if ~isnan(lohi)
+    % Ensure Nyquist frequency is respected.
     if 1/h.DELTA < 2*lohi(end)
-        % Nyquist.
         error('Upper cutoff frequency >= 1/2 the sampling frequency')
 
      end
 
-    x = detrend(x, 'linear');
-    taper = hanning(length(x));
-    x = taper .* x;
-    xf = bandpass(x, 1/h.DELTA, lohi(1), lohi(2));
+     % Remove mean and trend.
+     x = detrend(x, 'constant');
+     x = detrend(x, 'linear');
 
-else
-    xf = x;
+     % The taper is exactly 1 in the timewindow of interest 'xw1', and
+     % decays to zero outside.  It is constructed by generating a
+     % Hanning window that is the length of the timewindow of
+     % interest, splitting it down the middle, and attaching those
+     % symmetrically-decreasing cosine tapers to either end of the
+     % series of ones. That taper is then then be multiplied by the
+     % original time series.  Finally, that whole, tapered time series
+     % is bandpass filtered, and then is xw1 found again (it is still
+     % located in the same place (time), though the values inside that
+     % window have now changed thanks to filtering) from that
+     % tapered, filtered complete time series.
+     hanwin = hanning(length(xw1));
+     hanwin_middle = length(hanwin) / 2;
+
+     % This ensures: isequal(left_taper,flip(right_taper)) is true
+     % regardless whether x is length even or odd because hanning is
+     % exactly 1 in the middle of an odd length array (which is
+     % skipped here), or it never reaches 1, and no indices are skipped.
+     left_taper = hanwin(1:floor(hanwin_middle));
+     right_taper = hanwin(ceil(hanwin_middle)+1:end);
+
+     % Construct taper.
+     taper = zeros(size(x));
+     taper(W1.xlsamp:W1.xrsamp) = 1;
+
+     left_taper_idx = [W1.xlsamp-length(left_taper) : W1.xlsamp-1];
+     right_taper_idx = [W1.xrsamp+1 : W1.xrsamp+length(right_taper)];
+
+     % Only apply taper if it completely exists within the bounds of the
+     % input time series (no partial/abbreviated/cut-off taper allowed).
+     if left_taper_idx(1) >= 1 && right_taper_idx(end) <= length(x)
+         % Taper applied.
+         tapflag = 0;
+
+         % Check if any two contiguous datum with the taper == 0
+         % (likely signals missing, filler values).
+         within_taper = x(left_taper_idx(1):right_taper_idx(end));
+         zero_vals = find(within_taper == 0);
+         if any(diff(zero_vals)) == 1
+             zerflag = 1;
+
+         end
+
+         % Technically this taper is a Tukey taper with cosine taper fraction
+         % 0.5 -- see the verification at the end.
+         taper(left_taper_idx) = left_taper;
+         taper(right_taper_idx) = right_taper;
+
+         x = x .* taper;
+
+     else
+         % Taper not applied -- extends beyond first or last sample of 'x'.
+         tapflag = 1;
+
+     end
+
+     % Bandpass entire (maybe tapered) time series.
+     x = bandpass(x, 1/h.DELTA, lohi(1), lohi(2));
+
+     % Remove the relevant window from the tapered and filtered time series.
+     [xw1, W1, incomplete1] = timewindow(x, wlen, syn, 'middle', h.DELTA, h.B);
 
 end
 
-% Window the (bandpassed, maybe) time series.
-[xw1, W1, incomplete1] = timewindow(xf, wlen, syn, 'middle', h.DELTA, h.B);
-
 % The offset x-axis, which sets syn at 0 s.
+% N.B., W1.xax: places xw1 in the timing of the original input x
+%        xaxw1: places xw1 such that it is centered on the theoretical first arrival at 0 s
 xaxw1 = W1.xax - syn;
 
 % Changepoint estimate sample considering only the windowed portion centered on the theoretical first arrival.
@@ -206,10 +308,19 @@ if SNR > 1
     % complete time segment.
     tres = dat - syn;
 
-    % Maximum absolute amplitude (counts) considering a window of length
-    % wlen/2 starting at the actual phase arrival.
-    [xw2, W2, incomplete2] = timewindow(xf, wlen/2, dat, 'first', h.DELTA, h.B);
+    % Maximum absolute amplitude (e.g., counts or nm) considering a window
+    % of length wlen/2 starting at the actual phase arrival.
+    [xw2, W2, incomplete2] = timewindow(x, wlen/2, dat, 'first', h.DELTA, h.B);
 
+    % Check if any two contiguous datum within the second time window == 0
+    % (likely signals missing, filler values).
+    zero_vals = find(xw2  == 0);
+    if any(diff(zero_vals)) == 1
+        zerflag = 1;
+
+    end
+
+    % Identify the maximum (or minimum) amplitude within W2.
     maxy = max(xw2);
     miny = min(xw2);
     if maxy > abs(miny)
@@ -256,18 +367,46 @@ end
 
 % Determine windows' completion classification.
 if ~incomplete1 && ~incomplete2
-    incomplete = 0;
+    winflag = 0;
 
 end
 if incomplete1
-    incomplete = 1;
+    winflag = 1;
 
 end
 if incomplete2
-    incomplete = 2;
+    winflag = 2;
 
 end
 if incomplete1 && incomplete2
-    incomplete = 3;
+    winflag = 3;
 
 end
+
+% %_________________________________________________________________________________%
+% % Verify equality between my taper and the Tukey window. Mine is
+% % constructed the way it is so that I can keep track of timing easily.
+% % Easier for me to do it that way than to derive a Tukey window of the
+% % correct length and put it in the correct place.
+
+% % This cosine taper is a Tukey window with cosine fraction 0.5.
+% % Verified for both even- and odd-length windows.
+
+% % Joel's taper.
+% t1 = taper(find(taper > 0));
+
+% % MATLAB's builtin.
+% t2 = tukeywin(length(t1), 0.5);
+
+% % Difference.
+% max(abs(t1 -t2))
+
+% % Also, in both even- and odd-length xw1 cases.
+% isequal(length(left_taper), length(right_taper))
+
+% % And, if xw1 is even:
+% length(left_taper) + length(right_taper) == length(xw1)
+
+% % And, if xw1 is odd: because the singular 1 directly in the middle of
+% % the Hanning window is removed when it is split.
+% length(left_taper) + length(right_taper) == length(xw1) - 1
