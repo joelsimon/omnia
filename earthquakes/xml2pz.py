bc7df064744d192f25475758e8c616a5c16d6493
Joel D. Simon <jdsimon@princeton.edu>
2020-02-21 12:18:11 -0500

    Note that conversion from velocity RESP to displacement SACPZ confirmed

--- earthquakes/xml2pz.py
+++ earthquakes/xml2pz.py
@@ -3,11 +3,23 @@
 # .xml is replaced with .pz.  Input .xml file is removed from system
 # after execution.
 #
+# Requires obspy and is intended to be used within the MERMAID python
+# environment 'pymaid'.
+#
 # Input: .xml filename (e.g., AM.RF737.00.Z.xml)
 # Output: .pz filename (e.g., AM.RF737.00.Z.pz)
 #
-# Requires obspy and is intended to be used within the MERMAID python
-# environment 'pymaid'.
+# Ultimately for me this calls: ~/.conda/envs/pymaid/lib/python2.7/site-packages/obspy/io/sac/sacpz.m
+#
+# You can you see it conforms with the SEED convention of converting
+# STATION XML response files from their native unit (e.g., velocity in
+# meters per second) to displacement (meters) by adding the correct
+# number of zeros and updating the CONSTANT accordingly (in units of
+# displacement, meters).
+#
+# See $MERMAID/events/nearbystations/pz/examples/XML2PZ_example where
+# this is confirmed, specifically that a velocity RESP file is
+# correctly converted to displacement SACPZ file.
 #
 # Author: Joel D. Simon
 # Contact: jdsimon@princeton.edu
