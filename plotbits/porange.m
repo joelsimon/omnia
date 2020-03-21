cc9089679590fd63967fdaec25168564ac344d93
Joel D. Simon (Darwin) <jdsimon@princeton.edu>
2020-03-21 16:50:54 -0400

    Introduce function to return RGB color space of "Princeton Orange"

---
+++ plotbits/porange.m
@@ -0,0 +1,10 @@
+function col = porange
+%
+% PORANGE returns the RGB color space of "Princeton Orange,"
+% 100% red, 56.1% green and 0% blue.
+%
+% Author: Joel D. Simon
+% Contact: jdsimon@princeton.edu
+% Last modified: 21-Mar-2020, Version 2017b on MACI64
+
+col = [1 0.561 0];
