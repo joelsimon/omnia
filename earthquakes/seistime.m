0176f3571a5278784465e9a8304632df6b887da8
Joel D. Simon (Darwin) <jdsimon@princeton.edu>
2020-04-04 12:37:08 -0400

    Add examples explaining relative offset of x-axes compared with absolute SAC file times

--- earthquakes/seistime.m
+++ earthquakes/seistime.m
@@ -6,16 +6,16 @@ function [seisdate, seiststr, seisertime, refdate, evtdate] = seistime(h)
 % arbitrary), plus any offset time in seconds of the first/last sample
 % as designated by "B"/"E" field in SAC header.
 %
-%            seconds(seisdate.B - refdate)  == h.B
+%           seconds(seisdate.B - refdate) == h.B
 %
 % SEISTIME will also returns the reference time in the SAC header, and
 % maybe return the event time (reference time - h.O) if h.O (origin
 % time) is filled.
 %
-% N.B.: Because seisdate.B is the time at the first sample -->
+% NB, because seisdate.B is the time at the first sample -->
 %
-%   time relative to refdate: xaxis(h.NPTS, h.DELTA, h.B)
-%   time relative to seisdate.B: xaxis(h.NPTS, h.DELTA, 0)
+% Ex2: x-axis time relative to refdate: xaxis(h.NPTS, h.DELTA, h.B)
+% Ex3: x-axis time relative to first sample: xaxis(h.NPTS, h.DELTA, 0)
 %
 % First three outputs are same data in three formats: a datetime array
 % via datetime.m, a PDE/ISC-formatted string via datestr.m, and a
@@ -42,34 +42,24 @@ function [seisdate, seiststr, seisertime, refdate, evtdate] = seistime(h)
 % refdate     Reference time of the SAC header in datetime format
 % evtdate     Time of event (origin), if h.O field filled (def: [])
 %                evtdate = refdate + seconds(h.O)
-% _____________________
-%
-% N.B.: Datetime, by default, does not show millisecond
-% precision (but it does store it).
-%
-% >> foo = datetime('2014 987', 'InputFormat', 'uuuu SSS') - ...
-%          datetime('2014 986', 'InputFormat', 'uuuu SSS')
-% foo =
-%       duration
-%       00:00:00
-%
-% >> milliseconds(foo)
-%
-% ans =
-%      1
-%
-% Also: "The symbolic identifiers describing date and time formats are
-% different from those that describe the display formats of datetime
-% arrays."
-%
-% https://www.mathworks.com/help/matlab/ref/datestr.html#btenptl-1-formatOut
-%
-% This is annoying; 'uuuu' in datetime.m is 'yyyy' in datestr.m
-% _____________________
-%
-% Ex:
+% Ex1:
 %    [~, h] = readsac('m12.20130416T105310.sac')
 %    [seisdate, seiststr, seisertime, refdate] = SEISTIME(h)
+%
+% Ex2: (plot on x-axis where the time of the first sample is in reference to refdate)
+%    [x, h] = readsac('centcal.1.BHZ.SAC');
+%    seisdate = SEISTIME(h);
+%    xax = xaxis(h.NPTS, h.DELTA, h.B);
+%    plot(xax, x); title('First sample at seconds offset from refdate')
+%
+% Ex3: (plot on x-axis where the time of the first sample is in reference to seisdate.B)
+%    [x, h] = readsac('centcal.1.BHZ.SAC');
+%    seisdate = SEISTIME(h);
+%    xax = xaxis(h.NPTS, h.DELTA, 0);
+%    plot(xax, x); title('First sample at seconds offset from seisdate.B')
+%
+% In both cases, seisdate.B and seisdate.E are unchanged -- they are absolute
+% datetimes.  What changes is the relative time that you place the first sample.
 %
 % See also: arrivaltime.m, readsac.m
 %
