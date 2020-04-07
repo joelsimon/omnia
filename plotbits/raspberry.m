5ec2eb4d2e4e8870d65cc7c130a4146e01a29b21
Joel D. Simon (Darwin) <jdsimon@princeton.edu>
2020-04-07 18:03:58 -0400

    Introduce function to return color: raspberry

---
+++ plotbits/raspberry.m
@@ -0,0 +1,9 @@
+function col = raspberry
+% RASPBERRY returns the RGB color space of raspberry,
+% 89% red, 4.3% green and 36.5% blue.
+%
+% Author: Joel D. Simon
+% Contact: jdsimon@princeton.edu | joeldsimon@gmail.com
+% Last modified: 07-Apr-2020, Version 9.3.0.948333 (R2017b) Update 9 on MACI64
+
+col = [0.89 0.043 0.365];
