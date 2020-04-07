fb7fd85460eba80092e25849b5747d649c932c1f
Joel D. Simon (Darwin) <jdsimon@princeton.edu>
2020-04-07 14:05:23 -0400

    Update notation to match Supplemental Material (Sd --> SD)

--- mermaid/cpptstations/cpptsacpzconstant.m
+++ mermaid/cpptstations/cpptsacpzconstant.m
@@ -4,6 +4,9 @@ function [CONSTANT1, CONSTANT2] = cpptsacpzconstant
 % Returns the displacement SACPZ CONSTANT for six stations in the Reseau
 % Sismique Polynesien (RSP) network.
 %
+% Input:
+% --none--
+%
 % Output:
 % CONSTANT1   Displacement SACPZ CONSTANT corresponding to PAE, TVO
 % CONSTANT2   Displacement SACPZ CONSTANT corresponding to PMOR, VAH, TBI, RKT
@@ -11,8 +14,8 @@ function [CONSTANT1, CONSTANT2] = cpptsacpzconstant
 % See also: sacpzconstant.m
 %
 % Author: Joel D. Simon
-% Contact: jdsimon@princeton.edu
-% Last modified: 05-Apr-2020, Version 2017b on MACI64
+% Contact: jdsimon@princeton.edu | joeldsimon@gmail.com
+% Last modified: 07-Apr-2020, Version 9.3.0.948333 (R2017b) Update 9 on MACI64
 
 %% Relevant bits of original email.
 %%______________________________________________________________________________________%%
@@ -28,7 +31,7 @@ function [CONSTANT1, CONSTANT2] = cpptsacpzconstant
 % 0.5236 nm/s/LSB  at 1 Hz for PAE, TVO
 % 0.212 nm/s/LSB at 1 Hz for PMOR, VAH, TBI, RKT
 %
-% Response for PAE, TVO, PMOR, VAH, TBI, RKT  (High pass filter at 1 Hz, order 2) :
+% Response for PAE, TVO, PMOR, VAH, TBI, RKT  (high pass filter at 1 Hz, order 2) :
 %
 % 2 zeroes : (0;0) (0;0)
 %
@@ -45,7 +48,7 @@ function [CONSTANT1, CONSTANT2] = cpptsacpzconstant
 fs_Hz = 1;
 fs_rad = 2*pi*fs_Hz;
 
-%% Poles and zeros (the same for all stations stations).
+%% Poles and zeros (the same for all stations).
 
 % They were provided to me in terms of velocity.
 Z_vel = [0+0i ...
@@ -53,40 +56,40 @@ Z_vel = [0+0i ...
 P_vel = [-4.44-4.44i ...
          -4.44+4.44i];
 
-% To convert to displacement, add one zero. The poles are unchanged.
-Z_disp = [0+0i ...
-          0+0i ...
+% Add one zero to convert to displacement. The poles are unchanged.
+Z_disp = [Z_vel ...
           0+0i];
-P_disp = [-4.44-4.44i ...
-          -4.44+4.44i];
+P_disp = P_vel;
 
-%% Sensitivities (Sd in SEED parlance).
+%% Sensitivities (Sd in SEED parlance) -- NB, Sd is the sensitivity at a single
+%% stage (e.g., analog; V/(m/s)); I use SD to represent the combined
+%% sensitivities after all stages (digital; counts/(m/s)).
 
-% NB, the equality of [nm/s/lsb]^-1 = [(nm/s)/lsb]^-1 = counts/(nm/s) was
-% verified by Olivier Hyvernaud 22-Feb-2020, pers. comm.
+% NB, the equality of (nm/s/LSB)^-1 = counts/(nm/s) was verified by Olivier
+% Hyvernaud 22-Feb-2020, pers. comm.
 %
-% We must convert Sd from <physical_unit>/count --> count/<physical_unit> (the
+% We must convert SD from <physical_unit>/count --> count/<physical_unit> (the
 % convention in the SEED manual, and how SACPZ, RESP, and StationXML files are
-% delivered from IRIS).  Next we must convert from velocity to displacement,
-% such that a TRANSFER to NONE in SAC produces a displacement
-% seismogram. Finally, in keeping with SEED standard of SI units, we must
-% convert them from nanometers to meters.
+% delivered from IRIS).  Next we must convert SD velocity to displacement, such
+% that a TRANSFER to NONE in SAC produces a displacement seismogram. Finally, in
+% keeping with SEED standard of SI units, we must convert SD from
+% counts/nanometer to counts/meter.
 
 % I will refer to stations PAE, TVO as group 1 and all others as group 2.  These
-% are the sensitivities (gains) as provided in nm/s/LSB.
-Sd1_vel = 0.5236;
-Sd2_vel = 0.212;
+% are the sensitivities as provided in nm/s/LSB.
+SD1_vel = 0.5236;
+SD2_vel = 0.212;
 
 % Convert from nm/s/LSB to counts/(m/s).
-Sd1_vel = Sd1_vel^-1 * 1e9;
-Sd2_vel = Sd2_vel^-1 * 1e9;
+SD1_vel = SD1_vel^-1 * 1e9;
+SD2_vel = SD2_vel^-1 * 1e9;
 
 % Convert from velocity sensitivities to displacement sensitivities.  This
-% requires multiplying by frequency of sensitivity in rad/s because the
+% requires multiplying by the frequency of sensitivity in rad/s because the
 % sensitivity (while quoted in Hz) was computed in rad/s.
-Sd1_disp = Sd1_vel*fs_rad;
-Sd2_disp = Sd2_vel*fs_rad;
+SD1_disp = SD1_vel*fs_rad;
+SD2_disp = SD2_vel*fs_rad;
 
 % Finally, compute the constants.
-CONSTANT1 = sacpzconstant(Sd1_disp, fs_Hz, P_disp, Z_disp);
-CONSTANT2 = sacpzconstant(Sd2_disp, fs_Hz, P_disp, Z_disp);
+CONSTANT1 = sacpzconstant(SD1_disp, fs_Hz, P_disp, Z_disp);
+CONSTANT2 = sacpzconstant(SD2_disp, fs_Hz, P_disp, Z_disp);
