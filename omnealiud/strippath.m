9053584f6322883af64ddc0df9862a8c5fb9fd3c
Joel D. Simon (Darwin) <jdsimon@princeton.edu>
2020-04-17 16:41:21 -0400

    Add recursion for cell arrays

--- omnealiud/strippath.m
+++ omnealiud/strippath.m
@@ -4,11 +4,11 @@ function filename = strippath(filename)
 % Strips filename from path if separators ('/' or '\') present.
 %
 % Input:
-% filename       Filename, possibly with a full path
+% filename       Filename(s), possibly with a full path (cells okay)
 %
 % Output:
-% filename       Filename with full path removed
-% 
+% filename       Filename(s) with full path removed, output in char or cell
+%
 % Ex1: (leading path stripped)
 %    filename = '/mermaid35/identified/sac/m35.20141122T131937.sac'
 %    filename = STRIPPATH(filename)
@@ -16,9 +16,29 @@ function filename = strippath(filename)
 % Ex2: (no leading path; returns input)
 %    filename = STRIPPATH('m35.20141122T131937.sac')
 %
+% Ex3: (cell arrays allowed)
+%    filename = {'~/abc/test1.txt'; '~/abc/test2.txt'}
+%    filename = STRIPPATH(filename)
+%
 % Author: Joel D. Simon
-% Contact: jdsimon@princeton.edu
-% Last modified: 16-Nov-2017, Version 2017b
+% Contact: jdsimon@princeton.edu | joeldsimon@gmail.com
+% Last modified: 17-Apr-2020, Version 9.3.0.948333 (R2017b) Update 9 on MACI64
+
+%% Recursive.
+
+if isa(filename, 'cell')
+
+    %% Recursive.
+
+    for i = 1:length(filename)
+        temp_filename{i} = strippath(filename{i});
+
+    end
+    filename = temp_filename(:);
+
+    return
+
+end
 
 % Sanity
 if ~isa(filename,'char')
