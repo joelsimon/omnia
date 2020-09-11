4da8185f25e851c9fb8c97db4223dc5c34357f90
Joel D. Simon (Darwin) <jdsimon@princeton.edu>
2020-09-11 16:28:56 -0700

    Use list intersection for proper file list build

--- mermaid/automaidversions.m
+++ mermaid/automaidversions.m
@@ -1,29 +1,31 @@
-function automaidversions(mustequate)
+ function automaidversions(mustequate)
 % AUTOMAIDVERSIONS(musteqate)
 %
 % Compares SAC files compiled with different automaid versions.
 %
 % Author: Dr. Joel D. Simon
 % Contact: jdsimon@alumni.princeton.edu | joeldsimon@gmail.com
-% Last modified: 09-Sep-2020, Version 9.3.0.948333 (R2017b) Update 9 on MACI64
+% Last modified: 11-Sep-2020, Version 9.3.0.948333 (R2017b) Update 9 on MACI64
 
 sac_old = fullsac([], fullfile(getenv('MERMAID'), 'processed'));
 sac_new = fullsac([], fullfile(getenv('MERMAID'), 'test_processed'));
 
-[~, idx] = ismember(strippath(sac_old), strippath(sac_new));
-sac_old = sac_old(find(idx));
+[~, idx_old, idx_new] = intersect(strippath(sac_old), strippath(sac_new));
+sac_old = sac_old(idx_old);
+sac_new = sac_new(idx_new);
 
 if mustequate
     fid = fopen('~/old_new_diff_full.txt', 'w');
-    date_diff = 0;
-    dist_diff = 0;
+    allowed_date_diff = 0;
+    allowed_dist_diff = 0;
 
 else
     fid = fopen('~/old_new_diff_light.txt', 'w');
-    date_diff = 1e-3; % milliseconds
-    dist_diff = 1e-5; % ~1 m
+    allowed_date_diff = 1e-3; % milliseconds
+    allowed_dist_diff = 1e-5; % ~1 m
 
 end
+fmt = '%s\n';
 
 for i = 1:length(sac_old)
     [data_old, header_old] = readsac(sac_old{i});
@@ -37,36 +39,31 @@ for i = 1:length(sac_old)
     lon_new = header_new.STLO;
 
     if ~isequal(data_old, data_new)
-        sprintf('%s data', strippath(sac_old{i}))
-        fprintf(fid, '%s data\n', strippath(sac_old{i}))
+        fprintf(fid, fmt, sprintf('%s    data', strippath(sac_old{i})));
 
     end
 
-    if abs(seconds(seisdate_old.B - seisdate_new.B)) > date_diff
-        sprintf('%s date B', strippath(sac_old{i}))
-        %keyboard
-        fprintf(fid, '%s date.B\n ', strippath(sac_old{i}))
+    actual_dateB_diff = seconds(seisdate_old.B - seisdate_new.B);
+    if abs(actual_dateB_diff) > allowed_date_diff
+        fprintf(fid, fmt, sprintf('%s    date.B     %15.6f sec', strippath(sac_old{i}), actual_dateB_diff));
 
     end
 
-    if abs(seconds(seisdate_old.E - seisdate_new.E)) > date_diff
-        sprintf('%s date E', strippath(sac_old{i}))
-        %keyboard
-        fprintf(fid, '%s date.E\n', strippath(sac_old{i}))
+    actual_dateE_diff = seconds(seisdate_old.E - seisdate_new.E);
+    if abs(actual_dateE_diff) > allowed_date_diff
+        fprintf(fid, fmt, sprintf('%s    date.E     %15.6f sec', strippath(sac_old{i}), actual_dateE_diff));
 
     end
 
-    if abs(lat_old - lat_new) > dist_diff
-        sprintf('%s lat', strippath(sac_old{i}))
-        %keyboard
-        fprintf(fid, '%s lat\n', strippath(sac_old{i}))
+    actual_lat_diff = lat_old - lat_new;
+    if abs(actual_lat_diff) > allowed_dist_diff
+        fprintf(fid, fmt, sprintf('%s       lat     %15.6f deg', strippath(sac_old{i}), actual_lat_diff));
 
     end
 
-    if abs(lon_old - lon_new) > dist_diff
-        sprintf('%s lon', strippath(sac_old{i}))
-        %keyboard
-        fprintf(fid, '%s lon\n', strippath(sac_old{i}))
+    actual_lon_diff = lon_old - lon_new;
+    if abs(actual_lon_diff) > allowed_dist_diff
+        fprintf(fid, fmt, sprintf('%s       lon     %15.6f deg', strippath(sac_old{i}), actual_lon_diff));
 
     end
 end
