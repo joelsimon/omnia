6ecc6b25a3bf11f77b49e777b11ae599dee0149f
Joel D. Simon (Darwin) <jdsimon@princeton.edu>
2020-10-21 17:40:48 -0700

    Add example showing regexp-like behavior

--- omnealiud/recursivedir.m
+++ omnealiud/recursivedir.m
@@ -4,20 +4,29 @@ function filenames = recursivedir(d, filenames) % input: filenames is an interna
 % RECURSIVEDIR returns all non-dot filenames contained recursively
 % (i.e., including subdirectories) in the input directory.
 %
+% Use, d = dir(fullfile(partial_path, '**/*[pattern]*', '**/*[pattern2]*')) for
+% regexp-like behavior (though multiple patterns with dir.m is tricky and should
+% be used with caution...).
+%
 % Input:
-% d             Directory structure output from dir.m
+% d             Directory structure output from dir.m,
+%               partial path and dir('**/*[pattern]') allowed
 %
 % Output:
 % filenames     All (non-dot) filenames contained recursively
-%                   in the input directory.
+%                   in the input directory
 %
-% Ex:
-%    d = dir(getenv('OMNIA'))
+% Ex1: (fetch all files in $OMNIA')
+%    d = dir(getenv('OMNIA'));
 %    filenames = RECURSIVEDIR(d)
 %
-% Author: Joel D. Simon
-% Contact: jdsimon@princeton.edu
-% Last modified: 26-Sep-2019, Version 2017b on MACI64
+% Ex2: (fetch all SAC files contained recursively in $MERMAID/processed)
+%    d = dir(fullfile(getenv('MERMAID'), 'processed', '**/*.sac'));
+%    filenames = RECURSIVEDIR(d)
+%
+% Author: Dr. Joel D. Simon
+% Contact: jdsimon@alumni.princeton.edu | joeldsimon@gmail.com
+% Last modified: 21-Oct-2020, Version 9.3.0.948333 (R2017b) Update 9 on MACI64
 
 %% Recursive.
 
