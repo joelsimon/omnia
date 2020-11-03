ed6e6d81d669442e4043bce8cc14f43127ff5927
Joel D. Simon (Darwin) <jdsimon@princeton.edu>
2020-11-03 11:23:58 -0800

    Log and return max lat/lon difference

--- mermaid/automaidversions.m
+++ mermaid/automaidversions.m
@@ -1,5 +1,5 @@
-function automaidversions(mustequate, old, new, fname)
-% AUTOMAIDVERSIONS(mustequate, old, new, fname)
+function [max_lat_diff, max_lon_diff] = automaidversions(mustequate, old, new, fname)
+% [max_lat_diff, max_lon_diff] = AUTOMAIDVERSIONS(mustequate, old, new, fname)
 %
 % Compares SAC files compiled with different automaid versions.
 %
@@ -8,22 +8,22 @@ function automaidversions(mustequate, old, new, fname)
 %               false: data/location values must be same within sensible range
 % old           Directory containing old SAC files (def: $MERMAID/processed)
 % new           Directory containing new SAC files (def: $MERMAID/test_processed)
-% fname         Output file name (def: '~/old_new_diff[full/light].txt')
+% fname         Output file name (def: '~/automaidversions_diff_[full/light].txt')
 %
-% Author: Dr. Joel D. Simon
+% Author: Joel D. Simon
 % Contact: jdsimon@alumni.princeton.edu | joeldsimon@gmail.com
-% Last modified: 16-Oct-2020, Version 9.3.0.948333 (R2017b) Update 9 on MACI64
+% Last modified: 03-Nov-2020, Version 9.3.0.948333 (R2017b) Update 9 on MACI64
 
 defval('old', fullfile(getenv('MERMAID'), 'processed'))
 defval('new', fullfile(getenv('MERMAID'), 'test_processed'))
-defval('fname', fullfile(getenv('HOME'), 'old_new_diff'))
+defval('fname', fullfile(getenv('HOME'), 'automaidversions_diff'))
 
 sac_old = fullsac([], old);
 sac_new = fullsac([], new);
 
 [~, idx_old, idx_new] = intersect(strippath(sac_old), strippath(sac_new));
 sac_old = sac_old(idx_old);
-sac_new = sac_new(idx_new)
+sac_new = sac_new(idx_new);
 
 if mustequate
     fname = [fname '_full.txt'];
@@ -32,13 +32,15 @@ if mustequate
 
 else
     fname = [fname '_light.txt'];
-    allowed_date_diff = 1e-3; % milliseconds
-    allowed_dist_diff = 1e-5; % ~1 m
+    allowed_date_diff = 0.009 % seconds
+    allowed_dist_diff = 0.00009 % degrees
 
 end
 fid = fopen(fname, 'w');
 fmt = '%s\n';
 
+max_lat_diff = 0;
+max_lon_diff = 0;
 for i = 1:length(sac_old)
     [data_old, header_old] = readsac(sac_old{i});
     seisdate_old = seistime(header_old);
@@ -72,12 +74,28 @@ for i = 1:length(sac_old)
         fprintf(fid, fmt, sprintf('%s       lat     %15.6f deg', strippath(sac_old{i}), actual_lat_diff));
 
     end
+    if abs(actual_lat_diff) >  abs(max_lat_diff)
+        max_lat_diff = actual_lat_diff;
+        max_lat_sac = sac_new{i};
+
+    end
 
     actual_lon_diff = lon_old - lon_new;
     if abs(actual_lon_diff) > allowed_dist_diff
         fprintf(fid, fmt, sprintf('%s       lon     %15.6f deg', strippath(sac_old{i}), actual_lon_diff));
 
     end
+    if abs(actual_lon_diff) >  abs(max_lon_diff)
+        max_lon_diff = actual_lon_diff;
+        max_lon_sac = sac_new{i};
+
+    end
 
 end
 fprintf('Wrote %s\n', fname)
+
+max_lat_diff
+max_lat_sac
+
+max_lon_diff
+max_lon_sac
