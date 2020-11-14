f26946ff804a127150a9cdd8f380bbe9843f3607
Joel D. Simon (Darwin) <jdsimon@princeton.edu>
2020-11-13 16:51:01 -0800

    Introduce function to convert ISO 8601 date strings

---
+++ omnealiud/iso8601str2date.m
@@ -0,0 +1,39 @@
+function tdate = iso8601str2date(tstr, precision)
+% tdate = ISO8601STR2DATE(tstr, precision)
+%
+% Returns datetime array given input in iso8601 <date>T<time>Z format (UTC).
+%
+% Input:
+% tstr         iso8061 string or cell array, e.g. '2007-04-05T14:30Z'
+% precision    1: seconds, e.g., '2007-04-05T14:30Z' (def)
+%              2: milliseconds, e.g., '2007-04-05T14:30.123Z'
+%              3: microseconds, e.g., '2007-04-05T14:30.123456Z'
+%
+% Output:
+% tdate        Datetime array
+%
+% Author: Joel D. Simon
+% Contact: jdsimon@alumni.princeton.edu | joeldsimon@gmail.com
+% Last modified: 13-Nov-2020, Version 9.3.0.948333 (R2017b) Update 9 on MACI64
+
+% Defaults.
+defval('precision', 1);
+
+% Switch format specification
+switch precision
+  case 1
+    Format = 'uuuu-MM-dd''T''HH:mm:ss''Z''';
+
+  case 2
+    Format = 'uuuu-MM-dd''T''HH:mm:ss.SSS''Z''';
+
+  case 3
+    Format = 'uuuu-MM-dd''T''HH:mm:ss.SSSSSS''Z''';
+
+  otherwise
+    error('Specify one of 1, 2, or 3 for input ''precision''')
+
+end
+
+% Convert.
+tdate = datetime(tstr, 'Format', Format, 'TimeZone', 'UTC');
