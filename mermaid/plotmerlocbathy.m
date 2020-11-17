f20d302f21632bb98a96f1839b0f39a6422feea9
Joel D. Simon (Darwin) <jdsimon@princeton.edu>
2020-11-17 11:02:16 -0800

    Mid-commit: works with plotsouthpacificbathy

--- mermaid/plotmerlocbathy.m
+++ mermaid/plotmerlocbathy.m
@@ -15,29 +15,80 @@ evtdir = fullfile(merpath, 'events')
 nearbytbl = fullfile(getenv('MERMAID'), 'events', 'nearbystations', 'nearbystations.tbl');
 
 
-%%______________________________________________________________________________________%%
-%%                           simon2020_plotmerloc.m                                     %%
-%%______________________________________________________________________________________%%
+xlim([176 251])
+ylim([-33 4])
 
 clc
 close all
+%%______________________________________________________________________________________%%
+%% Plot bathymetric basemap
+%%______________________________________________________________________________________%%
+[ax_bathy, cb_bathy] = plotsouthpacificbathy();
 
-figure
-ha_bathy = simon2020_bathy_local(false);
-ha = axes;
-set(ha, 'Color', 'None', 'Position', ha_bathy.Position)
-hold(ha, 'on')
+%% Cosmetics to bathymetric basemap.
+xlim([176 251])
+ylim([-33 4])
+
+fig2print(gcf, 'flandscape')
+
+xlim([176 251])
+ylim([-33 4])
+ax = gca;
+hold(ax, 'on')
+
+fs = 13;
+
+cb_bathy.FontSize = fs;
+cb_bathy.Label.FontSize = fs;
+cb_bathy.Label.Interpreter = 'latex';
+
+movev(gca, 0.1)
+movev(cb_bathy, 0.1)
+cb_bathy.Ticks(9) = [];
+
+ax.XTick = [180:10:250];
+ax.XTickLabel = {'-180$^{\circ}$'  ...
+                 '-170$^{\circ}$' ...
+                 '-160$^{\circ}$' ...
+                 '-150$^{\circ}$' ...
+                 '-140$^{\circ}$' ...
+                 '-130$^{\circ}$' ...
+                 '-120$^{\circ}$' ...
+                 '-110$^{\circ}$'};
+
+
+ax.YTick = [-30:10:0];
+ax.YTickLabel = flip({'0$^{\circ}$' ...
+                    '-10$^{\circ}$' ...
+                    '-20$^{\circ}$' ...
+                    '-30$^{\circ}$'});
+
+longticks(ax, 3)
+cb_bathy.TickLength = 0.015;
+cb_bathy.TickDirection = 'out';
+
+axesfs(gcf, fs, fs+3)
+latimes
+cb_bathy.Location = 'EastOutside';
+
+%%______________________________________________________________________________________%%
+%% Overlay seconds (blank) axis to plot MERMAID tracks
+%%______________________________________________________________________________________%%
+
+ax_mer = axes;
+set(ax_mer, 'Color', 'None', 'Position', ax_bathy.Position)
+hold(ax_mer, 'on')
 fig2print(gcf, 'flandscape')
 
 % Equalize degrees in N/S and E/W directions.
-set(ha, 'DataAspectRatio', [1 1 1])
+set(ax_mer, 'DataAspectRatio', [1 1 1])
 
 fs = 13;
 axesfs(gcf, fs, fs)
 
 scsize = 10;
 tsize = fs - 2;
-
+keyboard
 %______________________________________________________________________________%
 
 % Plot MERMAID tracks.
@@ -83,13 +134,13 @@ days_deployed = days(locdate - locdate(1));
 max_days_deployed = days(last_day - locdate(1));
 
 cmap = jet(length(0:max_days_deployed));;
-colormap(ha, cmap);
+colormap(ax_mer, cmap);
 [col, cbticks, cbticklabels] = x2color(days_deployed, [], ...
                                        max_days_deployed, cmap, false);
 
-%plot(ha, lon, lat, ':', 'Color', [0.6 0.6 0.6])
-sc(1) = scatter(ha, lon, lat, scsize, col, 'filled');
-mer_tx(1) = text(ha, lon(1), lat(1)+1, '08', 'FontSize', tsize);
+%plot(ax_mer, lon, lat, ':', 'Color', [0.6 0.6 0.6])
+sc(1) = scatter(ax_mer, lon, lat, scsize, col, 'filled');
+mer_tx(1) = text(ax_mer, lon(1), lat(1)+1, '08', 'FontSize', tsize);
 
 cb = colorbar('Location', 'SouthOutside');
 cb.Ticks = cbticks(1:20:end);
@@ -126,20 +177,20 @@ for i = 2:length(name)
         lat(rm_idx) = [];
 
         % Plot a faint line connecting those points.
-        plot(ha, lon(rm_idx-1:rm_idx), lat(rm_idx-1:rm_idx), ':', ...
+        plot(ax_mer, lon(rm_idx-1:rm_idx), lat(rm_idx-1:rm_idx), ':', ...
              'Color', [0 0 0], 'LineWidth', 1);
 
         % Note again its still MERMAID 23.
-        mer_tx_23 = text(ha, 200, -29, '23', 'FontSize', tsize);
-        mer_tx_redploy = text(ha, 202, -31.5, '(redeployed)', 'FontSize', tsize, 'HorizontalAlignment', 'Center');
+        mer_tx_23 = text(ax_mer, 200, -29, '23', 'FontSize', tsize);
+        mer_tx_redploy = text(ax_mer, 202, -31.5, '(redeployed)', 'FontSize', tsize, 'HorizontalAlignment', 'Center');
 
     end
 
     days_deployed = days(locdate - locdate(1));
     col = x2color(days_deployed, [], max_days_deployed, cmap, false);
 
-    sc(i) = scatter(ha, lon, lat, scsize, col, 'filled');
-    mer_tx(i) = text(ha, lon(1), lat(1)+1, name{i}(3:4), 'FontSize', tsize);
+    sc(i) = scatter(ax_mer, lon, lat, scsize, col, 'filled');
+    mer_tx(i) = text(ax_mer, lon(1), lat(1)+1, name{i}(3:4), 'FontSize', tsize);
 
 end
 
@@ -149,8 +200,8 @@ end
 
 nb_lon(find(nb_lon<0)) = nb_lon(find(nb_lon<0)) + 360;
 for i = 1:length(nb_sta)
-    nb_pl(i) = plot(ha, nb_lon(i), nb_lat(i), 'kv', 'MarkerFaceColor', 'none');
-    nb_tx(i) = text(ha, nb_lon(i), nb_lat(i)+1, nb_sta{i}, 'FontSize', ...
+    nb_pl(i) = plot(ax_mer, nb_lon(i), nb_lat(i), 'kv', 'MarkerFaceColor', 'none');
+    nb_tx(i) = text(ax_mer, nb_lon(i), nb_lat(i)+1, nb_sta{i}, 'FontSize', ...
                     tsize, 'HorizontalAlignment', 'Center');
 
 end
@@ -163,8 +214,10 @@ maxlat = 4;
 maxlon = 251;
 minlat = -33;
 minlon = 176;
-ha.XLim = [minlon maxlon];
-ha.YLim = [minlat maxlat];
+ax_mer.XLim = [minlon maxlon];
+ax_mer.YLim = [minlat maxlat];
+%ax_mer.XLim = xlim;
+%ax_mer.YLim = ylim;
 box on
 
 cb.Label.Interpreter = 'latex';
@@ -186,7 +239,7 @@ cb.Label.String = 'Days since deployment';
 cb.FontSize = fs;
 cb.Label.FontSize = fs;
 
-longticks(ha, 3)
+longticks(ax_mer, 3)
 cb.TickDirection = 'out';
 cb.TickLength = 0.015;
 
@@ -239,151 +292,13 @@ lg.Color = 'None';
 %lg.Position(4) = 0.2;
 
 latimes
-set(ha, 'Color', 'None', 'Position', ha_bathy.Position)
-%tx = text(ha, 170, 5, '(a)', 'FontSize', 18, 'FontName', 'Helvetica', 'Interpreter', 'LaTex');
+set(ax_mer, 'Color', 'None', 'Position', ax_bathy.Position)
+%tx = text(ax_mer, 170, 5, '(a)', 'FontSize', 18, 'FontName', 'Helvetica', 'Interpreter', 'LaTex');
 
-ha.XTick = [];
-ha.YTick = [];
+ax_mer.XTick = [];
+ax_mer.YTick = [];
 movev(cb, -0.06)
 
 savepdf('merlocbathy')
 keyboard
 
-%%______________________________________________________________________________________%%
-%%                                   simon2020_bathy.m                                  %%
-%%______________________________________________________________________________________%%
-
-function ha = simon2020_bathy_local(plot_mermaids)
-% ha = SIMON2020_BATHY_LOCAL(PLOT_MERMAIDS)
-%
-% Local version of FJS' polynesia.m with some extra formatting touches.
-% (for him, last modified by fjsimons-at-alum.mit.edu, 03/12/2020)
-%
-% Modiyfing author: Joel D. Simon
-% Contact: jdsimon@alumni.princeton.edu | joeldsimon@gmail.com
-% Last modified: 16-Jun-2020, Version 9.3.0.948333 (R2017b) Update 9 on MACI64
-
-defval('plot_mermaids', true)
-
-close all
-
-load(fullfile(getenv('IFILES'),'TOPOGRAPHY','POLYNESIA','732c10d12f3c1ff02b85522b39bfd9ee1aa42244.mat'))
-
-% This from Joel Simon
-% http://ds.iris.edu/gmap/#network=*&starttime=2018-06-01&maxlat=4&maxlon=251&minlat=-33&minlon=176&drawingmode=box&planet=earth
-defval('c11',[176   4])
-defval('cmn',[251 -33])
-% Get the topography parameters
-defval('vers',2019);
-defval('npc',20);
-defval('mult',1); mult=round(mult);
-
-% Begin with a new figure, minimize it right away
-defval('fs',6);
-% Color limits
-% The reference global color rendition would be
-% imagefnan(c11,cmn,z,'demmap',[-7473 5731])
-% cax=[-4000 0.75*max(z(:))];
-% cax=halverange(minmax(z),75);
-defval('cax',[-7000 1500]);
-
-% Note that the print resolution for large images is worse than the
-% detail in the data themselves. One could force print with more dpi.
-clf
-% Color bar first...
-[cb,cm]=cax2dem(cax,'hor');
-% then map
-imagefnan(c11,cmn,z,cm,cax)
-% then colorbar again for adequate rendering
-[cb,cm]=cax2dem(cax,'hor');
-% Cosmetics
-% plotplates(c11,cmn) is just not good enough
-%deggies(gca)
-set(gca,'FontSize',fs)
-xlabel('Longitude')
-ylabel('Latitude')
-cb.XLabel.String=sprintf('GEBCO %i elevation (m)',vers);
-cb.XTick=unique([cb.XTick minmax(cax)]);
-warning off MATLAB:hg:shaped_arrays:ColorbarTickLengthScalar
-warning on MATLAB:hg:shaped_arrays:ColorbarTickLengthScalar
-
-%%______________________________________________________________________________________%%
-%%                                  Joel Edits                                          %%
-%%______________________________________________________________________________________%%
-
-fig2print(gcf, 'flandscape')
-
-xlim([176 251])
-ylim([-33 4])
-ha = gca;
-hold(ha, 'on')
-set(ha, 'DataAspectRatio', [1 1 1])
-
-fs = 13;
-
-cb.FontSize = fs;
-cb.Label.FontSize = fs;
-cb.Label.Interpreter = 'latex';
-
-movev(gca, 0.1)
-movev(cb, 0.1)
-cb.Ticks(9) = [];
-
-ha.XTick = [180:10:250];
-ha.XTickLabel = {'-180$^{\circ}$'  ...
-                 '-170$^{\circ}$' ...
-                 '-160$^{\circ}$' ...
-                 '-150$^{\circ}$' ...
-                 '-140$^{\circ}$' ...
-                 '-130$^{\circ}$' ...
-                 '-120$^{\circ}$' ...
-                 '-110$^{\circ}$'};
-
-
-ha.YTick = [-30:10:0];
-ha.YTickLabel = flip({'0$^{\circ}$' ...
-                    '-10$^{\circ}$' ...
-                    '-20$^{\circ}$' ...
-                    '-30$^{\circ}$'});
-
-longticks(ha, 3)
-cb.TickLength = 0.015;
-cb.TickDirection = 'out';
-
-axesfs(gcf, fs, fs+3)
-latimes
-cb.Location = 'EastOutside';
-
-
-%%______________________________________________________________________________________%%
-%%                    Overlay the MERMAID deployment locations
-%%______________________________________________________________________________________%%
-if plot_mermaids
-
-    % This is stripped from simon2020_globalstations.m
-
-    % Get the locations of all floats at the time of their deployment.
-    datadir = fullfile(getenv('SIMON2020_CODE'), 'data');
-    str = readtext(fullfile(datadir, 'misalo.txt'));
-
-    mlat = cellfun(@(xx) str2double(xx(31:40)), str);
-    mlon = cellfun(@(xx) str2double(xx(43:53)), str);
-
-    % Assuming this file is sorted...
-    P008_idx = cellstrfind(str, 'P008')';
-    P025_idx = cellstrfind(str, 'P025')';
-    Princeton_idx = P008_idx:P025_idx;
-
-    % Convert to from 0:180 = -180:0 to 0:360 longitude convection.
-    mlon(find(mlon<0)) = mlon(find(mlon<0)) + 360;
-
-    % Plot MERMAIDs.
-    pl = plot(ha, mlon(Princeton_idx), mlat(Princeton_idx), 'v', 'MarkerFaceColor', ...
-              porange, 'MarkerEdgeColor', 'k', 'MarkerSize', 12);
-
-
-end
-%%______________________________________________________________________________________%%
-
-moveh(ha, -0.03)
-moveh(cb.Label, 0.25)
