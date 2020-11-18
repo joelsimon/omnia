2cc10cac5b8983ed2d17447dde32ed18af1c393d
Joel D. Simon (Darwin) <jdsimon@princeton.edu>
2020-11-17 18:01:31 -0800

    Properly adjust lat/longitude limits
    Required I find the associated limits of the complete data vector, z.

--- mermaid/plotsouthpacificbathy.m
+++ mermaid/plotsouthpacificbathy.m
@@ -1,9 +1,9 @@
-function [ax, cb] = plotsouthpacificbathy(lat, lon, cax, filename)
-% [ax, cb] = PLOTSOUTHPACIFICBATHY(lat, lon, cax, filename)
+function [ax, cb] = plotsouthpacificbathy(lon, lat, cax, filename)
+% [ax, cb] = PLOTSOUTHPACIFICBATHY(lon, lat, cax, filename)
 %
 % Plot the bathymetry of the South Pacific
 %
-% lat       Latitude boundaries, northing [-90:90] (def: [-4 33])
+% lat       Latitude boundaries, southing [90:-90] (def: [4 -33])
 % lon       Longitude boundaries, westing [0:360] (def: [176 251])
 % cax       Colorbar limits, postive is meters below surface (def: -7000 1500)]
 % filename  Bathymetric .mat filename (from FJS)
@@ -15,19 +15,27 @@ function [ax, cb] = plotsouthpacificbathy(lat, lon, cax, filename)
 % Defaults
 % http://ds.iris.edu/gmap/#network=*&starttime=2018-06-01&maxlat=4&maxlon=251&minlat=-33&minlon=176&drawingmode=box&planet=earth
 defval('lon', [176 251])
-defval('lat', [-33 4])
+defval('lat', [4 -33])
 defval('cax',[-7000 1500]);
 defval('filename', fullfile(getenv('IFILES'),'TOPOGRAPHY','POLYNESIA','732c10d12f3c1ff02b85522b39bfd9ee1aa42244.mat'))
 
-% Parse lat/lon boundaries into matrix elements.
-c11 = [lon(1) lat(2)];
-cmn = [lon(2) lat(1)];
-
 % Adjust cax, if required, to avoid division by zero.
-cax(find(cax == 0)) = cax(find(cax == 0)) + 1e-3;
+cax(find(cax==0)) = cax(find(cax==0)) + 1e-3;
 
 % Load data .mat file
 load(filename)
+
+lonvals = linspace(175, 251, size(z, 2));
+lonidx =  nearestidx(lonvals, lon);
+
+latvals = linspace(4, -33, size(z, 1));
+latidx =  nearestidx(latvals, lat);
+
+z = z(latidx(1):latidx(2), lonidx(1):lonidx(2));
+
+% Parse lat/lon boundaries into matrix elements.
+c11 = [lon(1) lat(1)];
+cmn = [lon(2) lat(2)];
 
 % Color bar first...
 [cb, cm] = cax2dem(cax, 'hor');
