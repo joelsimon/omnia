9c57bdc80df2226df0d18a9ac1e6e5221951a2b1
Joel D. Simon (Darwin) <jdsimon@princeton.edu>
2020-11-18 15:27:22 -0800

    Introduce function to read loc.txt

---
+++ mermaid/readloc.m
@@ -0,0 +1,45 @@
+function loc = readloc(processed)
+% loc = READLOC(processed)
+%
+% Read interpolated MERMAID locations at the time of recording SAC/miniSEED files from text file
+% output by automaid v3.2.0+.
+%
+% Input:
+% processed     Processed directory output by automaid
+%                   (def: $MERMAID/processed)
+% Output:
+% loc           Interpolated GPS structure that parses loc.txt, organized by float name
+%
+% Author: Joel D. Simon
+% Contact: jdsimon@alumni.princeton.edu | joeldsimon@gmail.com
+% Last modified: 18-Nov-2020, Version 9.3.0.948333 (R2017b) Update 9 on MACI64
+
+% Default path.
+merpath = getenv('MERMAID');
+defval('processed', fullfile(merpath, 'processed'))
+
+% loc.txt format.
+fmt = '%40s    %10.6f    %11.6f    %4f\n';
+
+% Loop over every subdir in processed/ and read individual MERMAID's gps files.
+d = skipdotdir(dir(processed));
+for i = 1:length(d)
+    if d(i).isdir
+        % Dynamically name gps.(field) for individual MERMAIDs
+        mermaid = strrep(d(i).name(end-3:end), '-', '0');
+
+        % Read gps.txt file within individual float directory
+        file = fullfile(d(i).folder, d(i).name, 'loc.txt');
+        fid = fopen(file, 'r');
+        C  = textscan(fid, fmt, 'HeaderLines', 3);
+        fclose(fid);
+
+        % Parse (skip "|" partition of C{7})
+        loc.(mermaid).sac = cellfun(@(xx) [xx '.sac'], C{1}, 'UniformOutput', false);
+        loc.(mermaid).mseed = cellfun(@(xx) [xx '.mseed'], C{1}, 'UniformOutput', false);
+        loc.(mermaid).stla = C{2};
+        loc.(mermaid).stlo = C{3};
+        loc.(mermaid).stdp = C{4};
+        
+    end
+end
