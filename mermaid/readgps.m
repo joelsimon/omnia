6735324f1e463005d6b7efd06c5c4285afabea8d
Joel D. Simon (Darwin) <jdsimon@princeton.edu>
2020-12-08 09:58:39 -0800

    Swap gps.txt read for gps.csv read
    Also updated format slightly; e.g. for removal of "|" separator and
    swapping space between degrees and decimal minutes for underscore in the
    raw str lat/lon:
    
    "-22 01.463"  -> "-22_01.463"

--- mermaid/readgps.m
+++ mermaid/readgps.m
@@ -1,32 +1,40 @@
 function gps = readgps(processed)
 % gps = READGPS(processed)
 %
-% Read MERMAID GPS locations from text file output by automaid v3.2.0+.
+% Read MERMAID GPS locations from csv file output by automaid v3.3.0+.
 %
 % NB, P023 was out of the water on for GPS dates:
 % 2019-08-17T03:18:29Z
 % 2019-08-17T03:22:02Z
-% but those locations are NOT removed here. Be sure to remove them before processing, e.g., with
-% drifstats.m
+% but those locations are NOT removed here. Be sure to remove them before
+% processing, e.g., with driftstats.m
 %
 % Input:
 % processed     Processed directory output by automaid
 %                   (def: $MERMAID/processed)
 % Output:
-% gps           GPS structure that parses gps.txt, organized by float name
+% gps           GPS structure that parses gps.csv, organized by float name
 %
-% See also: driftstats.m, readloc.m
+% See also: driftstats.m
 %
 % Author: Joel D. Simon
 % Contact: jdsimon@alumni.princeton.edu | joeldsimon@gmail.com
-% Last modified: 18-Nov-2020, Version 9.3.0.948333 (R2017b) Update 9 on MACI64
+% Last modified: 08-Dec-2020, Version 9.3.0.948333 (R2017b) Update 9 on MACI64
 
 % Default path.
 merpath = getenv('MERMAID');
 defval('processed', fullfile(merpath, 'processed'))
 
-% gps.txt format.
-fmt = '%20s    %10.6f    %11.6f    %6.3f    %6.3f    %17.6f  %s  %15s    %3s %7s    %4s %7s\n';
+% gps.csv format.
+fmt = ['%s' ...
+       '%f' ...
+       '%f' ...
+       '%f' ...
+       '%f' ...
+       '%f' ...
+       '%s' ...
+       '%s' ...
+       '%s\n'];
 
 % Loop over every subdir in processed/ and read individual MERMAID's gps files.
 d = skipdotdir(dir(processed));
@@ -35,23 +43,23 @@ for i = 1:length(d)
         % Dynamically name gps.(field) for individual MERMAIDs
         mermaid = strrep(d(i).name(end-3:end), '-', '0');
 
-        % Read gps.txt file within individual float directory
-        file = fullfile(d(i).folder, d(i).name, 'gps.txt');
+        % Read gps.csv file within individual float directory
+        file = fullfile(d(i).folder, d(i).name, 'gps.csv');
         fid = fopen(file, 'r');
-        C  = textscan(fid, fmt, 'HeaderLines', 3);
+        C = textscan(fid, fmt, 'HeaderLines', 3, 'Delimiter', ',');
         fclose(fid);
 
-        % Parse (skip "|" partition of C{7})
+        % Parse.
         gps.(mermaid).time = C{1};
         gps.(mermaid).locdate = iso8601str2date(C{1});
         gps.(mermaid).lat = C{2};
         gps.(mermaid).lon = C{3};
         gps.(mermaid).hdop = C{4};
         gps.(mermaid).vdop = C{5};
-        gps.(mermaid).clockdrift = C{6};  % GPS_time - MERMAID_time
-        gps.(mermaid).source = C{8};
-        gps.(mermaid).rawstr_lat = cellfun(@(xx,yy) [xx ' ' yy], C{9}, C{10}, 'UniformOutput', false);
-        gps.(mermaid).rawstr_lon = cellfun(@(xx,yy) [xx ' ' yy], C{11}, C{12}, 'UniformOutput', false);
+        gps.(mermaid).clockdrift = C{6}; % GPS_time - MERMAID_time
+        gps.(mermaid).source = C{7};
+        gps.(mermaid).rawstr_lat = C{8}; % "[degrees]_[decimal minutes]"
+        gps.(mermaid).rawstr_lon = C{9};  % "[degrees]_[decimal minutes]"
 
     end
 end
