04981c8a53ab669d287436a9a5effa63719c2ec5
Joel D. Simon (Darwin) <jdsimon@princeton.edu>
2021-01-12 19:14:30 -0800

    Properly handle filled/unfilled location cases
    E.g.,
    'MH.P0008..BDH.D.2018.220.014200.SAC' vs
    'MH.P0008.00.BDH.D.2018.220.014200.SAC'

--- earthquakes/mseed2sac2date.m
+++ earthquakes/mseed2sac2date.m
@@ -1,7 +1,8 @@
 function tdate = mseed2sac2date(sac)
 % tdate = MSEED2SAC2DATE(sac)
 %
-% Returns the datetime (whole-second precision) of a SAC file converted with mseed2sac.
+% Returns the datetime (whole-second precision) of a SAC file converted with
+% mseed2sac and its default output filename.
 %
 % Input:
 % sac        MERMAID SAC filename (accepts cell arrays)
@@ -9,26 +10,40 @@ function tdate = mseed2sac2date(sac)
 % Output:
 % tdate      Datetime array of UTC starttimes
 %
-% Ex:
+% Ex: (same date; w/ & w/o location "00"; char or cell input)
 %    sac = 'MH.P0008..BDH.D.2018.220.014200.SAC'
+%    tdate = MSEED2SAC2DATE(sac)
+%    sac = 'MH.P0008.00.BDH.D.2018.220.014200.SAC'
+%    tdate = MSEED2SAC2DATE(sac)
+%    sac = {'MH.P0008..BDH.D.2018.220.014200.SAC' ...
+%           'MH.P0008.00.BDH.D.2018.220.014200.SAC'}
 %    tdate = MSEED2SAC2DATE(sac)
 %
 % See also: seistime.m
 %
 % Author: Joel D. Simon
 % Contact: jdsimon@alumni.princeton.edu | joeldsimon@gmail.com
-% Last modified: 27-Nov-2020, Version 9.3.0.948333 (R2017b) Update 9 on MACI64
+% Last modified: 12-Jan-2021, Version 9.3.0.948333 (R2017b) Update 9 on MACI64
 
+% Anonymous function to handle string splits
+splitter = @(xx) strsplit(xx, '.', 'CollapseDelimiters', false);
+
+% Collect list of SAC files
 sac = strippath(sac);
 if iscell(sac)
-    C = cellfun(@(xx) strsplit(xx, '.'), sac, 'UniformOutput', false);
-    tstr = cellfun(@(xx) [xx{5:end-1}], C, 'UniformOutput', false);
+    % Split each SAC file name at every period  '.'
+    C = cellfun(@(xx) splitter(xx), sac, 'UniformOutput', false);
+
+    % Construct a datestr from the split fields
+    tstr = cellfun(@(xx) [xx{6:end-1}], C, 'UniformOutput', false);
 
 else
-    C = strsplit(sac, '.');
-    tstr = [C{5:end-1}];
+    % Same as in the cellular case, minus the cells
+    C = splitter(sac);
+    tstr = [C{6:end-1}];
 
 end
 
+% Construct datetime array from fields parsed from the filename
 fmt = 'uuuuDDDHHmmss';
 tdate = datetime(tstr, 'InputFormat', fmt, 'TimeZone', 'UTC');
