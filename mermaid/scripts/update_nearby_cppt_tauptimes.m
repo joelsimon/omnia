90f059c4f08473500b048e22c9450c18fd90ee40
Joel D. Simon (Darwin) <jdsimon@princeton.edu>
2021-02-02 13:39:51 -0800

    Update EQ.TaupTimes for all nearby and CPPT events

---
+++ mermaid/scripts/update_nearby_cppt_tauptimes.m
@@ -0,0 +1,69 @@
+function update_nearby_cppt_tauptimes
+% UPDATE_NEARBY_CPPT_TAUPTIMES
+%
+% Runs updatetauptimes.m on all nearby and CPPT .evt files assuming JDS'
+% system defaults.
+%
+% Author: Joel D. Simon
+% Contact: jdsimon@alumni.princeton.edu | joeldsimon@gmail.com
+% Last modified: 02-Feb-2021, Version 9.3.0.948333 (R2017b) Update 9 on MACI64
+
+mer_dir = fullfile(getenv('MERMAID'), 'events');
+nearby_dir =  skipdotdir(dir(fullfile(mer_dir, 'nearbystations', 'evt')));
+cppt_dir =  skipdotdir(dir(fullfile(mer_dir, 'cpptstations', 'evt')));
+
+nearby_id = {nearby_dir.name};
+cppt_id = {cppt_dir.name};
+
+for i = 1:2
+    [sacfile, sacfile_u] = getnearbysac(nearby_id{i}, 'vel');
+    sacfile = [sacfile ; sacfile_u];
+
+    [EQ, EQ_u, evtfile, evtfile_u] = getnearbyevt(nearby_id{i});
+    EQ = [EQ ; EQ_u];
+    evtfile = [evtfile ; evtfile_u];
+
+    for j = 1:length(sacfile)
+        [~,h] = readsac(sacfile{j});
+
+        if ~isempty(EQ{j})
+            isupdated = updatetauptimes(sacfile{j}, evtfile{j});
+
+            if isupdated
+                fprintf('Yes: %i\n', h.NZMSEC)
+
+            else
+                fprintf('No: %i\n', h.NZMSEC)
+                if h.NZMSEC < 100 & h.NZMSEC ~= 0
+                    error(sprintf('%s', sacfile{j}))
+
+                end
+            end
+        end
+    end
+end
+
+for i = 1:2
+    sacfile = getcpptsac(cppt_id{i}, 'vel');
+
+    [EQ, evtfile] = getcpptevt(cppt_id{i});
+
+    for j = 1:length(sacfile)
+        [~,h] = readsac(sacfile{j});
+
+        if ~isempty(EQ{j})
+            isupdated = updatetauptimes(sacfile{j}, evtfile{j});
+
+            if isupdated
+                fprintf('Yes: %i\n', h.NZMSEC)
+
+            else
+                fprintf('No: %i\n', h.NZMSEC)
+                if h.NZMSEC < 100 & h.NZMSEC ~= 0
+                    error(sprintf('%s', sacfile{j}))
+
+                end
+            end
+        end
+    end
+end
