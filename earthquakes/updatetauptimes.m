8fe6664dbe00ae79d2ae114c1ff4e150696183af
Joel D. Simon (Darwin) <jdsimon@princeton.edu>
2021-03-05 11:10:19 -0800

    Clarify header

--- earthquakes/updatetauptimes.m
+++ earthquakes/updatetauptimes.m
@@ -1,6 +1,17 @@
 function isupdated = updatetauptimes(sacfile, evtfile)
 % isupdated = UPDATETAUPTIMES(sacfile, evtfile)
 %
+% Overwrite reviewed EQ.TaupTimes.
+%
+% UPDATETAUPTIMES updates the phase-arrival times due to a change in station
+% or event metadata.
+%
+% UPDATETAUPTIMES does not update the station or event metadata themselves
+% (see updateid.m to accomplish the latter).
+%
+% UPDATETAUPTIMES does not update the phase-arrival times of the corresponding
+% raw events, nor does it update the corresponding raw or reviewed .CP files.
+%
 % Input:
 % sacfile     Fullpath .SAC filename
 % evtfile     Fullpath .evt (EQ structure) filename
@@ -8,9 +19,14 @@ function isupdated = updatetauptimes(sacfile, evtfile)
 % Output:
 % isupdated   true if evtfile updated with new EQ struct (def: false)
 %
+% Output:
+% isupdated   Logical true/false alerting if EQ.TaupTimes updated
+%
+% See also: updateid.m, updatestloc.m (a wrapper; uses SAC filename)
+%
 % Author: Joel D. Simon
 % Contact: jdsimon@alumni.princeton.edu | joeldsimon@gmail.com
-% Last modified: 02-Feb-2021, Version 9.3.0.948333 (R2017b) Update 9 on MACI64
+% Last modified: 05-Mar-2021, Version 9.3.0.948333 (R2017b) Update 9 on MACI64
 
 % Read the EQ structure.
 tmp = load(evtfile, 'EQ', '-mat');
@@ -31,7 +47,7 @@ if ~strcmpi(sac_nopath, EQ.Filename)
     error('sacfile and evtfile do not correspond to one another')
 
 end
-    
+
 if isupdated
     EQ = new_EQ;
     save(evtfile, 'EQ', '-mat')
