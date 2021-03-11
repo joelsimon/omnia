abb6165acd4fee27914cc9a42c6e81bec1ad7ad9
Joel D. Simon (Darwin) <jdsimon@princeton.edu>
2021-03-10 17:13:01 -0800

    Allow custom map rotation

--- earthquakes/plateboundaries.m
+++ earthquakes/plateboundaries.m
@@ -1,19 +1,22 @@
-function [lat, lon, lat_360, lon_360] = plateboundaries
-% [lat, lon, lat_360, lon_360] = PLATEBOUNDARIES
+function [lat, lon, lat_360, lon_360] = plateboundaries(left_lon)
+% [lat, lon, lat_360, lon_360] = PLATEBOUNDARIES(left_lon)
 %
 % Return Earth's plate boundaries, contained in $IFILES/PLATES/plates.mtl.
 %
 % Input:
-% N/A
+% left_lon            Longitude at left of map; longitude between [-180:0] (def: -180)
 %
 % Output:
-% lat/lon            Latitude/longitude, latter being between -180:180 degrees
-% lat_360/lon_360    Latitude/longitude, latter being between 0:360 degrees
+% lat/lon            Latitude/longitude; longitude between [left_lon:360-left_lon]
+% lat_360/lon_360    Latitude/longitude, longitude between [0:360]
 %
 % Ex:
 % [lat, lon, lat_360, lon_360] = PLATEBOUNDARIES
-% figure; plot(lon, lat); xlim([-180 180]); ylim([-90 90]); box on
-% figure; plot(lon_360, lat_360); xlim([0 360]); ylim([-90 90]); box on
+% figure; plot(lon, lat, 'r'); xlim([-180 180]); ylim([-90 90]); box on
+% figure; plot(lon_360, lat_360, 'b'); xlim([0 360]); ylim([-90 90]); box on
+% [lat_neg20, lon_neg20] = PLATEBOUNDARIES(-20)
+% figure; plot(lon_neg20, lat_neg20, 'k'); xlim([-20 340]); ylim([-90 90]); box on
+% figure; hold on; plot(lon, lat, 'r'); plot(lon_neg20, lat_neg20, 'k'); plot(lon_360, lat_360, 'b')
 %
 % Author: Joel D. Simon
 % Contact: jdsimon@alumni.princeton.edu | joeldsimon@gmail.com
@@ -21,8 +24,15 @@ function [lat, lon, lat_360, lon_360] = plateboundaries
 
 % Wish list:
 %
-% *Average lat/lon around the edges so that the lines truly extended to the
-%  map edges of [-180 180] or [0 360]
+% *Average lat/lon around the longitude wrap so that the plate truly extended to
+%  the edges of the map
+
+% Default left edge at longitude: -180 degrees (180 W).
+defval('left_lon', -180)
+if left_lon < -180 || left_lon > 0
+    error('Input must be between 0 and 180')
+
+end
 
 %% Lifted from '$OMNIA/notmycode/fjs/plotplates.m'
 pathname = getenv('IFILES');
@@ -38,14 +48,16 @@ lat_360 = plates(:,2);
 lon = lon_360;
 lat = lat_360;
 
-idx = find(lon > 180);
+% Rotate longitudes from 0:360 to -180:180, or some other custom rotation.
+wrap_deg = 360+left_lon;
+idx = find(lon > wrap_deg);
 lon(idx) = lon(idx) - 360;
 
-% Find where the longitudes wrap by a large amount (screwing up plot) and
-% insert NaNs between those breaks.
-wrap = find(abs(diff(lon)) >= 180);
-lon = zipnanshift(lon, wrap, 'after');
-lat = zipnanshift(lat, wrap, 'after');
+% Locate the indices where the rotation caused longitudes to wrap and slide
+% NaNs between them to break up the lines.
+wrap_idx = find(abs(diff(lon)) >= wrap_deg);
+lon = zipnanshift(lon, wrap_idx, 'after');
+lat = zipnanshift(lat, wrap_idx, 'after');
 
 % Find and remove all occurrences where longitude goes [-, NaN, +], or [+, NaN, -]
 % (crosses the Prime Meridian) because those are set as NaN on FJS' original
@@ -65,7 +77,8 @@ for i = 1:length(nan_idx)
         left = lon(nidx-1);
         right = lon(nidx+1);
 
-        % Remove them if there is a sign flip (crosses 0 degrees)
+        % Remove the middle NaN if it there is a sign flip on either side of it,
+        % signaling that the plate crosses 0 degrees and should connect.
         if (sign(left) ~= sign(right)) && abs(left-right) < maxdiff
             rm_idx = [rm_idx ; nidx];
 
