614d16cc075dedc751f927f3cd1ff991bfab340b
Joel D. Simon (Darwin) <jdsimon@princeton.edu>
2021-05-13 19:29:56 -0700

    Add max start/end time diffs to printout
    Also change text file-printing format to exponential vs float to more
    quickly spot order-of-magnitude differences

--- mermaid/automaidversions.m
+++ mermaid/automaidversions.m
@@ -1,5 +1,5 @@
-function [max_lat_diff, max_lon_diff] = automaidversions(mustequate, old, new, fname)
-% [max_lat_diff, max_lon_diff] = AUTOMAIDVERSIONS(mustequate, old, new, fname)
+function [max_dateB_diff, max_dateE_diff, max_lat_diff, max_lon_diff] = automaidversions(mustequate, old, new, fname)
+% [max_dateB_diff, max_dateE_diff, max_lat_diff, max_lon_diff] = AUTOMAIDVERSIONS(mustequate, old, new, fname)
 %
 % Compares SAC files compiled with different automaid versions.
 %
@@ -12,7 +12,7 @@ function [max_lat_diff, max_lon_diff] = automaidversions(mustequate, old, new, f
 %
 % Author: Joel D. Simon
 % Contact: jdsimon@alumni.princeton.edu | joeldsimon@gmail.com
-% Last modified: 05-Nov-2020, Version 9.3.0.948333 (R2017b) Update 9 on MACI64
+% Last modified: 13-May-2021, Version 9.3.0.948333 (R2017b) Update 9 on MACI64
 
 defval('old', fullfile(getenv('MERMAID'), 'processed'))
 defval('new', fullfile(getenv('MERMAID'), 'test_processed'))
@@ -41,11 +41,22 @@ end
 fid = fopen(fname, 'w');
 fmt = '%s\n';
 
-fprintf('Allowed time difference: %.6f s\n', allowed_date_diff)
+fprintf('Allowed (absolute) time difference: %.6f s\n', allowed_date_diff)
 fprintf('Allowed (absolute) distance difference: %.8f deg\n\n', allowed_dist_diff)
 
+% Define anonymous function to capture order-of-magnitude differences.
+get_exp = @(xx) floor(log10(xx));
+
+max_dateB_diff = 0;
+max_dateE_diff = 0;
 max_lat_diff = 0;
 max_lon_diff = 0;
+
+max_dateB_sac = '';
+max_dateE_sac = '';
+max_lat_sac = '';
+max_lon_sac = '';
+
 for i = 1:length(sac_old)
     [data_old, header_old] = readsac(sac_old{i});
     seisdate_old = seistime(header_old);
@@ -62,44 +73,64 @@ for i = 1:length(sac_old)
 
     end
 
+    % Start time
     actual_dateB_diff = seconds(seisdate_old.B - seisdate_new.B);
     if abs(actual_dateB_diff) > allowed_date_diff
-        fprintf(fid, fmt, sprintf('%s    date.B     %15.6f sec', strippath(sac_old{i}), actual_dateB_diff));
+        fprintf(fid, fmt, sprintf('%s    date.B     10^%+i sec', strippath(sac_old{i}), get_exp(actual_dateB_diff)));
+        %fprintf(fid, fmt, sprintf('%s    date.B     %15.6f sec', strippath(sac_old{i}), actual_dateB_diff));
+
+    end
+    if abs(actual_dateB_diff) > abs(max_dateB_diff)
+        max_dateB_diff = actual_dateB_diff;
+        max_dateB_sac = sac_old{i};
 
     end
 
+    % End time
     actual_dateE_diff = seconds(seisdate_old.E - seisdate_new.E);
     if abs(actual_dateE_diff) > allowed_date_diff
-        fprintf(fid, fmt, sprintf('%s    date.E     %15.6f sec', strippath(sac_old{i}), actual_dateE_diff));
+        fprintf(fid, fmt, sprintf('%s    date.E     10^%+i sec', strippath(sac_old{i}), get_exp(actual_dateE_diff)));
+        %fprintf(fid, fmt, sprintf('%s    date.E     %15.6f sec', strippath(sac_old{i}), actual_dateE_diff));
+
+
+    end
+    if abs(actual_dateE_diff) > abs(max_dateE_diff)
+        max_dateE_diff = actual_dateE_diff;
+        max_dateE_sac = sac_old{i};
 
     end
 
+    % Latitude
     actual_lat_diff = lat_old - lat_new;
     if abs(actual_lat_diff) > allowed_dist_diff
-        fprintf(fid, fmt, sprintf('%s       lat     %15.6f deg', strippath(sac_old{i}), actual_lat_diff));
+        fprintf(fid, fmt, sprintf('%s       lat     10^%+i deg', strippath(sac_old{i}), get_exp(actual_lat_diff)));
+        %fprintf(fid, fmt, sprintf('%s       lat     %15.6f deg', strippath(sac_old{i}), actual_lat_diff));
 
     end
     if abs(actual_lat_diff) >  abs(max_lat_diff)
         max_lat_diff = actual_lat_diff;
-        max_lat_sac = sac_new{i};
+        max_lat_sac = sac_old{i};
 
     end
 
+    % Longitude
     actual_lon_diff = lon_old - lon_new;
     if abs(actual_lon_diff) > allowed_dist_diff
-        fprintf(fid, fmt, sprintf('%s       lon     %15.6f deg', strippath(sac_old{i}), actual_lon_diff));
+        fprintf(fid, fmt, sprintf('%s       lon     10^%+i deg', strippath(sac_old{i}), get_exp(actual_lon_diff)));
+        %fprintf(fid, fmt, sprintf('%s       lon     %15.6f deg', strippath(sac_old{i}), actual_lon_diff));
 
     end
     if abs(actual_lon_diff) >  abs(max_lon_diff)
         max_lon_diff = actual_lon_diff;
-        max_lon_sac = sac_new{i};
+        max_lon_sac = sac_old{i};
 
     end
-
 end
 fprintf('Wrote %s\n\n', fname)
 
 fprintf('Comparing --\n(old): %s\n(new): %s\n', old, new)
 
-fprintf('Max lat: %+.8f deg or %+i m (%s)\n', max_lat_diff, round(deg2km(max_lat_diff)*1000), strippath(max_lat_sac))
-fprintf('Max lon: %+.8f deg or %+i m (%s)\n', max_lon_diff, round(deg2km(max_lon_diff)*1000), strippath(max_lon_sac))
+fprintf('Max dateB: %+.8f s (%s)\n', max_dateB_diff, strippath(max_dateB_sac))
+fprintf('Max dateE: %+.8f s (%s)\n', max_dateE_diff, strippath(max_dateE_sac))
+fprintf('Max lat:   %+.8f deg or %+i m (%s)\n', max_lat_diff, round(deg2km(max_lat_diff)*1000), strippath(max_lat_sac))
+fprintf('Max lon:   %+.8f deg or %+i m (%s)\n', max_lon_diff, round(deg2km(max_lon_diff)*1000), strippath(max_lon_sac))
