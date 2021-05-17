50b56743527604aa22cf614149f25e2382b94385
Joel D. Simon (Darwin) <jdsimon@princeton.edu>
2021-05-17 14:49:59 -0700

    Swap default directories and print automaid versions
    Also, change all printout to 6-decimal precision from 8-decimal
    precision.

--- mermaid/automaidversions.m
+++ mermaid/automaidversions.m
@@ -4,18 +4,21 @@ function [max_dateB_diff, max_dateE_diff, max_lat_diff, max_lon_diff] = automaid
 % Compares SAC files compiled with different automaid versions.
 %
 % Input:
-% mustequate    true: data/location values must be exactly the same
+% mustequate    true: data/location values must be exactly the same (def)
 %               false: data/location values must be same within sensible range
-% old           Directory containing old SAC files (def: $MERMAID/processed)
-% new           Directory containing new SAC files (def: $MERMAID/test_processed)
+% old           Directory containing old SAC files (def: $MERMAID/test_processed)
+% new           Directory containing new SAC files (def: $MERMAID/processed)
 % fname         Output file name (def: '~/automaidversions_diff[_full/_light].txt')
 %
 % Author: Joel D. Simon
 % Contact: jdsimon@alumni.princeton.edu | joeldsimon@gmail.com
-% Last modified: 13-May-2021, Version 9.3.0.948333 (R2017b) Update 9 on MACI64
+% Last modified: 17-May-2021, Version 9.3.0.948333 (R2017b) Update 9 on MACI64
 
-defval('old', fullfile(getenv('MERMAID'), 'processed'))
-defval('new', fullfile(getenv('MERMAID'), 'test_processed'))
+clc
+
+defval('mustequate', true)
+defval('old', fullfile(getenv('MERMAID'), 'test_processed'))
+defval('new', fullfile(getenv('MERMAID'), 'processed'))
 defval('fname', fullfile(getenv('HOME'), 'automaidversions_diff'))
 
 sac_old = fullsac([], old);
@@ -41,8 +44,8 @@ end
 fid = fopen(fname, 'w');
 fmt = '%s\n';
 
-fprintf('Allowed (absolute) time difference: %.6f s\n', allowed_date_diff)
-fprintf('Allowed (absolute) distance difference: %.8f deg\n\n', allowed_dist_diff)
+fprintf('Allowed (absolute) time difference:     %.6f sec\n', allowed_date_diff)
+fprintf('Allowed (absolute) distance difference: %.6f deg (%.1f m)\n\n', allowed_dist_diff, deg2km(allowed_dist_diff)*1000)
 
 % Define anonymous function to capture order-of-magnitude differences.
 get_exp = @(xx) floor(log10(xx));
@@ -57,12 +60,17 @@ max_dateE_sac = '';
 max_lat_sac = '';
 max_lon_sac = '';
 
+version_old = {};
+version_new = {};
+
 for i = 1:length(sac_old)
+    % Read old data
     [data_old, header_old] = readsac(sac_old{i});
     seisdate_old = seistime(header_old);
     lat_old = header_old.STLA;
     lon_old = header_old.STLO;
 
+    % Read new data
     [data_new, header_new] = readsac(sac_new{i});
     seisdate_new = seistime(header_new);
     lat_new = header_new.STLA;
@@ -125,12 +133,24 @@ for i = 1:length(sac_old)
         max_lon_sac = sac_old{i};
 
     end
+
+    % Version numbers
+    version_old = [version_old ; {header_old.KUSER0}];
+    version_new = [version_new ; {header_new.KUSER0}];
+
 end
+version_old = unique(version_old);
+version_new = unique(version_new);
+
 fprintf('Wrote %s\n\n', fname)
 
 fprintf('Comparing --\n(old): %s\n(new): %s\n', old, new)
 
-fprintf('Max dateB: %+.8f s (%s)\n', max_dateB_diff, strippath(max_dateB_sac))
-fprintf('Max dateE: %+.8f s (%s)\n', max_dateE_diff, strippath(max_dateE_sac))
-fprintf('Max lat:   %+.8f deg or %+i m (%s)\n', max_lat_diff, round(deg2km(max_lat_diff)*1000), strippath(max_lat_sac))
-fprintf('Max lon:   %+.8f deg or %+i m (%s)\n', max_lon_diff, round(deg2km(max_lon_diff)*1000), strippath(max_lon_sac))
+fprintf('Max dateB: %+.6f s (%s)\n', max_dateB_diff, strippath(max_dateB_sac))
+fprintf('Max dateE: %+.6f s (%s)\n', max_dateE_diff, strippath(max_dateE_sac))
+fprintf('Max lat:   %+.6f deg or %+i m (%s)\n', max_lat_diff, round(deg2km(max_lat_diff)*1000), strippath(max_lat_sac))
+fprintf('Max lon:   %+.6f deg or %+i m (%s)\n', max_lon_diff, round(deg2km(max_lon_diff)*1000), strippath(max_lon_sac))
+fprintf('Old versions:\n')
+fprintf(' %s\n', version_old{:})
+fprintf('New versions:\n')
+fprintf(' %s\n', version_new{:})
