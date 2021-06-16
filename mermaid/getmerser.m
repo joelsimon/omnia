a6e8d5b7a753e19b1a03aa0b9e0db62f30332020
Joel D. Simon (Darwin) <jdsimon@princeton.edu>
2021-06-15 18:42:02 -0700

    Return MERMAID serial number given a SAC filename

---
+++ mermaid/getmerser.m
@@ -0,0 +1,20 @@
+function ser = getmerser(sacfile)
+% ser = GETMERSER(sacfile)
+%
+% Return MERMAID serial number given a SAC filename.
+%
+% Input:
+% sacfile     SAC filename following automaid v3.4+ convention
+%
+% Output:
+% ser        Two-digit MERMAID serial number
+%
+% Ex:
+%    sacfile = '20180819T042909.08_5B7A4C26.MER.DET.WLT5.sac'
+%    ser = GETMERSER(sacfile)
+%
+% Author: Joel D. Simon
+% Contact: jdsimon@alumni.princeton.edu | joeldsimon@gmail.com
+% Last modified: 15-Jun-2021, Version 9.3.0.948333 (R2017b) Update 9 on MACI64
+
+ser = fx(strsplit(fx(strsplit(strippath(sacfile), '.'), 2), '_'), 1);
