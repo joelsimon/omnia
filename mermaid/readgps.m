5c4f3e3592bbaf23dd258af1d47b6db403bf8b1c
Joel D. Simon (Darwin) <jdsimon@princeton.edu>
2021-09-21 15:04:52 -0700

    Add clockfreq, return five character station names

--- mermaid/readgps.m
+++ mermaid/readgps.m
@@ -1,9 +1,11 @@
 function gps = readgps(processed, rm23)
 % gps = READGPS(processed, rm23)
 %
-% Read MERMAID GPS locations from 'gps.csv' file output by automaid v3.3.0+.
+% Read MERMAID GPS locations from 'gps.csv' file output by automaid v3.4.0-K+
+% (when "clockfreq" column was added). Note that gps.(mermaid).locdate may
+% include redundant datetimes.
 %
-% NB, P023 was out of the water around the GPS dates of --
+% NB, P0023 was out of the water around the GPS dates of --
 % 2019-08-17T03:18:29Z
 % 2019-08-17T03:22:02Z
 % but unless `rm23` is true those locations are NOT removed here.
@@ -14,7 +16,7 @@ function gps = readgps(processed, rm23)
 %                   (def: $MERMAID/processed)
 % rm23          Replace GPS data associated with dates:
 %                   '2019-08-17T03:18:29Z' and '2019-08-17T03:22:02Z'
-%                   (when P023 was out of the water) with '', NaN, NaT
+%                   (when P0023 was out of the water) with '', NaN, NaT
 %                   (def: True)
 %
 % Output:
@@ -24,10 +26,7 @@ function gps = readgps(processed, rm23)
 %
 % Author: Joel D. Simon
 % Contact: jdsimon@alumni.princeton.edu | joeldsimon@gmail.com
-% Last modified: 17-Apr-2021, Version 9.3.0.948333 (R2017b) Update 9 on MACI64
-
-% "clockfreq" column added v3.4.0-K
-error('Must adjust based on automaid version')
+% Last modified: 17-Sep-2021, Version 9.3.0.948333 (R2017b) Update 9 on MACI64
 
 % Default path.
 merpath = getenv('MERMAID');
@@ -36,6 +35,7 @@ defval('rm23', true)
 
 % gps.csv format.
 fmt = ['%s' ...
+       '%f' ...
        '%f' ...
        '%f' ...
        '%f' ...
@@ -50,7 +50,7 @@ d = skipdotdir(dir(processed));
 for i = 1:length(d)
     if d(i).isdir
         % Dynamically name gps.(field) for individual MERMAIDs
-        mermaid = strrep(d(i).name(end-3:end), '-', '0');
+        mermaid = osean2fdsn(d(i).name);
 
         % Read gps.csv file within individual float directory
         file = fullfile(d(i).folder, d(i).name, 'gps.csv');
@@ -66,10 +66,16 @@ for i = 1:length(d)
         gps.(mermaid).hdop = C{4};
         gps.(mermaid).vdop = C{5};
         gps.(mermaid).clockdrift = C{6}; % GPS_time - MERMAID_time
-        gps.(mermaid).source = C{7};
-        gps.(mermaid).rawstr_lat = C{8}; % "[degrees]_[decimal minutes]"
-        gps.(mermaid).rawstr_lon = C{9};  % "[degrees]_[decimal minutes]"
+        gps.(mermaid).clockfreq = C{7};
+        gps.(mermaid).source = C{8};
+        gps.(mermaid).rawstr_lat = C{9}; % "[degrees]_[decimal minutes]"
+        gps.(mermaid).rawstr_lon = C{10};  % "[degrees]_[decimal minutes]"
 
+        % Verify sorting.
+        if ~isequal(gps.(mermaid).locdate, sort(gps.(mermaid).locdate))
+            error('GPS unsorted')
+
+        end
     end
 end
 
@@ -82,7 +88,7 @@ if rm23
     % after being redeployed, which would incorrectly compute the velocity of
     % the boat.
     %
-    % >> gps.P023.lat(386:395)  % Indices may change with rewrite of 'gps.csv'
+    % >> gps.P0023.lat(386:395)  % Indices may change with rewrite of 'gps.csv'
     %  -23.8419
     %  -23.8425
     %  -23.8436
@@ -94,7 +100,7 @@ if rm23
     %  -30.2723
     %  -30.2720
     %
-    % >> gps.P023.lon(386:395)
+    % >> gps.P0023.lon(386:395)
     % ans =
     %  -143.5497
     %  -143.5499
@@ -108,17 +114,17 @@ if rm23
     %  -157.9971
 
     bad_dates = iso8601str2date({'2019-08-17T03:18:29Z' '2019-08-17T03:22:02Z'});
-    [~, bad_idx] = intersect(gps.P023.locdate, bad_dates);
+    [~, bad_idx] = intersect(gps.P0023.locdate, bad_dates);
 
-    gps.P023.time(bad_idx) = {''};
-    gps.P023.locdate(bad_idx) = NaT;
-    gps.P023.lat(bad_idx) = NaN;
-    gps.P023.lon(bad_idx) = NaN;
-    gps.P023.hdop(bad_idx) = NaN;
-    gps.P023.vdop(bad_idx) = NaN;
-    gps.P023.clockdrift(bad_idx) = NaN;
-    gps.P023.source(bad_idx) = {''};
-    gps.P023.rawstr_lat(bad_idx) = {''};
-    gps.P023.rawstr_lon(bad_idx) = {''};
+    gps.P0023.time(bad_idx) = {''};
+    gps.P0023.locdate(bad_idx) = NaT;
+    gps.P0023.lat(bad_idx) = NaN;
+    gps.P0023.lon(bad_idx) = NaN;
+    gps.P0023.hdop(bad_idx) = NaN;
+    gps.P0023.vdop(bad_idx) = NaN;
+    gps.P0023.clockdrift(bad_idx) = NaN;
+    gps.P0023.source(bad_idx) = {''};
+    gps.P0023.rawstr_lat(bad_idx) = {''};
+    gps.P0023.rawstr_lon(bad_idx) = {''};
 
 end
