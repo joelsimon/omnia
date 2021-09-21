aed76c61c25a8f92cd3be25983fcd732df0f3b57
Joel D. Simon (Darwin) <jdsimon@princeton.edu>
2021-09-21 14:44:27 -0700

    Return reviewed EQ struct only (skip unreviewed)

---
+++ earthquakes/getrevevt.m
@@ -0,0 +1,51 @@
+function [revEQ, rev_evt] = getrevevt(sac, evtdir)
+% [revEQ, rev_evt] = GETREVEVT(sac, evtdir)
+%
+% Like getevt.m, but only return the reviewed .evt EQ structure.
+%
+% Useful, e.g., especially for REQ files where a reviewed .evt file saved
+% without any associated .raw.evt or .raw.pdf files in the unreviewed directory,
+% omissions that would otherwise break getevt.m.
+%
+% Input:
+% sac       SAC filename
+%               (def: '20180819T042909.08_5B7A4C26.MER.DET.WLT5.sac')
+% evtdir    Path to directory containing 'raw/' and 'reviewed'
+%               subdirectories (def: $MERMAID/events/)
+% Output:
+% revEQ     Reviewed EQ structure
+%               EQ = [] means .evt file exists and event unidentified
+%               EQ = NaN means .evt file does not exist
+% rev_evt   Full path to .evt file containing EQ structure
+%
+% See also: getevt.m
+%
+% Author: Joel D. Simon
+% Contact: jdsimon@alumni.princeton.edu | joeldsimon@gmail.com
+% Last modified: 20-Sep-2021, Version 9.3.0.948333 (R2017b) Update 9 on MACI64
+
+% Defaults.
+defval('sac', '20180819T042909.08_5B7A4C26.MER.DET.WLT5.sac')
+defval('evtdir', fullfile(getenv('MERMAID'), 'events'))
+
+% Use dir.m recursive search to look through 'identified/', 'unidentified/, and
+% 'purgatory/' subdirectories in 'reviewed'.
+sac_name = strippath(sac);
+evt_name = strrep(sac_name, '.sac', '.evt');
+
+rev_dir = dir(fullfile(evtdir, 'reviewed', '**/*', evt_name));
+if isempty(rev_dir)
+    warning('%s not found in %s', evt_name, rev_dir.folder)
+    revEQ = NaN;
+    rev_evt = [];
+
+else
+    rev_evt = fullfile(rev_dir.folder, rev_dir.name);
+    if contains(rev_evt, 'purgatory')
+        warning('\n%s in purgatory...perhaps not reviewed', sac_name)
+
+    end
+    rev_tmp = load(rev_evt, '-mat');
+    revEQ = rev_tmp.EQ;
+
+end
