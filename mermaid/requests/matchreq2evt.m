a56e61dced54fc45269ed904564dd1a74211eafa
Joel D. Simon (Darwin) <jdsimon@princeton.edu>
2021-11-23 13:30:04 -0800

    Add model and phase options and make recursive

--- mermaid/requests/matchreq2evt.m
+++ mermaid/requests/matchreq2evt.m
@@ -1,35 +1,55 @@
-function matchreq2evt(sacfile, eventid, evtdir)
-% MATCHREQ2EVT(SACFILE, EVENTID, EVTDIR)
+function [EQ, evtfile] = matchreq2evt(sacfile, eventid, model, phases, eventdir)
+% [EQ, evtfile] = MATCHREQ2EVT(sacfile, eventid, eventdir, phases)
 %
 % Writes .evt of identified earthquake associated with requested .sac file.
 % (really, this will work for any SAC, not just requested SAC...)
 %
 % Input:
-% sacfile    SAC filename
+% sacfile    .sac filename (or cell array of filenames)
 % eventid    IRIS event ID to associated with SAC filename
-% evtdir     Path to directory containing 'raw/' and 'reviewed'
-%                 subdirectories (def: $MERMAID/events/)
+% eventdir   Path to directory containing 'raw/' and 'reviewed'
+%                subdirectories (def: $MERMAID/events/)
+% model      Model in which to compute phase arrival times (def: 'ak135')
+% phases     Phases to compute arrival times for in EQ.TaupTimes struct
+%                (def: [defphases  ',4kmps,1.5kmps'])*
+% Output:
+% EQ         EQ structure from sac2evt.m
+% evtfile    .evt filename saved by this function
+%
+% * Approx. velocity of first-arriving surface waves (4kmps Rayleigh, 1.5kmps Scholte [and T wave])
+%   Scholte ~ 0.1--0.3 Hz (low frequency)
+%   T-wave ~ 5 Hz+ (high frequency)
+%   See especially Fig. 3 of Hable et al., 2019 [doi: 10.1093/gji/ggz333]
 %
 % Author: Joel D. Simon
 % Contact: jdsimon@alumni.princeton.edu | joeldsimon@gmail.com
-% Last modified: 22-Nov-2021, Version 9.3.0.948333 (R2017b) Update 9 on MACI64
+% Last modified: 23-Nov-2021, Version 9.3.0.948333 (R2017b) Update 9 on MACI64
+
+%% Recursive
 
 % Default directory path.
-defval('evtdir', fullfile(getenv('MERMAID'), 'events'));
+defval('eventdir', fullfile(getenv('MERMAID'), 'events'));
+defval('model', 'ak135')
+defval('phases', [defphases  ',4kmps,1.5kmps'])
 
-% Use ak135 model and compute arrival times for all default phases plus the
-% fastest surface waves (4 kmps Rayleigh, 1.5 kmps Scholte and T wave).
-% T-wave  ~ 5 Hz+ (high freq)
-% Scholte ~ 0.1 - 0.3 Hz (low freq)
-% [search for Scholte, T-wave at same arrival time but differing frequency bands]
-phases = [defphases  ',4kmps,1.5kmps'];
+if iscell(sacfile)
+
+    %% Recursion.
+
+    for i = 1:length(sacfile)
+        [EQ(i), evtfile{i}] = matchreq2evt(sacfile{i}, eventid, model, phases, eventdir);
+
+    end
+    return
+
+end
 
 % Fetch most up-to-date info from IRIS.
-EQ = sac2evt(sacfile, [], phases, [], 'eventid', eventid);
+EQ = sac2evt(sacfile, model, phases, [], 'eventid', num2str(eventid));
 
 % Save EQ struct in .evt file.
 evtfile = strrep(strippath(sacfile), '.sac', '.evt');
-evtfile = fullfile(evtdir, 'reviewed', 'identified', 'evt', evtfile);
+evtfile = fullfile(eventdir, 'reviewed', 'identified', 'evt', evtfile);
 save(evtfile, 'EQ', '-mat')
 
 fprintf('Wrote %s\n', evtfile)
