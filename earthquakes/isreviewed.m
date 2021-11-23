b5c286ae09845805f1417fefd7abfe8a0e1577e5
Joel D. Simon (Darwin) <jdsimon@princeton.edu>
2021-11-23 12:04:16 -0800

    Determine if .sac has associated reviewed .evt

---
+++ earthquakes/isreviewed.m
@@ -0,0 +1,28 @@
+function tf = isreviewed(sacfile, evtdir)
+% ISREVIEWED(sacfile, evtdir)
+%
+% Returns true if the input .sac has an associated reviewed .evt file.
+%
+% Input:
+% sacfile    SAC filename
+% evtdir     Path to directory containing 'raw/' and 'reviewed'
+%                 subdirectories (def: $MERMAID/events/)
+% Output:
+%            logical true or false
+%
+% Author: Joel D. Simon
+% Contact: jdsimon@alumni.princeton.edu | joeldsimon@gmail.com
+% Last modified: 22-Nov-2021, Version 9.3.0.948333 (R2017b) Update 9 on MACI64
+
+defval('evtdir', fullfile(getenv('MERMAID'), 'events'))
+
+evtfile = strrep(strippath(sacfile), '.sac', '.evt');
+D = dir(fullfile(evtdir, 'reviewed', sprintf('**/*%s', evtfile)));
+if isempty(D)
+   tf = false;
+
+else
+    tf = true;
+
+end
+
