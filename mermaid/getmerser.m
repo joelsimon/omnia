bc048f315b8e461c84b7e9a4f681879f1e0dec06
Joel D. Simon (Darwin) <jdsimon@princeton.edu>
2021-12-06 21:53:39 -0800

    Make recursive for cell arrays

--- mermaid/getmerser.m
+++ mermaid/getmerser.m
@@ -15,6 +15,16 @@ function ser = getmerser(sacfile)
 %
 % Author: Joel D. Simon
 % Contact: jdsimon@alumni.princeton.edu | joeldsimon@gmail.com
-% Last modified: 15-Jun-2021, Version 9.3.0.948333 (R2017b) Update 9 on MACI64
+% Last modified: 06-Dec-2021, Version 9.3.0.948333 (R2017b) Update 9 on MACI64
+
+%% Recursive.
+
+if iscell(sacfile)
+    for i = 1:length(sacfile)
+        ser{i} = getmerser(sacfile{i});
+    end
+    return
+
+end
 
 ser = fx(strsplit(fx(strsplit(strippath(sacfile), '.'), 2), '_'), 1);
