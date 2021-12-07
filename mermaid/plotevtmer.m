02d4fc2509b0e4c2830c2ab0e61d43bcf5bf8987
Joel D. Simon (Darwin) <jdsimon@princeton.edu>
2021-12-06 22:58:44 -0800

    Add MERMAIDs returning neither DET nor REQ

--- mermaid/plotevtmer.m
+++ mermaid/plotevtmer.m
@@ -38,7 +38,9 @@ else
 
 end
 
-% Keep only uniq
+% Only plot MERMAID location once if multiple files exists for each event
+% (e.g., REQ multiple phases for same event with slightly different
+% interpolated locations).
 [det_ser, det_idx] = unique(det_ser);
 det_sac = det_sac(det_idx);
 
@@ -46,7 +48,7 @@ det_sac = det_sac(det_idx);
 req_sac = req_sac(req_idx);
 
 % Determine which SAC files only exist as requests.
-[~, req_only_idx] = setdiff(req_ser, det_ser);
+[req_only_ser, req_only_idx] = setdiff(req_ser, det_ser);
 req_only_sac = req_sac(req_only_idx);
 
 % Collect a single matching .evt file (the event location is the same for all
@@ -56,6 +58,7 @@ EQ = getevt(det_sac{1}, evtdir, false);
 evt_lon = EQ.PreferredLongitude;
 evt_lon360 = longitude360(evt_lon);
 evt_lat = EQ.PreferredLatitude;
+evt_date = irisstr2date(EQ.PreferredTime);
 
 % Plot basemap.
 F.f = figure;
@@ -67,14 +70,23 @@ hold(F.ha, 'on')
 F.evt = plot(F.ha, longitude360(EQ.PreferredLongitude), EQ.PreferredLatitude, ...
              'b*', 'MarkerSize', 10);
 
+% Mark interpolated locations of MERMAIDs that neither recorded a DET or REQ
+% file (update as needed; better use kstnm...).
+gps = readgps([], false);
+all_mer_ser = {'08', '09', '10', '11', '12', '13', '16', '17', '18', '19', '20', '21', '22', '23', '24', '25'};
+miss_ser = setdiff(all_mer_ser, [det_ser req_only_ser])
+F = plotmiss(F, miss_ser, gps, evt_date);
+
 % Plot MERMAID DET locations (triggered) in black.
-F = plotit(F, det_sac, evt_lat, evt_lon);
+F = plotsac(F, det_sac, evt_lat, evt_lon);
 
 % Plot only those MERMAID REQ (requested) locations in red only if that MERMAID
 % was not also triggered (not necessarily to surface).
-F = plotit(F, req_only_sac, evt_lat, evt_lon);
+F = plotsac(F, req_only_sac, evt_lat, evt_lon);
 
 % Cosmetics.
+uistack(F.gc, 'bottom');
+uistack(F.tx, 'top')
 F.ha.XLim = [0 360];
 F.ha.YLim = [-90 90];
 
@@ -97,7 +109,7 @@ grid(F.ha, 'on')
 box(F.ha, 'on')
 axesfs(F.f, 7, 9)
 
-function F = plotit(F, sac_files, evt_lat, evt_lon)
+function F = plotsac(F, sac_files, evt_lat, evt_lon)
 
 for i = 1:length(sac_files)
     [x, h] = readsac(sac_files{i});
@@ -128,5 +140,23 @@ for i = 1:length(sac_files)
     F.tx(i) = text(F.ha, mer_lon360, mer_lat, sprintf('%s', mer_ser), ...
                    'HorizontalAlignment', 'Center');
     F.info{i} = sprintf('%i: MERMAID %s (%s)', i, mer_ser, det_req);
+
+end
+
+function F = plotmiss(F, miss_ser, gps, evt_date)
+
+for i = 1:length(miss_ser)
+    mer_ser = miss_ser{i};
+    kstnm = sprintf('P00%s', mer_ser);
+    mer_struct = gps.(kstnm);
+    [mer_lat, mer_lon] = interpmerloc(mer_struct, evt_date);
+    mer_lon360 = longitude360(mer_lon);
+
+    F.pl(i) = plot(F.ha, mer_lon360, mer_lat, 'v', 'MarkerFaceColor', [0.6 0.6 ...
+                        0.6], 'MarkerEdgeColor', [0.6 0.6 0.6], 'MarkerSize', 15);
+    %F.gc(i) = [];
+    F.tx(i) = text(F.ha, mer_lon360, mer_lat, sprintf('%s', mer_ser), ...
+                   'HorizontalAlignment', 'Center');
+    F.info{i} = sprintf('%i: MERMAID %s (none)', i, mer_ser);
 
 end
