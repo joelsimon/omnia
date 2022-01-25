445b857221fd3e2d40f3d91a864ce0f28119bd1a
Joel D. Simon (Darwin) <jdsimon@princeton.edu>
2022-01-25 12:36:52 -0800

    Return request duration in seconds

--- mermaid/requests/reqdateduration.m
+++ mermaid/requests/reqdateduration.m
@@ -1,5 +1,5 @@
-function [req_str, start_date, end_date] = reqdateduration(start_date, end_date)
-% [req_str, start_date, end_date] = REQDATEDURATION(start_date, end_date)
+function [req_str, start_date, req_sec, end_date] = reqdateduration(start_date, end_date)
+%  [req_str, start_date, req_sec, end_date] = REQDATEDURATION(start_date, end_date)
 %
 % Returns partial .cmd request*, the start date and duration only, formatted
 % properly and split into multiple requests if the duration is greater than 1800
@@ -14,6 +14,7 @@ function [req_str, start_date, end_date] = reqdateduration(start_date, end_date)
 % req_str       Cell array of start date and duration, formatted for .cmd
 %                   "mermaid REQUEST...", possibly split across multiple lines
 % start_date    Datetime array of start of each request line
+% req_sec       Double array duration in seconds of each request line
 % end_date      Datetime array of end of each request line
 %
 % *E.g. if the full request is "mermaid REQUEST:2018-05-14T22_14_00,120,-1",
@@ -22,16 +23,16 @@ function [req_str, start_date, end_date] = reqdateduration(start_date, end_date)
 % Ex1: Request 0.25 hours of data; single-line request okay
 %    start_date = iso8601str2date('2000-01-01T00:00:00Z');
 %    end_date = start_date + hours(0.25);
-%    [req_str, start_date, end_date] = REQDATEDURATION(start_date, end_date)
+%    [req_str, start_date, req_sec, end_date] = REQDATEDURATION(start_date, end_date)
 %
 % Ex2: Request 5.25 hours of data; must split request over multiple lines
 %    start_date = iso8601str2date('2000-01-01T00:00:00Z');
 %    end_date = start_date + hours(5.25);
-%    [req_str, start_date, end_date] = REQDATEDURATION(start_date, end_date)
+%    [req_str, start_date, req_sec, end_date] = REQDATEDURATION(start_date, end_date)
 %
 % Author: Joel D. Simon
 % Contact: jdsimon@alumni.princeton.edu | joeldsimon@gmail.com
-% Last modified: 24-Jan-2022, Version 9.3.0.948333 (R2017b) Update 9 on MACI64
+% Last modified: 25-Jan-2022, Version 9.3.0.948333 (R2017b) Update 9 on MACI64
 
 % Sanity.
 if ~isdatetime(start_date) || ~isdatetime(end_date)
@@ -49,32 +50,35 @@ max_duration = floor(max_duration * 0.95);
 
 % If request less than the maximum duration, return in a single line.
 if req_duration < max_duration
-    req_secs = ceil(seconds(end_date - start_date));
-    req_str = {sprintf('%s,%i', reqdate(start_date), req_secs)};
+    req_sec = ceil(seconds(end_date - start_date));
+    req_str = {sprintf('%s,%i', reqdate(start_date), req_sec)};
 
 else
     % Otherwise split across multiple requests, the next starting where the previous
     % ends, with durations equal to the maximum-allowed request duration.
     line_start(1) = start_date;
-    req_str{1} = sprintf('%s,%i', reqdate(line_start(1)), max_duration);
-    line_end(1) = line_start(1) + seconds(max_duration);
+    req_sec(1) = max_duration;
+    req_str{1} = sprintf('%s,%i', reqdate(line_start(1)), req_sec);
+    line_end(1) = line_start(1) + seconds(req_sec)
 
     for i = 2:floor(req_duration / max_duration)
         line_start(i) = line_end(i-1);
-        req_str{i} = sprintf('%s,%i', reqdate(line_start(i)), max_duration);
-        line_end(i) = line_start(i) + seconds(max_duration);
+        req_sec(i) = req_sec(i-1);
+        req_str{i} = sprintf('%s,%i', reqdate(line_start(i)), req_sec(i));
+        line_end(i) = line_start(i) + seconds(req_sec(i));
 
     end
 
     % Finish the multiline request with whatever remaining time is leftover after
     % requesting maximum-duration chunks.
     line_start(i+1) = line_end(i);
-    rem_secs = ceil(seconds(end_date - line_start(i+1)));
-    req_str{i+1} = sprintf('%s,%i', reqdate(line_start(i+1)), rem_secs);
-    line_end(i+1) = end_date;
+    req_sec(i+1) = ceil(seconds(end_date - line_start(i+1)));
+    req_str{i+1} = sprintf('%s,%i', reqdate(line_start(i+1)), req_sec(i+1));
+    line_end(i+1) = line_start(i+1) + seconds(req_sec(i+1));
 
     req_str = req_str';
     start_date = line_start';
     end_date = line_end';
+    req_sec = req_sec';
 
 end
