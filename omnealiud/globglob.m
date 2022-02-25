345ab4c5aad36ac4a0fbdfd7a91b673d5ca0d93b
Joel D. Simon (Darwin) <jdsimon@princeton.edu>
2022-02-24 17:40:06 -0800

    Match filename patterns like Python's `glob.glob()`

---
+++ omnealiud/globglob.m
@@ -0,0 +1,24 @@
+function fname = globglob(varargin)
+% fname = GLOBGLOB(patter1, pattern, ..., patternN)
+%
+% Return matched filename pattern, like Python's `glob.glob`
+% Beware of odd wildcard expansion, which may differ between systems.
+%
+% Input:
+% pattern   Fileparts pattern (wildcard "*" allowed)
+%
+% Output:
+% fname     Cell array of matched filenames
+%
+% Ex:
+%    GLOBGLOB(getenv('OMNIA'), 'exfiles')
+%    GLOBGLOB(getenv('OMNIA'), 'exfiles', '*.sac')
+%    GLOBGLOB(getenv('OMNIA'), '*x*s')
+%    GLOBGLOB(getenv('OMNIA'), '*x*s', '*.*a*')
+%
+% Author: Joel D. Simon
+% Contact: jdsimon@alumni.princeton.edu | joeldsimon@gmail.com
+% Last modified: 24-Feb-2022, Version 9.3.0.948333 (R2017b) Update 9 on MACI64
+
+% nasty one-liner...
+fname = fullfiledir(skipdotdir(dir(fullfile(varargin{:}))));
