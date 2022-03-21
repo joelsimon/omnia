10089508c86b1da8fc3b4ef2594b3280fa267bd4
Joel D. Simon (Darwin) <jdsimon@princeton.edu>
2022-03-21 10:52:40 -0700

    Add option return unrounded sampling frequency
    Also default to use `1/h.DETLA` when the usually present h.NPTS, h.B,
    and h.E are missing.

--- earthquakes/efes.m
+++ earthquakes/efes.m
@@ -1,22 +1,35 @@
-function fs = efes(h)
-% fs = EFES(h)
+function fs = efes(h, rd)
+% fs = EFES(h, rd)
 %
 % Returns the nominal sampling frequency of a SAC waveform in Hz,
 % rounded to the nearest integer.
 %
 % Input:
 % h           Header structure returned by readsac.m
+% rd          true to round `fs` to nearest integer (Def: true)
 %
 % Output:
 % fs          ROUNDED sampling frequency of the SAC waveform [Hz]
 %
 % Ex:
 %    [~, h] = readsac('centcal.1.BHZ.SAC');
-%    fs = EFES(h)
+%    fs1 = EFES(h)
+%    fs2 = EFES(h, false)
 %
 % Author: Joel D. Simon
-% Contact: jdsimon@princeton.edu
-% Last modified: 01-Feb-2018, Version 2017b
+% Contact: jdsimon@alumni.princeton.edu | joeldsimon@gmail.com
+% Last modified: 21-Mar-2022, Version 9.3.0.948333 (R2017b) Update 9 on MACI64
 
-% Find nominal sampling frequency .
-fs = round(h.NPTS / (h.E - h.B));
+% Find nominal sampling frequency.
+defval('rd', true)
+if any([h.NPTS h.E h.B] == -12345)
+    fs = 1 / h.DELTA;
+
+else
+    fs = h.NPTS / (h.E - h.B);
+
+end
+if rd
+    fs = round(fs);
+
+end
