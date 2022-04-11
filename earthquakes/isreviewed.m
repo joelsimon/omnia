abca9012e1e95c1e986401fda42522345fdff3e1
Joel D. Simon (Darwin) <jdsimon@princeton.edu>
2022-04-11 09:58:26 -0700

    Edit to work for both "*.sac" and "*.SAC"

--- earthquakes/isreviewed.m
+++ earthquakes/isreviewed.m
@@ -12,11 +12,11 @@ function tf = isreviewed(sacfile, evtdir)
 %
 % Author: Joel D. Simon
 % Contact: jdsimon@alumni.princeton.edu | joeldsimon@gmail.com
-% Last modified: 22-Nov-2021, Version 9.3.0.948333 (R2017b) Update 9 on MACI64
+% Last modified: 11-Apr-2022, Version 9.3.0.948333 (R2017b) Update 9 on MACI64
 
 defval('evtdir', fullfile(getenv('MERMAID'), 'events'))
 
-evtfile = strrep(strippath(sacfile), '.sac', '.evt');
+evtfile = strrep(strippath(lower(sacfile)), '.sac', '.evt');
 D = dir(fullfile(evtdir, 'reviewed', sprintf('**/*%s', evtfile)));
 if isempty(D)
    tf = false;
@@ -25,4 +25,3 @@ else
     tf = true;
 
 end
-
