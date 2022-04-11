64a80f4c39f7f684a46185dfa98df57447a2de0d
Joel D. Simon (Darwin) <jdsimon@princeton.edu>
2022-04-11 09:27:40 -0700

    A mild convenience

---
+++ omnealiud/ls1.m
@@ -0,0 +1,10 @@
+function ls1()
+% LS1
+%
+% Alias for `ls -1`
+%
+% Author: Joel D. Simon
+% Contact: jdsimon@alumni.princeton.edu | joeldsimon@gmail.com
+% Last modified: 11-Apr-2022, Version 9.3.0.948333 (R2017b) Update 9 on MACI64
+
+ls -1
