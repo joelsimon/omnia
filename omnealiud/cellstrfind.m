dfcbd9634a124f5fa8b7035fd92f04bc43dda698
Joel D. Simon (Darwin) <jdsimon@princeton.edu>
2022-04-15 17:11:07 -0700

    Make recursive to handle `pattern` as cell array

--- omnealiud/cellstrfind.m
+++ omnealiud/cellstrfind.m
@@ -1,15 +1,14 @@
 function [idx, sellout] = cellstrfind(sell, pattern)
 % [idx, sellout] = CELLSTRFIND(sell, pattern)
 %
-% Returns the indices and strings in a cell which contain the regexp
-% pattern.
+% Return indices and strings in cell array which contain the regexp pattern.
 %
 % Input:
 % sell       Cell array of strings
 % pattern    Regular expression (e.g., '-P-\d\d\/')
-%
+%                (accepts cell arrays)
 % Output:
-% idx        The index of matches (def: {})
+% idx        The index of matches (def: [])
 % sellout    Cell of matching strings,
 %                literally sell(idx) (def: {})
 %
@@ -17,6 +16,7 @@ function [idx, sellout] = cellstrfind(sell, pattern)
 %    sell = {'dog' 'froggy' 'hop' 'on' 'a' 'soggy' 'log' 'in' 'a' 'bog'}
 %    [idx, sellout] = CELLSTRFIND(sell, 'og')
 %    [idx, sellout] = CELLSTRFIND(sell, '[^0-9]og.y')
+%    [idx, sellout] = CELLSTRFIND(sell, {'dog', 'bog', 'froggy'})
 %
 % Ex2: (?i) turns on case-insensitivity; (?-i) turns off case-insensitivity
 %    sell = {'UPPERCASE', 'lowercase', 'MiXeDcAsE', 'mIxEdCaSe'}
@@ -26,9 +26,28 @@ function [idx, sellout] = cellstrfind(sell, pattern)
 %    [idx, sellout] = CELLSTRFIND(sell, '(?i)cas(?-i)e')
 %
 % Author: Joel D. Simon
-% Contact: jdsimon@princeton.edu
-% Last modified: 28-Nov-2019, Version 2017b on GLNXA64
+% Contact: jdsimon@alumni.princeton.edu | joeldsimon@gmail.com
+% Last modified: 15-Apr-2022, Version 9.3.0.948333 (R2017b) Update 9 on MACI64
 
+%% RECURSIVE.
+
+if iscell(pattern)
+
+    %% RECURSION.
+
+    idx = [];
+    sellout = {};
+    for i = 1:length(pattern)
+        [A, B] = cellstrfind(sell, pattern{i});
+        idx = [idx ; A];
+        sellout = [sellout ; B];
+
+    end
+    return
+
+end
+
+% Main.
 matches = regexp(sell, pattern);
 idx = find(~cellfun(@isempty, matches));
 sellout = sell(idx);
