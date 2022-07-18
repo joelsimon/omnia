95588ffea088a79245207b79ea7584816169bb1c
Joel D. Simon (Darwin) <jdsimon@princeton.edu>
2022-07-18 16:35:16 -0700

    Correct docstring and add more examples

--- plotbits/longitude360.m
+++ plotbits/longitude360.m
@@ -1,22 +1,34 @@
 function lon = longitude360(lon)
 % lon = LONGITUDE360(lon)
 %
-% Converts longitude from [-90:90] to [0:360] degree system.
+% Converts longitude from [-180:+180] to [0:360] degree system.
 %
 % Input:
-% lon      Longitude in [-90:90] degree system
+% lon      Longitude in [-180:+180] degree system
 %
 % Output:
 % lon      Longitude in [0:360] degree system
 %
-% Example:
+% Example: (heading east from Prime Meridian)
+%    LONGITUDE360(0)
+%    LONGITUDE360(1)
+%    LONGITUDE360(45)
+%    LONGITUDE360(90)
+%    LONGITUDE360(135)
+%    LONGITUDE360(180)
+%    LONGITUDE360(-135)
 %    LONGITUDE360(-90)
 %    LONGITUDE360(-45)
-%    LONGITUDE360(+45)
-%    LONGITUDE360(+90)
+%    LONGITUDE360(-1)
+%    LONGITUDE360(360)
 %
 % Author: Joel D. Simon
 % Contact: jdsimon@alumni.princeton.edu | joeldsimon@gmail.com
-% Last modified: 03-Dec-2021, Version 9.3.0.948333 (R2017b) Update 9 on MACI64
+% Last modified: 18-Jul-2022, Version 9.3.0.948333 (R2017b) Update 9 on MACI64
+
+if lon < -180 || lon > 360
+    error('Longitude must be within [-180:180], inclusive')
+
+end
 
 lon(find(lon < 0)) = lon(find(lon < 0)) + 360;
