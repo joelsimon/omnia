4d5b0c5015cc84ddcae4865723e81ff1c2907b21
Joel D. Simon (Darwin) <jdsimon@princeton.edu>
2022-07-26 11:41:48 -0700

    Allow vectorized data

--- plotbits/longitude360.m
+++ plotbits/longitude360.m
@@ -1,7 +1,13 @@
 function lon = longitude360(lon)
 % lon = LONGITUDE360(lon)
 %
-% Converts longitude from [-180:+180] to [0:360] degree system.
+% Convert longitude from [-180:+180] to [0:360] degree system.
+%
+% NB: -180 is converted to 180; 360 is unattainable
+%   LONGITUDE360(-180) == LONGITUDE360(180) % 180
+%   LONGITUDE360(-0.1) == 359.9000
+%   LONGITUDE360(0) == 0 % not 360
+%   LONGITUDE360(0.1) == 0.1
 %
 % Input:
 % lon      Longitude in [-180:+180] degree system
@@ -10,23 +16,24 @@ function lon = longitude360(lon)
 % lon      Longitude in [0:360] degree system
 %
 % Example: (heading east from Prime Meridian)
-%    LONGITUDE360(0)
-%    LONGITUDE360(1)
-%    LONGITUDE360(45)
-%    LONGITUDE360(90)
-%    LONGITUDE360(135)
-%    LONGITUDE360(180)
+%    LONGITUDE360(+0)
+%    LONGITUDE360(+1)
+%    LONGITUDE360(+45)
+%    LONGITUDE360(+90)
+%    LONGITUDE360(+135)
+%    LONGITUDE360(+180)
+%    LONGITUDE360(-180)
 %    LONGITUDE360(-135)
 %    LONGITUDE360(-90)
 %    LONGITUDE360(-45)
 %    LONGITUDE360(-1)
-%    LONGITUDE360(360)
+%    LONGITUDE360(-0)
 %
 % Author: Joel D. Simon
 % Contact: jdsimon@alumni.princeton.edu | joeldsimon@gmail.com
-% Last modified: 18-Jul-2022, Version 9.3.0.948333 (R2017b) Update 9 on MACI64
+% Last modified: 26-Jul-2022, Version 9.3.0.948333 (R2017b) Update 9 on MACI64
 
-if lon < -180 || lon > 360
+if any(lon < -180) || any(lon > 180)
     error('Longitude must be within [-180:180], inclusive')
 
 end
