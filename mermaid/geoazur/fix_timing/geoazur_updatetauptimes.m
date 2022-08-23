e8d80f2d5a745c51cd9d3bb1e2434c10b778b4de
Joel D. Simon (Darwin) <jdsimon@princeton.edu>
2022-08-23 14:57:44 -0700

    Explain .evt update due to length 2 pressure field

--- mermaid/geoazur/fix_timing/geoazur_updatetauptimes.m
+++ mermaid/geoazur/fix_timing/geoazur_updatetauptimes.m
@@ -7,7 +7,7 @@ function geoazur_updatetauptimes
 %
 % Author: Joel D. Simon
 % Contact: jdsimon@alumni.princeton.edu | joeldsimon@gmail.com
-% Last modified: 21-Sep-2021, Version 9.3.0.948333 (R2017b) Update 9 on MACI64
+% Last modified: 23-Aug-2022, Version 9.3.0.948333 (R2017b) Update 9 on MACI64
 
 % Load data.
 defval('sac_diro', getenv('MERAZUR'));
@@ -17,10 +17,10 @@ defval('evt_diro', fullfile(rematch_diro, 'reviewed', 'identified', 'evt'))
 s = mermaid_sacf('id', sac_diro);
 
 fname_time = fullfile(evt_diro, 'updated_timing');
-fid_time = fopen(fname_time, 'w+');
+fid_time = fopen(fname_time, 'w');
 
 fname_press = fullfile(evt_diro, 'updated_pressure');
-fid_press = fopen(fname_press, 'w+');
+fid_press = fopen(fname_press, 'w');
 
 for i = 1:length(s)
     i
@@ -35,19 +35,39 @@ for i = 1:length(s)
     [~, h] = readsac(sac_file);
 
     if isupdated
-        if h.NZMSEC > 0 && h.NZMSEC < 100
+        if h.NZMSEC < 100
             fprintf(fid_time, sac_str)
 
         elseif isequal(rmfield(new_EQ.TaupTimes, 'pressure'), rmfield(old_EQ.TaupTimes, 'pressure'))
             fprintf(fid_press, sac_str)
 
+            % % E.g., m12.20150306T082259.sac
+            % %
+            % % For some reason it appears that the the "Pn/g" and "Sn/g" phases were
+            % % printing the full [1 x 2] output of `reid` (P and S) as opposed to
+            % % just one or the other. I went back and reran with $OMNIA at their time of
+            % % creation and cannot replicate error. I don't know the cause...must
+            % % have popped up during some `updateevt`-esque run on the GeoAzur
+            % % data.  I verified this to be the case with these lines.  Also,
+            % % reflections (PP and PKIKP) were previously printing with
+            % % pressures and now they are returned empty, creating another diff.
+            % %
+            % % Bug: these are all [1 x 2]
+            % disp('double')
+            % old_EQ.TaupTimes(find(contains({old_EQ.TaupTimes.phaseName}, {'Pn', 'Sn', 'Pg', 'Sg'}))).pressure
+            % % These are all [1 x 1]
+            % disp('single')
+            % old_EQ.TaupTimes(find(~contains({old_EQ.TaupTimes.phaseName}, {'Pn', 'Sn', 'Pg', 'Sg'}))).pressure
+            % keyboard
+            % clc
+
         else
-            error(fprintf('%s updated for unkown reason', sac_str))
+            error(fprintf('%s updated for unknown reason\n', sac_str))
 
         end
     else
-        if h.NZMSEC > 0 && h.NZMSEC < 100
-            error(fprintf('%s should not have updated', sac_str))
+        if h.NZMSEC < 100
+            error(fprintf('%s should have updated\n', sac_str))
 
         end
     end
