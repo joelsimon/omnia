80ca2286de7e66d46553f69b6e823e549b5d6fb0
Joel D. Simon (Darwin) <jdsimon@princeton.edu>
2023-01-31 16:22:25 -0800

    Return station lat/lon, and optionally write CSV file

--- mermaid/intoodeep.m
+++ mermaid/intoodeep.m
@@ -1,5 +1,5 @@
-function [danger, dates, stdp, ocdp] = intoodeep(s, mbuffer, vers)
-% [danger, dates, stdp, ocdp] = INTOODEEP(s, mbuffer, vers)
+function [danger, dates, stdp, ocdp, stla, stlo] = intoodeep(s, mbuffer, vers, sav)
+% [danger, dates, stdp, ocdp, stla, stlo] = INTOODEEP(s, mbuffer, vers, sav)
 %
 % Returns list of SAC files whose station depth is less than (deeper) the GEBCO
 % depth at that location plus a variable depth buffer.
@@ -9,26 +9,34 @@ function [danger, dates, stdp, ocdp] = intoodeep(s, mbuffer, vers)
 % mbuffer    Depth buffer -- ocean must be at least 'mbuffer' deeper than
 %                MERMAID to not be flagged [m] (def: 500)
 % vers       GEBCO bathymetric version (see gebco.m) (def: '2014')
+% sav        true to write output to text file in pwd (false)
 %
 % Output:
-% danger     Cell array of SAC files flagged for being too close to the seafloor
-% dates      Cell array of datetimes of flagged SAC files
-% stdp       Cell array of station depths of flagged SAC files
-% ocdp       Cell array of ocean depths of flagged SAC files
+% danger     SAC files flagged for being too close to the seafloor
+% dates      Datetimes of flagged SAC files
+% stdp       Station depths of flagged SAC files
+% ocdp       GEBCO ocean depths of flagged SAC files
+% stla       Station latitudes of flagged SAC files
+% stlo       Station longitudes of flagged SAC files
 %
 % See also: gebco.m
 %
 % Author: Joel D. Simon
-% Contact: jdsimon@princeton.edu | joeldsimon@gmail.com
-% Last modified: 17-Apr-2020, Version 9.3.0.948333 (R2017b) Update 9 on MACI64
+% Contact: jdsimon@alumni.princeton.edu | joeldsimon@gmail.com
+% Last modified: 31-Jan-2023, Version 9.3.0.948333 (R2017b) Update 9 on MACI64
 
 defval('s', fullsac);
 defval('mbuffer', 500);
 defval('vers', '2014')
+defval('sav', false);
 
 danger = {};
-stdp = {};
-ocdp = {};
+dates = [];
+stdp = [];
+ocdp = [];
+stlo = [];
+stla = [];
+
 for i = 1:length(s)
     [~, h] = readsac(s{i});
     z_mermaid = -h.STDP;
@@ -41,7 +49,25 @@ for i = 1:length(s)
         danger = [danger ; s(i)];
         stdp = [stdp ; z_mermaid];
         ocdp = [ocdp ; z_ocean];
+        stla = [stla ; h.STLA];
+        stlo = [stlo ; h.STLO];
 
     end
 end
-dates = mersac2date(danger);
+if ~isempty(danger)
+    dates = mersac2date(danger);
+
+end
+
+if sav
+    fname = fullfile(pwd, sprintf('%s.txt', mfilename));
+    fid = fopen(fname, 'w');
+    fmt = '%s,%s,%i,%i,%.4f,%.4f\n';
+    for i = 1:length(danger)
+        fprintf(fid, fmt, strippath(danger{i}), datestr(dates(i), 1), stdp(i), ocdp(i), stla(i), stlo(i));
+
+    end
+    fclose(fid)
+    fprintf('Wrote: %s\n', fname)
+
+end
