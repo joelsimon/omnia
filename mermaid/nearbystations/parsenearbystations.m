3cf3781bbac816a6c4a62092f5055ae5687df14b
Joel D. Simon (Darwin) <jdsimon@princeton.edu>
2023-05-17 14:27:58 -0700

    Return all station info, optionally verbose or unique

--- mermaid/nearbystations/parsenearbystations.m
+++ mermaid/nearbystations/parsenearbystations.m
@@ -1,33 +1,42 @@
-function [network, station, datacenter, url] = parsenearbystations(txtfile)
-% [network, station, datacenter, url] = PARSENEARBYSTATIONS(txtfile)
+function [network, station, datacenter, url, latitude, longitude, elevation, sitename, starttime, endtime] = parsenearbystations(txtfile, uniq)
+% [network, station, datacenter, url, latitude, longitude, elevation, sitename, starttime, endtime] = PARSENEARBYSTATIONS(txtfile, uniq)
 %
 % Parses the textfile 'nearbystations.txt' generated at
 % http://ds.iris.edu/gmap/.
 %
-% N.B.: stations can and do change location, sensor type, active
-% status etc. while mainting the same station name.  And while the
-% input txtfile includes this information to some extent, it is not
-% returned by PARSENEARBYSTATIONS because the most up-to-date info
-% should be retrieved with irisFetch.Stations.
+% N.B.: stations can and do change location, sensor type, active status
+% etc. while maintaining the same station name.  The input txtfile includes this
+% information to some extent, it is not returned by PARSENEARBYSTATIONS by
+% default because the most up-to-date info should be retrieved with
+% irisFetch.Stations.
 %
 % Input:
 % txtfile      Text file of station names to parse, from http://ds.iris.edu/gmap
 %                  (def: '$MERMAID/events/nearbystations/nearbystations.txt')
+% uniq         true to only return birth-location info for each stations (def: true)
+%                  e.g., if Raspberry station moved, only return initial lat/lon
 %
 % Output:
 % network      Network code
 % station      Station code
 % datacenter   Name of relevant data center
 % url          Suggested* url of relevant data center
+% latitude     Latitude [decimal degrees]
+% longitude    Longitude [decimal degrees]
+% elevation    Elevation [m]
+% sitename     Site name, if given
+% starttime    Station birthdate, as datetime
+% endtime      Station death, as datetime
 %
 % *Invalid for http://raspberryshake.net: should be https ('s')
 %
 % Author: Joel D. Simon
-% Contact: jdsimon@princeton.edu
-% Last modified: 23-Nov-2019, Version 2017b on MACI64
+% Contact: jdsimon@alumni.princeton.edu | joeldsimon@gmail.com
+% Last modified: 09-May-2023, Version 9.3.0.948333 (R2017b) Update 9 on MACI64
 
 % Default.
 defval('txtfile', fullfile(getenv('MERMAID'), 'events', 'nearbystations', 'nearbystations.txt'))
+defval('uniq', true)
 
 % Read the entire file into memory.
 tx = readtext(txtfile);
@@ -40,6 +49,9 @@ tx(1:dcline(1)-1) = [];
 
 % Adjust the dcline accordingly.
 dcline = dcline - (dcline(1)-1);
+
+% Specify datetime format.
+date_fmt = 'uuuu-MM-dd''T''HH:mm:ss';
 
 % Loop through every line and keep the network and station names.
 tx_idx = 0;
@@ -67,9 +79,15 @@ for i = 1:length(tx)
     else
         % Parse the station info.
         tx_idx = tx_idx + 1;
-        this_line = strsplit(tx{i}, '|');
+        this_line = strsplit(tx{i}, '|', 'CollapseDelimiters', false);
         network{tx_idx} = this_line{1};
         station{tx_idx} = this_line{2};
+        latitude(tx_idx) = str2double(this_line{3});
+        longitude(tx_idx) = str2double(this_line{4});
+        elevation(tx_idx) = str2double(this_line{5});
+        sitename{tx_idx} = str2double(this_line{6});
+        starttime(tx_idx) = datetime(this_line{7}, 'Format', date_fmt, 'TimeZone', 'UTC');
+        endtime(tx_idx) = datetime(this_line{8}, 'Format', date_fmt, 'TimeZone', 'UTC');
         datacenter{tx_idx} = block_datacenter;
         url{tx_idx} = block_url;
 
@@ -77,7 +95,17 @@ for i = 1:length(tx)
 end
 
 % Remove duplicate stations and reorder outputs accordingly.
-[station, uniq_idx] = unique(station);
-network = network(uniq_idx);
-datacenter = datacenter(uniq_idx);
-url = url(uniq_idx);
+if uniq
+    [~, idx] = unique(station);
+    network = network(idx);
+    station = station(idx);
+    latitude = latitude(idx);
+    longitude = longitude(idx);
+    elevation = elevation(idx);
+    sitename = sitename(idx);
+    starttime = starttime(idx);
+    endtime = endtime(idx);
+    datacenter = datacenter(idx);
+    url = url(idx);
+
+end
