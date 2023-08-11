dbcfe6354d86fb055bae67ce34d16ea4e0325f7a
Joel D. Simon (Darwin) <jdsimon@princeton.edu>
2023-08-11 13:55:50 -0700

    Write text file of event IDs

---
+++ mermaid/writeeventid.m
@@ -0,0 +1,34 @@
+function writeeventid(sacdir, evtdir)
+% WRITEEVENTID(sacdir, evtdir)
+%
+% Write event text file with lines: <sac_filename>  | <contrib_id>  | <iris_id>
+%
+% Author: Joel D. Simon
+% Contact: jdsimon@alumni.princeton.edu | joeldsimon@gmail.com
+% Last modified: 11-Aug-2023, Version 9.3.0.948333 (R2017b) Update 9 on MACI64
+
+defval('sacdir', fullfile(getenv('MERMAID'), 'processed'))
+defval('evtdir', fullfile(getenv('MERMAID'), 'events'))
+
+fmt = '%-45s  | %14s  |  %8s\n';
+fname = fullfile(evtdir, 'reviewed', 'identified', 'txt', 'eventid.txt');
+writeaccess('unlock', fname, false);
+fid = fopen(fname, 'w');
+
+[sac, evt] = revsac(1, sacdir, evtdir, 'ALL');
+for i = 1:length(evt)
+    sname = strippath(sac{i});
+    tmp = load(evt{i}, '-MAT');
+    EQ = tmp.EQ;
+    if length(EQ) > 1
+        EQ = EQ(1);
+        sname = [sname '*'];
+
+    end
+    [contrib_eventid, ~, iris_eventid] = eventid(EQ);
+    fprintf(fid, fmt, sname, contrib_eventid, iris_eventid);
+
+end
+fclose(fid)
+writeaccess('lock', fname);
+sprintf('Wrote: %s\n', fname);
