1b92794ab5c776b651e0201815c4a75fcbc8db7a
Joel D. Simon (Darwin) <jdsimon@princeton.edu>
2023-08-11 11:48:25 -0700

    Change directory to $HUNGA/code

---
+++ omnealiud/hunga.m
@@ -0,0 +1,10 @@
+function hunga
+% HUNGA
+%
+% Change directory to $HUNGA/code.
+%
+% Author: Joel D. Simon
+% Contact: jdsimon@alumni.princeton.edu | joeldsimon@gmail.com
+% Last modified: 08-Aug-2023, Version 9.3.0.948333 (R2017b) Update 9 on MACI64
+
+cd(fullfile(getenv('HUNGA'), 'code'))
