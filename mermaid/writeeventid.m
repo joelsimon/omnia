4700fef7bc17441437f1ab3e13c0751a1f32f573
Joel D. Simon (Darwin) <jdsimon@princeton.edu>
2023-08-14 14:27:25 -0700

    Add header

--- mermaid/writeeventid.m
+++ mermaid/writeeventid.m
@@ -11,7 +11,7 @@ function writeeventid(sacdir, evtdir)
 %
 % Author: Joel D. Simon
 % Contact: jdsimon@alumni.princeton.edu | joeldsimon@gmail.com
-% Last modified: 11-Aug-2023, Version 9.3.0.948333 (R2017b) Update 9 on MACI64
+% Last modified: 14-Aug-2023, Version 9.3.0.948333 (R2017b) Update 9 on MACI64
 
 defval('sacdir', fullfile(getenv('MERMAID'), 'processed'))
 defval('evtdir', fullfile(getenv('MERMAID'), 'events'))
@@ -20,6 +20,7 @@ fmt = '%-45s  | %14s  |  %8s\n';
 fname = fullfile(evtdir, 'reviewed', 'identified', 'txt', 'eventid.txt');
 writeaccess('unlock', fname, false);
 fid = fopen(fname, 'w');
+fprintf(fid, '                                SAC_FILENAME         CONTRIB_ID      IRIS_ID\n')';
 
 [sac, evt] = revsac(1, sacdir, evtdir, 'ALL');
 for i = 1:length(evt)
