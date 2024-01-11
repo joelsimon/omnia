8caeaadb6d5284c16f06c764add10b7eb7dac40c
Joel D. Simon (Darwin) <jdsimon@princeton.edu>
2024-01-10 16:00:28 -0800

    Allow iso8601 datestrings that don't end in "Z"

--- omnealiud/iso8601str2date.m
+++ omnealiud/iso8601str2date.m
@@ -1,5 +1,5 @@
-function tdate = iso8601str2date(tstr, precision)
-% tdate = ISO8601STR2DATE(tstr, precision)
+function tdate = iso8601str2date(tstr, precision, noz)
+% tdate = ISO8601STR2DATE(tstr, precision, noz)
 %
 % Returns datetime array given input in iso8601 <date>T<time>Z format (UTC).
 %
@@ -8,16 +8,18 @@ function tdate = iso8601str2date(tstr, precision)
 % precision    1: seconds, e.g., '2007-04-05T14:30Z' (def)
 %              2: milliseconds, e.g., '2007-04-05T01:14:30.123Z'
 %              3: microseconds, e.g., '2007-04-05T01:14:30.123456Z'
+% noz          true if no "Z", e.g., '2007-04-05T14:30' (def: false)
 %
 % Output:
 % tdate        Datetime array
 %
 % Author: Joel D. Simon
 % Contact: jdsimon@alumni.princeton.edu | joeldsimon@gmail.com
-% Last modified: 30-Jul-2021, Version 9.3.0.948333 (R2017b) Update 9 on MACI64
+% Last modified: 10-Jan-2024, Version 9.3.0.948333 (R2017b) Update 9 on MACI64
 
 % Defaults.
-defval('precision', 1);
+defval('precision', 1)
+defval('noz', false)
 
 % Switch format specification
 switch precision
@@ -35,5 +37,11 @@ switch precision
 
 end
 
-% Convert.
+% Chop off trailing "Z" from formatspec, if not supplied with input datestr.
+if noz
+    Format = strrep(Format, '''Z''', '');
+
+end
+
+% Convert from string to datetime.
 tdate = datetime(tstr, 'Format', Format, 'TimeZone', 'UTC');
