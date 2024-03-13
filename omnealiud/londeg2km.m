d741140b75980c3f7fc06f1cc20465aaf36bedcc
Joel D. Simon (Darwin) <jdsimon@princeton.edu>
2024-03-13 16:18:58 -0700

    Convert degrees of longitude to distance
    ...at constant latitude

---
+++ omnealiud/londeg2km.m
@@ -0,0 +1,18 @@
+function km = londeg2km(lat, deg)
+% LONDEG2KM(lat, deg)
+%
+% Convert distance from longitude degrees to kilometers at a constant latitude.
+%
+% Input:
+% lat     Degree of constant latitude [deg]
+% deg     Degrees of longitude (east-west distance) [deg]
+%
+% Ex: (Distance of 1 degree of longitude from North Pole to equator)
+%    LONDEG2KM([90 60 45 15 0]', 1)
+%
+% Author: Joel D. Simon
+% Contact: jdsimon@alumni.princeton.edu | joeldsimon@gmail.com
+% Last modified: 13-Mar-2024, Version 9.3.0.948333 (R2017b) Update 9 on MACI64
+
+defval('deg', 1)
+km = deg2km(distance(lat, 0, lat, deg));
