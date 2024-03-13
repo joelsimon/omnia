cb93f6079868cd0e888ad9c4784ce13122d8b80f
Joel D. Simon (Darwin) <jdsimon@princeton.edu>
2024-03-13 16:59:34 -0700

    Refactor and swap resolution input from m to deg
    Also massively refactor code for readability.  Moved original
    fresnelgrid.m to $OMNIA/atm. That one was faster, and I think could
    accomplish the same thing...I just need to work on it a little more?  I
    think this version has a slightly fatter (closer to reality) Fresnel
    zone for more of the path, due to how I was indexing arcs and whatnot.
    Maybe just forget about that one; this seems to work.

--- earthquakes/fresnelgrid.m
+++ earthquakes/fresnelgrid.m
@@ -1,75 +1,83 @@
-function [fzlat, fzlon, gclat, gclon, fr, gcidx] = ...
-    fresnelgrid(lat1, lon1, lat2, lon2, v, f, res_m, plt)
-% [fzlat, fzlon, gclat, gclon, fr, gcidx] = FRESNELGRID(lat1, lon1, lat2, lon2, v, f, res_m, plt)
+function [fzlat, fzlon, gcidx, fr, deg] = ...
+            fresnelgrid3(lat1, lon1, lat2, lon2, vel, freq, deg, plt)
+% [fzlat, fzlon, gcidx, fr, deg] = ...
+%     FRESNELGRID3(lat1, lon1, lat2, lon2, vel, freq, deg, plt)
 %
-% Compute 2-D (lat/lon only; map view, no depth) Fresnel-zone tracks (like
-% great-circle tracks) for constant-velocity waves (e.g., surface or T waves) on
-% equally spaced grid.
-% 
-% Like fresnelzone.m, but with geographically equally spaced sampling along each
-% Fresnel radii, as opposed to equal number of samples regardless of radii
-% length, as is done there.
+% Compute 2-D (map view; no depth) Fresnel zone for constant-velocity waves
+% (e.g., surface or T waves) on equal-area grid.
+%
+% Output fzlat/lon matrices arranged with rows of Fresnel radii and columns of
+% Fresnel "tracks," of which the latter run (sub)parallel to great-circle path,
+% itself at center: gclat=fzlat(:,<middle_index); gclon=fzlon(:,<middle_index>).
 %
 % Input:
 % lat1/lon1        Latitude and longitude of source [deg]
 % lat2/lon2        Latitude and longitude of receiver [deg]
-% v                Wave velocity [m/s]
-% f                Wave frequency [Hz]
-% res_m            Square-grid resolution [m]
+% vel              Wave velocity [m/s]
+% freq             Wave frequency [Hz]
+% deg*             Grid resolution (lengths of sides of lat/lon square) [deg]*
 % plt              true to plot output (def: false)
 %
 % Output:
-% fzlat**          Latitude of Fresnel-zone tracks [deg]
-% fzlon**          Longitude of Fresnel-zone tracks [deg]
-% gclat            Latitude of great-circle tracks [deg]
-% gclon            Longitude of great-circle tracks [deg]
-% fr               Fresnel radius at every point along great circle [deg]
-% gcidx            Column index of great-circle track (in middle all all tracks)
+% fzlat            Latitudes of Fresnel zone [deg]
+% fzlon            Longitudes of Fresnel zone [deg]
+% gcidx            Column index of great-circle path (middle of output matrix)
 %                  e.g., gclat = fzlat(:, gcidx);
 %                        gclon = fzlat(:, gclon)
+% fr               Fresnel radii length at every point along great circle [deg]
+% deg*             Actual grid resolution [deg]*
 %
-% Ex: (20 s surface wave emanating from Hung-Tonga recorded at MH.N0005,
-%      with grid spacing equal to 1/10 wavelength)
-%    lat1=-20.546; lon1=-175.390; lat2=-11.809; lon2=-144.310;
-%    v=3500; f=1/20; lambda=v/f; res_m=lambda/10;
-%    FRESNELGRID(lat1, lon1, lat2, lon2, v, f, res_m, true)
+% *Actual grid resolution is dictated by discretization of great circle path and
+%  will be less than requested.
+%
+% Ex: (Fresnel zone for 20 s surface wave from Sydney to Portland using 0.25 deg grid)
+%    lat1=-33; lon1=151; lat2=45; lon2=-122; vel=3500; freq=1/20; deg=0.25; plt=true;
+%    FRESNELGRID3(lat1,lon1,lat2,lon2,vel,freq,deg,plt);
 %
 % See also: fresnelzone.m
 %
 % Author: Joel D. Simon
 % Contact: jdsimon@alumni.princeton.edu | joeldsimon@gmail.com
-% Last modified: 05-Mar-2024, Version 9.3.0.948333 (R2017b) Update 9 on MACI64
+% Last modified: 13-Mar-2024, Version 9.3.0.948333 (R2017b) Update 9 on MACI64
+
+% NTS: There are various ways to speed this up (e.g., `cumdist` outside loop and
+% computing all `fresnelradius` in one go; merging azimuth computation into main
+% loop; computing max radius at every point, filling "rectangle" with lat/lon
+% and then latter set to NaN those outside radius (allows you to make one run of
+% `track1` for all points), etc.  I did all that and didn't find the speedup
+% worth the loss of readability. This is a slow code in general, so be it.
 
 % Default.
 defval('plt', false)
 
-% Compute great circle (epicentral distance) length.
-gc_tot_dist_m = deg2km(distance(lat1, lon1, lat2, lon2)) * 1000;
+% Compute great-circle length (epicentral distance).
+gc_tot_dist_deg = distance(lat1, lon1, lat2, lon2, 'degrees');
+gc_tot_dist_m = deg2km(gc_tot_dist_deg) * 1e3;
 
-% Determine number of Fresnel radii based on requested grid resolution.
-num_fr = ceil(gc_tot_dist_m / res_m);
+% Determine number of points along great-circle path to compute Fresnel radii
+% based on requested resolution in degrees. Add one for end point (resolution is
+% an interval).
+num_fr = ceil(gc_tot_dist_deg / deg) + 1;
 
 % Compute lat/lon of great circle path between source and receiver.
 [gclat, gclon] = track2(lat1, lon1, lat2, lon2, [], [], num_fr);
 
-% Compute maximum Fresnel radius, at middle of great-circle path.
-fr_max_m = fresnelmax(v, f, gc_tot_dist_m);
-fr_max_deg = km2deg(fr_max_m / 1000);
+% Determine actual degree resolution (at most equal to, likely less than
+% requested).
+delta_deg = distance(gclat(1), gclon(1), gclat(2), gclon(2), 'degrees');
 
-% Array specifying the total distance in meters from great-circle path along
-% the Fresnel-radii (perpendicular)
-npts_fr = ceil(fr_max_m / res_m);
-fr_cum_dist_m = linspace(0, fr_max_m, npts_fr);
+% Note request vs. reality.
+fprintf('Requested grid spacing: %.4f degrees (~%.1f km sides; %.1f sq. km)\n', ...
+        deg, deg2km(deg), deg2km(deg)^2);
+fprintf('   Actual grid spacing: %.4f degrees (~%.1f km sides; %.1f sq. km)\n', ...
+        delta_deg, deg2km(delta_deg), deg2km(delta_deg)^2);
 
-gc_cum_dist_m = deg2km(cumdist(gclat, gclon)) * 1000;
-gc_cum_dist_m(end) = gc_tot_dist_m;
-fr_m = fresnelradius(gc_cum_dist_m, gc_tot_dist_m, v, f);
 
 % Compute azimuth (degrees) at every point along great circle path.  Each
 % Fresnel radii will be oriented normal to the instantaneous azimuth at that
 % point (picture a fish spine).
 for i = 1:num_fr-1
-    az(i) = azimuth(gclat(i), gclon(i), gclat(i+1), gclon(i+1));
+    az(i) = azimuth(gclat(i), gclon(i), gclat(i+1), gclon(i+1), 'degrees');
 
 end
 az = az';
@@ -91,69 +99,112 @@ else
 
 end
 
-% Generate lat/lon grid given requested grid resolution. At each point along
-% great circle path a positive and negative spoke of maximum Fresnel-radii is
-% projected normal to great circle path.  E.g., if great-circle path is roughly
-% E-W this grid forms a rectangle with length equal to great-circle path and N-S
-% height double the maximum Fresnel radii. This is the "raw" grid with all
-% lat/lon present, before setting to NaN those indices which are outside
-% (further from great-circle path) Fresnel radii.
-[fzlat_neg, fzlon_neg] = track1(gclat, gclon, az-90, repmat(fr_max_deg, size(gclat)), [], [], npts_fr);
-[fzlat_pos, fzlon_pos] = track1(gclat, gclon, az+90, repmat(fr_max_deg, size(gclat)), [], [], npts_fr);
+% Determine number of points along longest Fresnel radii (at midpoint of
+% great-circle) to determine size of output matrix.
+fr_max_m = fresnelmax(vel, freq, gc_tot_dist_m);
+fr_max_km = fr_max_m / 1e3;
+fr_max_deg = km2deg(fr_max_km);
+max_arclen_delta_deg = [0:delta_deg:fr_max_deg];
+npts_max_fr = length(max_arclen_delta_deg);
 
-% Loop over all Fresnel radii (e.g., moving from E (left) to W (right) in
-% example) along great-circle path and set to NaN all points in raw lat/lon grid
-% which are further away from great-circle path than specified Fresnel radii
-% length at that point in the path.  Skip setting to NaN the middle radii (one
-% or two indices), which may otherwise have their final lat/lon set to NaN due
-% to rounding errors (we know the middle of the great-circle path must have the
-% max Fresnel radius).  This loops over the whole path, and NaNs out positive
-% and negative radii. I could be more clever and go to middle of path (maximum
-% radius) and mirror indices.
-mid_idx = mididx(1:num_fr);
+% Initiate matrices to hold positive, negative Fresnel radii in latitude and
+% longitude (think of a fish spine as viewed above).
+frlat_neg = NaN(npts_max_fr, num_fr);
+frlon_neg = NaN(npts_max_fr, num_fr);
+frlat_pos = NaN(npts_max_fr, num_fr);
+frlon_pos = NaN(npts_max_fr, num_fr);
+
+% Loop over every point along great-circle where Fresnel radii must be
+% computed.
 for i = 1:num_fr
-    % Out of the Fresnel zone
-    if any(i==mid_idx)
-        continue
+    % Determine Fresnel radii length in degrees at this point.
+    gc_fr_dist_deg = distance(lat1, lon1, gclat(i), gclon(i));
+    gc_fr_dist_m = deg2km(gc_fr_dist_deg) * 1e3;
+    fr_m = fresnelradius(gc_fr_dist_m, gc_tot_dist_m, vel, freq);
+    fr_km = fr_m / 1e3;
+    fr_deg(i) = km2deg(fr_km);
 
-    end
+    % Discretize Fresnel radii arc length into equal chunks of delta degrees
+    % and use final value (distance in degrees) to specify full length of arc
+    % (equal to or less than actual Fresnel length), and array length to
+    % specify how many points along that arch to be computed.
+    arclen_delta_deg = [0:delta_deg:fr_deg(i)];
 
-    out_idx = find(fr_cum_dist_m > fr_m(i), 1);
+    % Project Fresnel radii at +/-90 degrees (normal) to great-circle path at arc
+    % lengths defined by max discretized radii, splitting into
+    % equal-degree-length chunks just found.
+    [lat_neg, lon_neg] = ...
+        track1(gclat(i), gclon(i), az(i)-90, arclen_delta_deg(end), [], 'degrees', length(arclen_delta_deg));
 
-    fzlat_neg(out_idx:end, i) = NaN;
-    fzlon_neg(out_idx:end, i) = NaN;
+    [lat_pos, lon_pos] = ...
+        track1(gclat(i), gclon(i), az(i)+90, arclen_delta_deg(end), [], 'degrees', length(arclen_delta_deg));
 
-    fzlat_pos(out_idx:end, i) = NaN;
-    fzlon_pos(out_idx:end, i) = NaN;
+    % Only the maximum radii (midpoint of path) can ever have all indices of output
+    % matrices filled (although it think depending on discretization, even that
+    % array might have a NaN at the end), so fill NaNs from end of Fresnel
+    % radii arc length to full length of column.  All lat/lon and pos/neg
+    % have same length; pick one to determine number of NaNs.
+    nan_fill = NaN(size(frlat_neg,1)-length(lat_neg), 1);
+    frlat_neg(:,i) = [lat_neg ;  nan_fill];
+    frlon_neg(:,i) = [lon_neg ;  nan_fill];
 
+    frlat_pos(:,i) = [lat_pos ;  nan_fill];
+    frlon_pos(:,i) = [lon_pos ;  nan_fill];
 
 end
 
 % The first point along each pos/neg Fresnel radius is on great circle itself;
 % cut it.  E.g., the positive track starts on great circle and tracks north/east
 % the specific Fresnel radius.
-fzlat_neg(1, :) = [];
-fzlon_neg(1, :) = [];
-fzlat_pos(1, :) = [];
-fzlon_pos(1, :) = [];
+frlat_neg(1, :) = [];
+frlon_neg(1, :) = [];
+frlat_pos(1, :) = [];
+frlon_pos(1, :) = [];
 
-% At this point the columns define the Fresnel radius tracks (normal to great
-% circle) and the rows define the Fresnel-zone tracks (adjacent to great
+% At this point the columns define the Fresnel radii (normal to great circle)
+% and the rows define the Fresnel-zone "tracks" ((sub)parallel to great
 % circle). Flip left-right the negative lat/lon so that shape of matrix mirrors
 % shape of Fresnel zone (with NaNs). Transpose and concatenate pos/neg radius
 % tracks so that columns of the output define Fresnel-zone tracks.  Slot
 % great-circle latitudes and longitudes (multiply repeated and cut above) in
 % middle of positive and negative tracks.
-fzlat = [fliplr(fzlat_neg') gclat fzlat_pos'];
-fzlon = [fliplr(fzlon_neg') gclon fzlon_pos'];
+fzlat = [fliplr(frlat_neg') gclat frlat_pos'];
+fzlon = [fliplr(frlon_neg') gclon frlon_pos'];
+
+% Now rows are now Fresnel "tracks" that run (sub)parallel to great circle.
+% This is a check to verify equal area (we know Fresnel radii perpendicular to
+% great-circle have constant delta deg spacing; this verifies spacing between
+% neighboring Fresnel radii are similarly spaced spacing set to delta deg).
+ct = 0;
+for i = 1:size(fzlat,1);
+    lat = fzlat(i,:);
+    lon = fzlon(i,:);
+
+    lat = lat(find(isfinite(lat)));
+    lon = lon(find(isfinite(lon)));
+
+    diff_deg = diff(cumdist(lat',lon'));
+    if ~isempty(diff_deg)
+        ct = ct+1;
+        mind(ct) = min(diff_deg);
+        maxd(ct) = max(diff_deg);
+
+    end
+end
+difer(min(mind)-delta_deg, 10, 1, NaN)
+difer(max(maxd)-delta_deg, 10, 1, NaN)
 
 % Return index (middle of tracks) of column representing great-circle track.
-gcidx = size(fzlat_neg', 2) + 1;
+gcidx = size(frlat_neg', 2) + 1;
+
+% Collect outputs.
+deg = delta_deg;
+fr = fr_deg;
 
 %% ___________________________________________________________________________ %%
 
 if plt
-    % Toggle big on or off to swap basemap
+    % Toggle big on or off to swap basemap for Pacific.
     big = false;
 
     figure
@@ -162,33 +213,35 @@ if plt
 
     else
         plotcont
+        axesfs([], 10, 10)
+        longticks([], 2)
+        xlim([0 360])
+        ylim([-90 90])
         box on
 
     end
-    xlim([120 300])
-    ylim([-60 80])
-
     hold on
 
-    % Plot Fresnel-zone tracks; COLUMNS of output.
+    % Plot Fresnel radii; COLUMNS of output.
     for i = 1:size(fzlon, 2);
-        fz_track =  plot(longitude360(fzlon(:, i)), fzlat(:, i), 'b');
+        fr_track =  plot(longitude360(fzlon(:, i)), fzlat(:, i), 'b-');
 
     end
 
-    % Plot (again, redundantly for color) great-circle track, which should fall in middle.
+    % Plot (again, redundantly for color) great-circle track, which should fall in
+    % middle.
     gc_track = plot(longitude360(fzlon(:, gcidx)), fzlat(:, gcidx), 'r');
 
-    % Plot Fresnel radii; ROWS of output
+    % Plot Fresnel "tracks" (sub)parallel to great-circle; ROWS of output
     for i = 1:size(fzlon, 1);
-        fz_radius = plot(longitude360(fzlon(i, :)), fzlat(i, :), 'k-o');
+        fr_radius = plot(longitude360(fzlon(i, :)), fzlat(i, :), 'ko-');
 
     end
 
-    legend([gc_track fz_track fz_radius], 'Great-Circle Track', ...
+    legend([gc_track fr_track fr_radius], 'Great-Circle Track', ...
            'Fresnel Tracks', 'Fresnel Radii');
     hold off
-    axesfs([], 10, 10)
     latimes2
+    shg
 
 end
