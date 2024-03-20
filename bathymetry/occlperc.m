8f620d970bbbc09fa2b6e81e63289e2984db242e
Joel D. Simon (Darwin) <jdsimon@princeton.edu>
2024-03-20 11:38:06 -0700

    Return input test depths as output column

--- bathymetry/occlperc.m
+++ bathymetry/occlperc.m
@@ -1,5 +1,5 @@
-function perc = occlperc(z, tz)
-% perc = OCCLPERC(z, tz)
+function [perc, tz] = occlperc(z, tz)
+% [perc, tz] = OCCLPERC(z, tz)
 %
 % Compute occlusion percentage where occlusion is defined to be any elevation
 % (`z`) that is HIGHER (less deep) than a test elevation (`tz).
@@ -7,22 +7,23 @@ function perc = occlperc(z, tz)
 % Think
 %
 % Input:
-% z        Elevation array or matrix, e.g., from gebco.m [m]
 % tz       Test elevation array [m]
+% z        Elevation array or matrix, e.g., from gebco.m [m]
 %
 % Output:
-% perc     Occlusion percentage
+% perc     Occlusion percentage at test depths, returned as column [m]
+% tz       Test elevation array, returned as column [m]
 %
 % Ex:
 %    z = [-10 -5 0; -25 -15 -20; -10 0 -5]
-%    tz = [0:-10:-30]
+%    tz = [0:-10:-30]'
 %    OCCLPERC(z, tz)
 %
 % See also: gebco.m
 %
 % Author: Joel D. Simon
 % Contact: jdsimon@alumni.princeton.edu | joeldsimon@gmail.com
-% Last modified: 19-Mar-2024, Version 9.3.0.948333 (R2017b) Update 9 on MACI64
+% Last modified: 20-Mar-2024, Version 9.3.0.948333 (R2017b) Update 9 on MACI64
 
 if ~isrow(tz)
     tz = tz';
@@ -32,3 +33,6 @@ z = z(:);
 numz = sum(~isnan(z));
 numo = sum(z > tz);
 perc = 100 * (numo / numz);
+
+tz = tz';
+perc = perc';
