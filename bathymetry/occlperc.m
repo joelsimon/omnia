ec30b20334203759ca45791e8084b6cea24b53ab
Joel D. Simon (Darwin) <jdsimon@princeton.edu>
2024-03-27 11:47:18 -0700

    Retern output with same size as input

--- bathymetry/occlperc.m
+++ bathymetry/occlperc.m
@@ -1,5 +1,5 @@
-function [perc, tz] = occlperc(z, tz)
-% [perc, tz] = OCCLPERC(z, tz)
+function perc = occlperc(z, tz)
+% perc = OCCLPERC(z, tz)
 %
 % Compute occlusion percentage where occlusion is defined to be any elevation
 % (`z`) that is HIGHER (less deep) than a test elevation (`tz).
@@ -11,8 +11,7 @@ function [perc, tz] = occlperc(z, tz)
 % z        Elevation array or matrix, e.g., from gebco.m [m]
 %
 % Output:
-% perc     Occlusion percentage at test depths, returned as column [m]
-% tz       Test elevation array, returned as column [m]
+% perc     Occlusion percentage at every test depth [m]
 %
 % Ex:
 %    z = [-10 -5 0; -25 -15 -20; -10 0 -5]
@@ -25,14 +24,32 @@ function [perc, tz] = occlperc(z, tz)
 % Contact: jdsimon@alumni.princeton.edu | joeldsimon@gmail.com
 % Last modified: 20-Mar-2024, Version 9.3.0.948333 (R2017b) Update 9 on MACI64
 
+% Must convert test depths to rows so that ">" comparison works as expect for matrix.
+trow = true;
 if ~isrow(tz)
+    trow = false;
     tz = tz';
 
 end
+
+% Vectorize elevation matrix (slide next column to start where last ends).
 z = z(:);
+
+% Tally number of actual elevations (excluding NaNs).
 numz = sum(~isnan(z));
+
+% Tally number of elevations greater than the test elevation.  This works for
+% arrays of `tz` because, "Arrays with compatible sizes are implicitly expanded
+% to be the same size during execution of the calculation." (Essentially `z` is
+% copied `tz`-number of times for us).
 numo = sum(z > tz);
+
+% The output percentage is just the number of elevations greater than the
+% test elevations divided by the total number of non-NaN elevations.
 perc = 100 * (numo / numz);
 
-tz = tz';
-perc = perc';
+% Return output with same dimension as input test depth.
+if ~trow
+    perc = perc';
+
+end
