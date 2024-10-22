c7e93cf7c1d11165f00b92072740c4a37949763b
Joel D. Simon (geo_mac) <jdsimon@princeton.edu>
2024-10-21 18:05:51 -0700

    Handle char or cell; return same dim as input

--- earthquakes/sackstnm.m
+++ earthquakes/sackstnm.m
@@ -1,14 +1,26 @@
 function kstnm = sackstnm(sac)
 % kstnm = SACKSTNM(sac)
 %
-% Return cell array of SAC header fields KSTNM (five-character station name)
+% Return cell array of SAC header fields KSTNM (five-character station name).
 %
 % Author: Joel D. Simon
 % Contact: jdsimon@alumni.princeton.edu | joeldsimon@gmail.com
-% Last modified: 09-Oct-2024, 24.1.0.2568132 (R2024a) Update 1 on MACA64 (geo_mac)
+% Last modified: 21-Oct-2024, 24.1.0.2568132 (R2024a) Update 1 on MACA64 (geo_mac)
 
-for i = 1:length(sac)
-    h = sachdr(sac{i});
-    kstnm{i} = h.KSTNM;
+% Recursion
+if iscell(sac)
+    for i = 1:length(sac)
+        kstnm{i} = sackstnm(sac{i});
+
+    end
+    if ~isrow(sac)
+        kstnm = kstnm';
+
+    end
+    return
 
 end
+
+% Main
+h = sachdr(sac);
+kstnm = h.KSTNM;
