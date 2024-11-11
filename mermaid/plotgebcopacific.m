25645dd4a2ee0196d95320885ccbcc8ecad4527a
Joel D. Simon (geo_mac) <jdsimon@princeton.edu>
2024-11-11 11:57:15 -0800

    Allow specification of color-axis limits

--- mermaid/plotgebcopacific.m
+++ mermaid/plotgebcopacific.m
@@ -1,11 +1,16 @@
-function F = plotgebcopacific()
-% F = PLOTGEBCOPACIFIC
+function F = plotgebcopacific(cax)
+% F = PLOTGEBCOPACIFIC(cax)
 %
 % Plot GEBCO basemap of entire South Pacific: 120 -> 300 E; -60 -> 80 N.
 %
+% Input:
+% cax     Color-axis limits (discontinuous at 0) [def: [-7000 1500]]
+%
 % Author: Joel D. Simon
 % Contact: jdsimon@alumni.princeton.edu | joeldsimon@gmail.com
-% Last modified: 10-Jul-2023, Version 9.3.0.948333 (R2017b) Update 9 on MACI64
+% Last modified: 11-Nov-2024, 24.1.0.2568132 (R2024a) Update 1 on MACA64 (geo_mac)
+
+defval('cax', [-7000 1500])
 
 % Plot basemap.
 F.f = figure;
@@ -19,7 +24,6 @@ c11 = [120 80];
 cmn = [300 -60];
 
 % Color bar first...
-cax = [-7000 1500];
 [F.cb, F.cm] = cax2dem(cax, 'hor');
 
 % then map
