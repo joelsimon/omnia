58f5d8e427460213690527f54005bb7b9a09f74a
Joel D. Simon (geo_mac) <jdsimon@princeton.edu>
2025-02-18 14:24:13 -0800

    Make recursive for cell inputs

--- earthquakes/sachdr.m
+++ earthquakes/sachdr.m
@@ -4,15 +4,26 @@ function  HdrData = sachdr(filename, endianness)
 % Wrapper for readsac.m that only returns header.
 %
 % Input:
-% filename        SAC file name
+% filename        SAC file name (or cell array)
 % endianness      'l': little-endian (default)
 %                 'b': big-endian
 % Output:
-% HdrData         The header structure array
+% HdrData         The header structure array (or arrays, if filename is cell)
 %
 % Author: Joel D. Simon
 % Contact: jdsimon@alumni.princeton.edu | joeldsimon@gmail.com
-% Last modified: 17-Jan-2024, Version 9.3.0.948333 (R2017b) Update 9 on MACI64
+% Last modified: 18-Feb-2025, 24.1.0.2568132 (R2024a) Update 1 on MACA64 (geo_mac)
 
+% Default.
 defval('endianness', 'l')
+
+% Recursion.
+if iscell(filename)
+    for i = 1:length(filename)
+        HdrData(i) = sachdr(filename{i}, endianness);
+    end
+    return
+end
+
+% Main.
 [~, HdrData] = readsac(filename, endianness, true);
