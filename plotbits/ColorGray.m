5397f376133fc02d996a40002af83a87aba6ffdc
Joel D. Simon (geo_mac) <jdsimon@princeton.edu>
2025-03-10 16:03:49 -0700

    Return gray/grey RGB triplet

---
+++ plotbits/ColorGray.m
@@ -0,0 +1,15 @@
+function col = ColorGray
+% COLORGRAY
+%
+% Returns gray RGB tripled: [0.6 0.6 0.6].
+%
+% Ex:
+%    plot(1:10, 'Color', COLORGRAY)
+%
+% Author: Joel D. Simon
+% Contact: jdsimon@alumni.princeton.edu | joeldsimon@gmail.com
+% Last modified: 10-Mar-2025, 24.1.0.2568132 (R2024a) Update 1 on MACA64 (geo_mac)
+
+col = [0.6 0.6 0.6];
+   
+    
---
+++ plotbits/ColorGrey.m
@@ -0,0 +1,13 @@
+function col = ColorGrey
+% COLORGREY
+%
+% Returns grey RGB tripled: [0.6 0.6 0.6].
+%
+% Ex:
+%    plot(1:10, 'Color', COLORGREY)
+%
+% Author: Joel D. Simon
+% Contact: jdsimon@alumni.princeton.edu | joeldsimon@gmail.com
+% Last modified: 10-Mar-2025, 24.1.0.2568132 (R2024a) Update 1 on MACA64 (geo_mac)
+
+col = ColorGray;
