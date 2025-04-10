bbb4cbbbfde21eeb3aa0d0dfe56c429200f82b82
Joel D. Simon (geo_mac) <jdsimon@princeton.edu>
2025-04-10 09:26:38 -0700

    MERMAID SAC re-preprocessing: re-merge and re-fill
    This is just sacworkflow without the copy (don't want to recopy from
    processed/); useful if you want to test merge schemes, etc.

---
+++ JGR2025/src/sacworkflow2.m
@@ -0,0 +1,59 @@
+function sacworkflow2(ser)
+% SACWORKFLOW2(ser)
+%
+% Re-merge and re-fill .sac, e.g., after removal of overlap/unused/bad .sac
+%
+% This is just sacworkflow.m without the copy.
+%
+% Author: Joel D. Simon
+% Contact: jdsimon@alumni.princeton.edu | joeldsimon@gmail.com
+% Last modified: 01-Feb-2023, Version 9.3.0.948333 (R2017b) Update 9 on MACI64
+
+clc
+close all
+
+% Paths
+sacdir = fullfile(getenv('HUNGA'), 'sac');
+undir = fullfile(sacdir, 'unmerged');
+metadir = fullfile(sacdir, 'meta');
+
+%% Merge (combine multiple *.sac to write *.merged.sac)
+if isempty(ser)
+    unsac = globglob(undir, '*sac');
+    unser = unique(getmerser(unsac));
+
+else
+    unser = {ser};
+
+end
+for i = 1:length(unser)
+    % Merge
+    sac2mergesac_out = fullfile(metadir, sprintf('sac2mergedsac_%s.out', unser{i}));
+    merge_cmd = sprintf('sac2mergedsac %s >! %s', unser{i}, sac2mergesac_out);
+    fprintf('Running: `%s`\n', merge_cmd);
+    [status, result] = system(merge_cmd);
+    if status
+        error('`%s` failed with error:\n%s', merge_cmd, result)
+
+    end
+
+    % Write gap file (from .txt to .mat)
+    writegap(sac2mergesac_out);
+
+end
+
+%% Fill (interpolate zero-filled gaps from merge and write *merged.filled.sac)
+if isempty(unser)
+    sac = globglob(sacdir, '*merged.sac');
+
+else
+    sac = globglob(sacdir, sprintf('*.%s_*merged.sac', ser));
+
+end
+for i = 1:length(sac)
+    fprintf('Runing `mergedsac2filledsac`: %s\n', strippath(sac{i}))
+    mergedsac2filledsac(sac{i}, false);
+
+end
+
+warning('Now go delete/remake .evt files')
