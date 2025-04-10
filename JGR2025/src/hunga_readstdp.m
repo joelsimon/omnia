8e95546ce6bc011c52fcf9ada0532c1193a80fbc
Joel D. Simon (geo_mac) <jdsimon@princeton.edu>
2025-04-10 12:32:38 -0700

    Read station depth from metadata text file

---
+++ JGR2025/src/hunga_readstdp.m
@@ -0,0 +1,27 @@
+function stdp = hunga_readstdp(kstnm, filename)
+% stdp = HUNGA_READSTDP(kstnm, filename)
+%
+% Input:
+% kstnm    Station name; if supplied, only one depth returned [m]
+%
+% Author: Joel D. Simon
+% Contact: jdsimon@alumni.princeton.edu | joeldsimon@gmail.com
+% Last modified: 18-Dec-2024, 24.1.0.2568132 (R2024a) Update 1 on MACA64 (geo_mac)
+
+defval('filename', fullfile(getenv('HUNGA'), 'sac', 'meta', 'stdp.txt'))
+defval('kstnm', [])
+
+fid = fopen(filename, 'r');
+txt = textscan(fid, '%s %f');
+fclose(fid);
+if ~isempty(kstnm)
+    stdp = txt{2}(cellstrfind(txt{1}, kstnm));
+
+else
+    stdp = struct();
+    for i = 1:length(txt{1})
+        stdp.(txt{1}{i}) = txt{2}(i);
+
+    end
+end
+
