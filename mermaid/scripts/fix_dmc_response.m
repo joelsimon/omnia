b17db339a1c6faea1b56bcd7cdc51f56927dcd0e
Joel D. Simon (geo_mac) <jdsimon@princeton.edu>
2025-07-02 13:56:50 -0700

    Edit dmc_fixed.pz to have int before decimal
    E.g, 0.4e-1 --> 4.0e-2

--- mermaid/scripts/fix_dmc_response.m
+++ mermaid/scripts/fix_dmc_response.m
@@ -28,24 +28,31 @@ function fix_dmc_response
 %
 % Author: Joel D. Simon
 % Contact: jdsimon@alumni.princeton.edu | joeldsimon@gmail.com
-% Last modified: 23-Jun-2025, 24.1.0.2568132 (R2024a) Update 1 on MACA64 (geo_mac)
+% Last modified: 02-Jul-2025, 24.1.0.2568132 (R2024a) Update 1 on MACA64 (geo_mac)
 
 clc
 
-old = readtext('response/dmc_error.pz'); % bad
-new = readtext('response/nolet.pz'); % good
+% Old.
+f1 ='response/dmc_error.pz';
 
-% % You can set old=dmc_fixed.pz and compare (no output) to nolet show my
-% % formatted fix is identical to how Nolet originally printed in PDF.
-% old = readtext('response/dmc_fixed.pz'); % also good; formatted for DMC
+% New.
+f2 = 'response/dmc_fixed.pz';
 
+% %% UNCOMMENT TO VERIFY THAT NOLET AND MY REFORMATED FIX ARE IDENTICAL
+% f1 = 'response/nolet.pz';
+% %% UNCOMMENT TO VERIFY THAT NOLET AND MY REFORMATED FIX ARE IDENTICAL
+
+old = readtext(f1);
+new = readtext(f2);
+
+dif = false;
 for i = 1:length(old)
     if startsWith(old{i}, {'Z', 'P', 'C'})
         continue
 
     end
-    ds = old{i};
-    ns = new{i};
+    ds = strip(old{i});
+    ns = strip(new{i});
 
     dd = str2num(ds);
     nd = str2num(ns);
@@ -53,15 +60,22 @@ for i = 1:length(old)
     wrong = (dd ~= nd);
 
     if wrong(1)
+        dif = true;
         fprintf('LINE %2i: (first column)\n', i)
         fprintf('    DMC: %14s\n', ds(1:14))
         fprintf('  NOLET: %14s\n\n', ns(1:14))
 
     end
     if wrong(2)
+        dif = true;
         fprintf('LINE %2i: (second column)\n', i)
         fprintf('    DMC: %14s\n', ds(15:end))
         fprintf('  NOLET: %14s\n\n', ns(15:end))
 
     end
+end
+
+if dif == false
+    fprintf('%s and %s are identical\n', strippath(f1), strippath(f2))
+
 end
