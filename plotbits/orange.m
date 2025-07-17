42cc42fdb69127e5d6bc255ca459452487d644fc
Joel D. Simon (geo_mac) <jdsimon@princeton.edu>
2025-07-17 12:59:57 -0700

    Orange you glad I didn't say banana?

---
+++ plotbits/orange.m
@@ -0,0 +1,10 @@
+function rgb = orange
+% ORANGE
+%
+% Returns RGB orange: [1.0 0.5 0.0]
+%
+% Author: Joel D. Simon
+% Contact: jdsimon@alumni.princeton.edu | joeldsimon@gmail.com
+% Last modified: 02-Jul-2025, 24.1.0.2568132 (R2024a) Update 1 on MACA64 (geo_mac)
+
+rgb = [1.0 0.5 0.0];
